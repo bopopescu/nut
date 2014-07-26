5d07abce39d4376d935f0f0017d76a035b4ddfb7
jiaxin <edison7500@gmail.com>
2014-07-27 01:32:53 +0800

    update

---
+++ nut/apps/base/models/__init__.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class BaseModel(models.Model):
+
+    class Meta:
+        abstract = True
+
+    def toDict(self):
+        fields = []
+        for f in  self._meta.fields:
+            fields.append(f.column)
+        d = {}
+        for attr in fields:
+            d[attr] = "%s" % getattr(self, attr)
+        return d
+
+__author__ = 'edison7500'
