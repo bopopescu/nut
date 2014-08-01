3d18457c286ffb12e39e2e9b90b7f8fc7025c002
edison7500 <edison7500@gmail.com>
2014-08-02 05:19:50 +0800

    update

--- nut/apps/management/views/entities.py
+++ nut/apps/management/views/entities.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from apps.core.models import Entity
 
 
@@ -10,7 +11,15 @@ def list(request, template = 'management/entities/list.html'):
     return render_to_response(template,
                             {
 
-                            }
-                              )
+                            },
+                            context_instance = RequestContext(request))
+
+
+def detail(request, entity_id,  template='management/entities/detail.html'):
+
+    return render_to_response(template,
+                        {},
+                        context_instance = RequestContext(request))
+
 
 __author__ = 'edison7500'
