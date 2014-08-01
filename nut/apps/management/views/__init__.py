28a64c1dc1f03442639799ceb3ebe39ab57f678a
edison7500 <edison7500@gmail.com>
2014-08-02 00:17:00 +0800

    update

--- nut/apps/management/views/__init__.py
+++ nut/apps/management/views/__init__.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render_to_response
-
+from django.template import RequestContext
 
 def dashboard(request, template='management/dashboard.html'):
 
 
-    return render_to_response(template)
+    return render_to_response(template,
+                                {},
+                                context_instance = RequestContext(request))
 
 
 __author__ = 'edison7500'
