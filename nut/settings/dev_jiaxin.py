ed32b25e9d5712e560fc3118f6191f9d2e972b52
edison7500 <edison7500@gmail.com>
2014-08-01 22:48:50 +0800

    update

--- nut/settings/dev_jiaxin.py
+++ nut/settings/dev_jiaxin.py
@@ -4,4 +4,14 @@ DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
 APPEND_SLASH = True
 
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
+MIDDLEWARE_CLASSES = (
+    # ...
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # ...
+)
+
 __author__ = 'edison7500'
