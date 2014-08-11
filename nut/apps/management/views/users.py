e263f01efccae65fbc1ae71042ee84396991dc53
edison7500 <edison7500@gmail.com>
2014-08-11 11:54:34 +0800

    update

--- nut/apps/management/views/users.py
+++ nut/apps/management/views/users.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.paginator import Paginator, EmptyPage, InvalidPage
@@ -7,13 +8,21 @@ from apps.core.models import GKUser
 
 def list(request, template="management/users/list.html"):
 
+    page = request.GET.get('page', 1)
     user_list = GKUser.objects.all()
 
+    paginator = Paginator(user_list, 30)
 
+    try:
+        users = paginator.page(page)
+    except InvalidPage:
+        users = paginator.page(1)
+    except EmptyPage:
+        raise Http404
 
     return render_to_response(template,
                             {
-
+                                'users':users,
                             },
                             context_instance = RequestContext(request))
 
