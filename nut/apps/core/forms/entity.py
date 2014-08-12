10b5457ab3d024d87a7624fe7401aa90f6cf6c02
edison7500 <edison7500@gmail.com>
2014-08-12 11:01:11 +0800

    update

--- nut/apps/core/forms/entity.py
+++ nut/apps/core/forms/entity.py
@@ -3,13 +3,17 @@ from django.utils.translation import gettext_lazy as _
 
 
 class EntityFrom(forms.Form):
-    id = forms.CharField()
+    id = forms.CharField(label=_('user_id'),
+                         widget=forms.NumberInput(attrs={'class':'form-control', 'disabled':''}),
+                         help_text=_(''))
     brand = forms.CharField(label=_('brand'),
                             widget=forms.TextInput(attrs={'class':'form-control'}),
                             help_text=_(''))
-    title = forms.CharField()
-    intro = forms.CharField()
-    price = forms.DecimalField(max_digits=20, decimal_places=2)
+    title = forms.CharField(label=_('title'),
+                            widget=forms.TextInput(attrs={'class':'form-control'}),
+                            help_text=_(''))
+    intro = forms.CharField(label=_('intro'), widget=forms.Textarea(attrs={'class':'form-control'}), help_text=_(''))
+    price = forms.DecimalField(max_digits=20, decimal_places=2, label=_('price'))
 
 
 __author__ = 'edison7500'
