36f1b9d15cd8fb7317e432cbed50819b1bc37600
edison7500 <edison7500@gmail.com>
2014-08-14 15:26:59 +0800

    update banner css

--- nut/apps/management/views/banner.py
+++ nut/apps/management/views/banner.py
@@ -3,13 +3,15 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
-from apps.core.models import Banner
+from apps.core.models import Banner, Show_Banner
 
 def list(request, template="management/banner/list.html"):
 
     page = request.GET.get('page', 1)
-    banner_list = Banner.objects.all()
 
+    show_banners = Show_Banner.objects.all()[0:4]
+
+    banner_list = Banner.objects.all()
     paginator = Paginator(banner_list, 30)
 
     try:
@@ -22,7 +24,8 @@ def list(request, template="management/banner/list.html"):
     return render_to_response(
                     template,
                     {
-                        'banners':banners,
+                        'banners': banners,
+                        'show_banners': show_banners,
                     },
                     context_instance = RequestContext(request)
                 )
