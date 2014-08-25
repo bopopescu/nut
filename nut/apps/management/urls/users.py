171beffec6cecf658c2407a7bf3da19897f89d26
edison7500 <edison7500@gmail.com>
2014-08-25 23:54:28 +0800

    updaet

--- nut/apps/management/urls/users.py
+++ nut/apps/management/urls/users.py
@@ -4,6 +4,7 @@ urlpatterns = patterns(
     'apps.management.views.users',
     url(r'^$', 'list', name='management_user_list'),
     url(r'^(?P<user_id>\d+)/edit/$', 'edit', name='management_user_edit'),
+    url(r'^(?P<user_id>\d+)/reset-password/$', 'reset_password', name='management_user_reset_password'),
 )
 
 __author__ = 'edison'
