d59cac273bf9bec69c678ce3e743304b7b38c6f7
edison7500 <edison7500@gmail.com>
2014-08-26 01:04:41 +0800

    update

--- nut/apps/management/views/note_comment.py
+++ nut/apps/management/views/note_comment.py
@@ -1,22 +1,35 @@
 from django.http import Http404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.core.paginator import Paginator, EmptyPage, InvalidPage
 from django.utils.log import getLogger
 
 from apps.core.models import Note_Comment
+from apps.core.extend.paginator import ExtentPaginator, InvalidPage, EmptyPage
+
 
 log = getLogger('django')
 
 
 def list(request, template='management/comment/list.html'):
 
+    page = request.GET.get('page', 1)
+
     comment_list = Note_Comment.objects.all()
+
+    paginator = ExtentPaginator(comment_list, 30)
+
+
+    try:
+        comments = paginator.page(page)
+    except InvalidPage:
+        comments = paginator.page(1)
+    except EmptyPage:
+        raise Http404
 
     return render_to_response(
         template,
         {
-
+            'comments': comments,
         },
         context_instance = RequestContext(request)
     )
