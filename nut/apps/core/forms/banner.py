e0e78d44681ba45ce800f3fdfb06b06240f6104c
jiaxin <edison7500@gmail.com>
2014-08-28 12:27:23 +0800

    update

--- nut/apps/core/forms/banner.py
+++ nut/apps/core/forms/banner.py
@@ -1,1 +1,12 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from django.utils.log import getLogger
+
+log = getLogger('django')
+
+
+class CreateBannerForm(forms.Form):
+    key = forms.CharField(label=_('key'))
+    banner_image = forms.ImageField()
+
 __author__ = 'edison'
