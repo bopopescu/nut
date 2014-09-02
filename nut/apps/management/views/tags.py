681d7505885fd48555020f411838b64807d49307
edison7500 <edison7500@gmail.com>
2014-09-02 13:00:42 +0800

    update

--- nut/apps/management/views/tags.py
+++ nut/apps/management/views/tags.py
@@ -3,15 +3,25 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from apps.core.models import Tag
-
+from apps.core.extend.paginator import ExtentPaginator, EmptyPage, InvalidPage
 
 def list(request, template='management/tags/list.html'):
 
+    page = request.GET.get('page', 1)
+    tag_list = Tag.objects.all()
+
+    paginator = ExtentPaginator(tag_list, 30)
+    try:
+        _tags = paginator.page(page)
+    except InvalidPage:
+        _tags = paginator.page(1)
+    except EmptyPage:
+        raise Http404
 
     return render_to_response(
             template,
             {
-
+                'tags': _tags,
             },
             context_instance = RequestContext(request)
             )
