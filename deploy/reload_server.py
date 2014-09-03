ae6aef4b7788423abe0de744ae97b4eba85dde59
edison7500 <edison7500@gmail.com>
2014-09-03 15:40:55 +0800

    add deploy script

---
+++ deploy/reload_server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+
+import ConfigParser
+
+from fabric.api import *
+from fabric.context_managers import *
+
+Config = ConfigParser.ConfigParser()
+Config.read('config.ini')
+
+
+env.hosts = ['114.113.154.49']
+env.user = Config.get('global', 'user')
+#env.key = Config.get('global', 'key')
+env.password = 'jessie1@#'
+
+script_dir = Config.get('server', 'script_dir')
+
+def reload_gunicorn():
+    with cd(script_dir):
+        sudo('/bin/bash ./gunicorn reload')
+
+def restart_celery():
+    with cd(script_dir):
+        run('/bin/bash ./celeryd restart')
+
+def reload():
+    # execute(sudo)
+    execute(reload_gunicorn)
+    execute(restart_celery)
---
+++ deploy/update_online_code.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+find ../ -name '*.pyc' -exec rm {} \;
+#FAB='/Users/edison7500/PycharmProjects/django15/bin/fab'
+FAB=`which fab`
+
+${FAB} -f upload_code.py upload
+${FAB} -f reload_server.py reload
+#${FAB} -f upload_static.py deploy_static
