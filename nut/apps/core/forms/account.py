e17952378f9b722e2be0012f676edd66a4137e12
edison7500 <edison7500@gmail.com>
2014-09-12 15:08:44 +0800

    add use login

--- nut/apps/core/forms/account.py
+++ nut/apps/core/forms/account.py
@@ -1,1 +1,24 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+
+class GuoKuUserSignInForm(forms.Form):
+
+    email = forms.EmailField(
+        label=_('email'),
+    )
+
+    password = forms.CharField(
+        label=_('password'),
+        widget=forms.PasswordInput(),
+        min_length=6,
+        help_text=_('')
+    )
+
+    def __init__(self, request=None, *args, **kwargs):
+        self.request = request
+        self.user_cache = None
+
+        super(GuoKuUserSignInForm, self).__init__(*args, **kwargs)
+
 __author__ = 'edison'
