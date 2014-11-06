ba5eb6976829128b5f5d923aa00f15dec35e3d41
edison7500 <edison7500@gmail.com>
2014-11-06 20:58:54 +0800

    update

--- nut/apps/api/serializers/entity.py
+++ nut/apps/api/serializers/entity.py
@@ -1,12 +1,30 @@
-from apps.core.models import Entity
+from apps.core.models import Entity, GKUser
 from rest_framework import serializers
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    # nickname = serializers.CharField(source='profile.nickname')
+
+    class Meta:
+        model = GKUser
+        fields = ('email',)
 
 
 class EntitySerializer(serializers.HyperlinkedModelSerializer):
 
+    user = UserSerializer()
+    chief_image = serializers.URLField(source='chief_image')
+    detail_images = serializers.CharField(source='detail_images')
+    like_count = serializers.IntegerField(source='like_count', read_only=True)
+    note_count = serializers.IntegerField(source='note_count', required=True)
+    # creator = serializers.CharField(source='user')
+
     class Meta:
         model = Entity
-        depth = 1
+        fields = ('user', 'url', 'entity_hash', 'brand', 'title', 'intro', 'rate', 'price', 'status', 'chief_image', 'detail_images', 'like_count', 'note_count' )
+        # depth = 1
 
 
 
--- nut/apps/api/views/user.py
+++ nut/apps/api/views/user.py
@@ -10,15 +10,15 @@ from apps.api.serializers.user import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = GKUser.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAdminUser,)
 
 
-    @list_route()
-    def recent_users(self, request):
-        recent_users = GKUser.objects.filter(is_active=GKUser.active).order('-last_login')
+    @list_route(permission_classes=[permissions.IsAuthenticatedOrReadOnly])
+    def active(self, request):
+        recent_users = GKUser.objects.filter(is_active=GKUser.active)
         # recent_users = GKUser.objects.filter(is_admin=True)
         page = self.paginate_queryset(recent_users)
         serializer = self.get_pagination_serializer(page)
-        return Response(serializer)
+        return Response(serializer.data)
 
 __author__ = 'edison7500'
