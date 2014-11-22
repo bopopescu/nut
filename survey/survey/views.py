73df497c2bac6b7432a8ed07eb8a916eb881981e
edison7500 <edison7500@gmail.com>
2014-11-22 22:29:40 +0800

    update

--- survey/survey/views.py
+++ survey/survey/views.py
@@ -57,64 +57,18 @@ SURVEY_TEMPLATES = [
     "survey/profile.html",
     # 'profile': 'survey/profile.html',
     "survey/life.html",
+    "survey/consume.html",
     # ''
 ]
 
 class SurveyWizard(SessionWizardView):
 
-    # def get_form_initial(self, step):
-    #     log.info(step)
-    #     survey = Survey.objects.get(pk=1)
-    #     return self.initial_dict.get(step, {'survey': survey})
-    #
-    # def get_form_instance(self, step):
-    #     log.info(self.initial_dict)
-
-    # def get(self, request, sid, *args, **kwargs):
-    #     self.storage.reset()
-    #     self.storage.current_step = self.steps.first
-    #     self.survey = Survey.objects.get(pk=sid)
-    #     category_items = Category.objects.filter(survey=self.survey)
-    #
-    #     categories = [c.name for c in category_items]
-    #
-    #     return self.render(self.get_form(), survey = self.survey, category = categories[int(self.steps.current)])
-
-    # def post(self, *args, **kwargs):
-    #     # log.info(kwargs)
-    #     sid = kwargs.pop('sid')
-    #     wizard_goto_step = self.request.POST.get('wizard_goto_step', None)
-    #     if wizard_goto_step and wizard_goto_step in self.get_form_list():
-    #         return self.render_goto_step(wizard_goto_step)
-    #
-    #     management_form = ManagementForm(self.request.POST, prefix=self.prefix)
-    #     if not management_form.is_valid():
-    #         raise ValidationError(
-    #             _('ManagementForm data is missing or has been tampered.'),
-    #             code='missing_management_form',
-    #         )
-    #
-    #     form_current_step = management_form.cleaned_data['current_step']
-    #     if (form_current_step != self.steps.current and
-    #             self.storage.current_step is not None):
-    #         # form refreshed, change current step
-    #         self.storage.current_step = form_current_step
-    #
-    #     # get the form for the current step
-    #     self.survey = Survey.objects.get(pk=sid)
-    #     category_items = Category.objects.filter(survey=self.survey)
-    #
-    #     categories = [c.name for c in category_items]
-    #
-    #     form = self.get_form(data=self.request.POST, files=self.request.FILES)
-    #
-    #     return HttpResponse("OK")
 
     def get_form_kwargs(self, step=None):
         self.survey = Survey.objects.get(pk=1)
         # self.survey = Survey.objects.get(pk=sid)
         # category_items = Category.objects.filter(survey=self.survey)
-        step = self.steps.current
+        # step = self.steps.current
         cid = int(step) + 1
         # categories = [c.name for c in category_items]
         return {
@@ -122,38 +76,9 @@ class SurveyWizard(SessionWizardView):
             'cid': cid,
         }
 
-    def get_form(self, step=None, data=None, files=None):
-        step = self.steps.current
-        kwargs = self.get_form_kwargs(step)
-        # self.survey = Survey.objects.get(pk=1)
-        cid = int(step) + 1
-
-        # self.survey.question_set.filter(category_id=cid)
-        kwargs.update({
-            'data': data,
-            'files': files,
-            'prefix': self.get_form_prefix(step, self.form_list[step]),
-            'initial': self.get_form_initial(step),
-            # 'survey':self.survey,
-            # 'cid': cid,
-            # 'category': self.survey.question_set.filter(category=1),
-        })
-
-
-        if issubclass(self.form_list[step], forms.ModelForm):
-            # If the form is based on ModelForm, add instance if available
-            # and not previously set.
-            kwargs.setdefault('instance', self.get_form_instance(step))
-        elif issubclass(self.form_list[step], forms.models.BaseModelFormSet):
-            # If the form is based on ModelFormSet, add queryset if available
-            # and not previous set.
-            kwargs.setdefault('queryset', self.get_form_instance(step))
-        log.info(kwargs)
-        return self.form_list[step](**kwargs)
-        # return super(SurveyWizard, self).get_form(step, data, files)
 
     def get_template_names(self):
-        log.info(self.steps.current)
+        # log.info(self.steps.current)
         step = int(self.steps.current)
         return [SURVEY_TEMPLATES[step]]
 
