b4b8c712eafee76fa2e2ea2259cc76e07cf16f85
edison7500 <edison7500@gmail.com>
2014-11-23 21:28:11 +0800

    update web 4.0

--- nut/apps/web/urls/__init__.py
+++ nut/apps/web/urls/__init__.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url, include, patterns
+from apps.web.views.main import AboutView, JobsView, Agreement
+
 
 urlpatterns = patterns(
     'apps.web.views',
@@ -7,6 +9,17 @@ urlpatterns = patterns(
 
     url(r'^selection/$', 'main.selection', name='web_selection'),
     url(r'^popular/$', 'main.popular', name='web_popular'),
+    url(r'^search/$', 'main.search', name='web_search'),
+)
+
+
+# static page
+urlpatterns += patterns(
+    'apps.web.views.main',
+
+    url(r'^about/$', AboutView.as_view(), name='web_about'),
+    url(r'^jobs/$', JobsView.as_view(), name='web_jobs'),
+    url(r'^agreement$', Agreement.as_view(), name='web_agreement'),
 )
 
 __author__ = 'edison7500'
