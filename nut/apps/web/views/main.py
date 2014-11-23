a620fc53cfdd284fddaeeb5bb59610f4a2b57190
edison7500 <edison7500@gmail.com>
2014-11-23 21:28:11 +0800

    update web 4.0

--- nut/apps/web/views/main.py
+++ nut/apps/web/views/main.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
+from django.views.generic import TemplateView
 
 
 def index(request):
@@ -30,6 +31,29 @@ def popular(request, template='web/main/popular.html'):
         },
 
     )
+
+def search(request, template="web/main/search.html"):
+
+
+    return render_to_response(
+        template,
+        {
+
+        },
+
+    )
+
+class AboutView(TemplateView):
+    template_name = "web/about.html"
+
+class Agreement(TemplateView):
+    template_name = "web/agreement.html"
+
+class JobsView(TemplateView):
+    template_name = "web/jobs.html"
+
+class FaqView(TemplateView):
+    template_name = "web/base_faq.html"
 
 __author__ = 'edison'
 
