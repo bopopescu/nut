5b13cea6852818521afca972f5d4003e19a51036
edison7500 <edison7500@gmail.com>
2014-11-23 21:15:33 +0800

    add guoku web 4.0

--- nut/apps/web/views/main.py
+++ nut/apps/web/views/main.py
@@ -1,1 +1,37 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
+
+
+def index(request):
+
+
+    return HttpResponse("OK")
+
+
+def selection(request, template='web/main/selection.html'):
+
+
+    return render_to_response(
+        template,
+        {
+
+        },
+
+    )
+
+
+def popular(request, template='web/main/popular.html'):
+
+
+    return render_to_response(
+        template,
+        {
+
+        },
+
+    )
+
 __author__ = 'edison'
+
+
+
