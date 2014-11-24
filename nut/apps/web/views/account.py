09b2441a9a86bb6da03dc1f1a9c505458f7cf13e
edison7500 <edison7500@gmail.com>
2014-11-24 15:31:35 +0800

    update

--- nut/apps/web/views/account.py
+++ nut/apps/web/views/account.py
@@ -1,1 +1,21 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from apps.web.forms.account import UserSignInForm
+
+
+def login(request, template='web/account/login.html'):
+
+    if request.method == "POST":
+        _forms = UserSignInForm(request.POST)
+    else:
+        _forms = UserSignInForm()
+
+    return render_to_response(
+        template,
+        {
+            'forms': _forms,
+        },
+        context_instance = RequestContext(request)
+    )
+
 __author__ = 'edison'
