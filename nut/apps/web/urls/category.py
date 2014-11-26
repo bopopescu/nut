befd7b1fda70e1263a407bbc090d281e273c0232
jiaxin <edison7500@gmail.com>
2014-11-26 13:46:51 +0800

    update

--- nut/apps/web/urls/category.py
+++ nut/apps/web/urls/category.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns(
     'apps.web.views.category',
     url(r'^$', 'list', name='web_category_list'),
-    url(r'^(?P<cid>\+d)/$', 'detail', name='web_category_detail'),
+    url(r'^(?P<cid>\d+)/$', 'detail', name='web_category_detail'),
 )
 
 __author__ = 'edison7500'
--- nut/apps/web/views/category.py
+++ nut/apps/web/views/category.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render_to_response
+from django.http import Http404
 from django.views.decorators.http import require_GET
 from django.template import RequestContext
 
-from apps.core.models import Category
-
+from apps.core.models import Category, Sub_Category,  Entity
+from apps.core.extend.paginator import ExtentPaginator, PageNotAnInteger, EmptyPage
 
 @require_GET
 def list(request, template='web/category/list.html'):
@@ -21,12 +22,24 @@ def list(request, template='web/category/list.html'):
 
 @require_GET
 def detail(request, cid, template='web/category/detail.html'):
+    _cid = cid
+    _page = request.GET.get('page', 1)
+    _entity_list = Entity.objects.filter(status= Entity.selection, category=_cid)
+    _sub_category = Sub_Category.objects.get(pk = _cid)
 
+    paginator = ExtentPaginator(_entity_list, 30)
+    try:
+        _entities = paginator.page(_page)
+    except PageNotAnInteger:
+        _entities = paginator.page(1)
+    except EmptyPage:
+        raise Http404
 
     return render_to_response(
         template,
         {
-
+            'sub_category': _sub_category,
+            'entities': _entities,
         },
         context_instance = RequestContext(request),
     )
