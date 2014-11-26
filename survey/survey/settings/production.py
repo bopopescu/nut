bfcbdd0c49d11036ef8445072ebe8baf98b03a05
jiaxin <edison7500@gmail.com>
2014-11-26 15:26:01 +0800

    update

---
+++ survey/survey/settings/production.py
@@ -0,0 +1,15 @@
+from settings import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'survey',                      # Or path to database file if using sqlite3.
+        'USER': 'guoku',                      # Not used with sqlite3.
+        'PASSWORD': 'guoku1@#',                  # Not used with sqlite3.
+        'HOST': '10.0.2.90',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+__author__ = 'edison7500'
