7995c16cd9ea81ee8fadc9a5812a434f01b621f5
edison7500 <edison7500@gmail.com>
2014-12-05 11:48:32 +0800

    update

--- nut/apps/web/views/user.py
+++ nut/apps/web/views/user.py
@@ -1,1 +1,16 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def settings(request, template="web/user/settings.html"):
+
+    return render_to_response(
+        template,
+        {
+
+        },
+        context_instance = RequestContext(request),
+    )
+
+
 __author__ = 'edison'
