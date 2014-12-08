7258c81544eea7d6d51a2d14dbb518391e9fe0c7
edison7500 <edison7500@gmail.com>
2014-12-08 21:11:17 +0800

    update

--- nut/apps/web/forms/user.py
+++ nut/apps/web/forms/user.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
 # from apps.core.forms.user import UserForm
 from apps.core.models import User_Profile
@@ -7,6 +8,11 @@ from apps.core.models import User_Profile
 
 
 class UserSettingsForm(forms.Form):
+
+    error_messages = {
+        'duplicate_email' : _('A user with that email already exists.'),
+    }
+
     email = forms.EmailField(label=_('email'),
                              widget=forms.TextInput(attrs={'class':'form-control', 'type':'email'}),
                              help_text=_(''),
@@ -42,6 +48,22 @@ class UserSettingsForm(forms.Form):
                              required=False,
                              help_text=_(''))
 
+
+    def clean_email(self):
+        _email = self.cleaned_data.get('email')
+
+        if self.user_cache.email == _email:
+            return _email
+
+        try:
+            get_user_model()._default_manager.get(email = _email)
+        except get_user_model().DoesNotExist:
+            return _email
+        raise forms.ValidationError(
+            self.error_messages['duplicate_email'],
+            code= 'duplicate_email',
+        )
+        # return _email
 
     def __init__(self, user, *args, **kwargs):
         self.user_cache = user
