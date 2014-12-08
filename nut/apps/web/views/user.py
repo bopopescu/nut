1f9f433928bf618cc8836d66e3045c6b1ce90e16
edison7500 <edison7500@gmail.com>
2014-12-08 16:59:51 +0800

    update

--- nut/apps/web/views/user.py
+++ nut/apps/web/views/user.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from django.contrib.auth.forms import PasswordChangeForm
 from apps.web.forms.user import UserSettingsForm
 
 
@@ -10,14 +11,17 @@ def settings(request, template="web/user/settings.html"):
 
     if request.method == 'POST':
         _profile_form = UserSettingsForm(request.POST, user=_user)
+        _password_form = PasswordChangeForm(request.POST, user=_user)
     else:
         _profile_form = UserSettingsForm(user=_user)
+        _password_form = PasswordChangeForm(user=_user)
 
     return render_to_response(
         template,
         {
             'user':_user,
             'profile_form':_profile_form,
+            'password_form':_password_form,
         },
         context_instance = RequestContext(request),
     )
