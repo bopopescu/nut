e3972968e55edf2dad66d65d9ac903349cdf8d57
jiaxin <edison7500@gmail.com>
2014-12-10 16:19:25 +0800

    update

--- nut/apps/web/views/entity.py
+++ nut/apps/web/views/entity.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.template import loader
 
-from apps.core.models import Entity
+from apps.core.utils.http import JSONResponse
+from apps.core.models import Entity, Note, Note_Comment
 from django.utils.log import getLogger
 
 log = getLogger('django')
@@ -23,6 +25,30 @@ def entity_detail(request, entity_hash, templates='web/entity/detail.html'):
     )
 
 
+
+def entity_note_comment(request, nid, template='web/entity/note/comment_list.html'):
+
+    _user = None
+    if request.user.is_authenticated():
+        _user = request.user
+
+
+    _comment_list = Note_Comment.objects.filter(note_id= nid)
+
+    log.info(_comment_list)
+    _t = loader.get_template(template)
+    _c = RequestContext(request, {
+        'comment_list': _comment_list,
+        # 'note_context': _note_context,
+    })
+    _data = _t.render(_c)
+
+    return JSONResponse(
+        data={
+            'data':_data,
+        },
+        content_type='text/html; charset=utf-8',
+    )
 
 
 __author__ = 'edison'
