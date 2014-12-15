e25cb845a9446462ba7dbc3301f5f096d22da6e0
edison7500 <edison7500@gmail.com>
2014-12-15 23:43:39 +0800

    add image resize

--- nut/apps/images/views.py
+++ nut/apps/images/views.py
@@ -1,9 +1,30 @@
 from django.http import HttpResponse
-from wand.image import  Image as WandImage
+from django.utils.log import getLogger
+
+# from wand.image import  Image as WandImage
+from django.core.files.storage import default_storage
+from apps.core.utils.image import HandleImage
+# from django.utils.
+
+log = getLogger('django')
 
 
-def images(request, size, file_name):
+def images(request, file_name, size=None):
+    # log.info(size)
+    # size = request.GET.get('s', None)
+    image_name = 'images/' + file_name
 
-    return HttpResponse
+    f = default_storage.open(image_name)
+    # log.info(f.read())
+
+    if size is not None:
+        image = HandleImage(f)
+        _size = float(size)
+        image.resize(_size, _size)
+        image_data = image.image_data
+    else:
+        image_data = f.read()
+
+    return HttpResponse(image_data, content_type='image/jpeg')
 
 __author__ = 'edison'
