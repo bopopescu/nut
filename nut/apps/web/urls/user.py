63dbf5235ef116a5325993628e499315568f9486
edison7500 <edison7500@gmail.com>
2014-12-19 00:37:29 +0800

    update

--- nut/apps/web/urls/user.py
+++ nut/apps/web/urls/user.py
@@ -6,6 +6,8 @@ urlpatterns = patterns(
     url(r'settings/$', 'settings', name='web_user_settings'),
     url(r'upload/avatar/$', 'upload_avatar', name='web_user_upload_avatar'),
     url(r'(?P<user_id>\d+)/note/', 'post_note', name='web_user_post_note'),
+    url(r'(?P<user_id>\d+)/fans/', 'fans', name='web_user_fans'),
+    url(r'(?P<user_id>\d+)/followings/', 'following', name='web_user_followings'),
 )
 
 
