417f95dce843d61c8b21df3fe7c9638e4b305c56
edison7500 <edison7500@gmail.com>
2014-12-18 22:40:13 +0800

    update

--- nut/apps/web/urls/user.py
+++ nut/apps/web/urls/user.py
@@ -5,6 +5,7 @@ urlpatterns = patterns(
     'apps.web.views.user',
     url(r'settings/$', 'settings', name='web_user_settings'),
     url(r'upload/avatar/$', 'upload_avatar', name='web_user_upload_avatar'),
+    url(r'(?P<user_id>\d+)/note/', 'post_note', name='web_user_post_note'),
 )
 
 
