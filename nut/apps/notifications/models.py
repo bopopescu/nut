40de62bcf77601a02b6516ce9722e3eff9eb510b
谢 家欣 <edison7500@jiaxins-MacBook-Air-4.local>
2014-12-18 18:33:15 +0800

    update

---
+++ nut/apps/notifications/models.py
@@ -0,0 +1,25 @@
+import datetime
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+from django.db import models
+from django.utils.timezone import utc
+
+from model_utils import managers, Choices
+
+now=datetime.datetime.now
+if getattr(settings, 'USE_TZ'):
+    try:
+        from django.utils import timezone
+        now = timezone.now
+    except ImportError:
+        pass
+
+class Notification(models.Model):
+    LEVELS = Choices('success', 'info', 'warning', 'error')
+    level = models.CharField(choices=LEVELS, default='info', max_length=20)
+
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, blank=False, related_name='notifications')
+    unread = models.BooleanField(default=True, blank=False)
+
+__author__ = 'edison7500'
