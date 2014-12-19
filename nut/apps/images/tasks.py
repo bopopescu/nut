8915dcbea97110b4ef54710efed21e02ef99b2ce
edison7500 <edison7500@gmail.com>
2014-12-20 00:54:49 +0800

    update

--- nut/apps/images/tasks.py
+++ nut/apps/images/tasks.py
@@ -1,1 +1,25 @@
+from celery.task import task
+from apps.core.tasks import DebugTask, BaseTask
+from apps.core.utils.image import HandleImage
+from django.core.files.storage import default_storage
+
+
+@task(base=BaseTask)
+def resize(image_name, size=None, **kwargs):
+
+    # print "OKOK"
+
+    f = default_storage.open(image_name)
+    # log.info(f.read())
+
+    if size is not None:
+        image = HandleImage(f)
+        _size = float(size)
+        image.resize(_size, _size)
+        image_data = image.image_data
+    else:
+        image_data = f.read()
+
+    return image_data
+
 __author__ = 'edison'
--- nut/apps/images/views.py
+++ nut/apps/images/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.utils.log import getLogger
 
+from apps.images.tasks import resize
 # from wand.image import  Image as WandImage
-from django.core.files.storage import default_storage
-from apps.core.utils.image import HandleImage
+# from django.core.files.storage import default_storage
+# from apps.core.utils.image import HandleImage
+
 # from django.utils.
 
 log = getLogger('django')
@@ -17,17 +19,19 @@ def images(request, file_name, size=None):
     log.info(path)
     image_name = "%s/%s" % (path[1], path[-1])
 
-    f = default_storage.open(image_name)
-    # log.info(f.read())
+    # f = default_storage.open(image_name)
+    # # log.info(f.read())
+    #
+    # if size is not None:
+    #     image = HandleImage(f)
+    #     _size = float(size)
+    #     image.resize(_size, _size)
+    #     image_data = image.image_data
+    # else:
+    #     image_data = f.read()
 
-    if size is not None:
-        image = HandleImage(f)
-        _size = float(size)
-        image.resize(_size, _size)
-        image_data = image.image_data
-    else:
-        image_data = f.read()
-
+    result = resize.apply_async((image_name, size))
+    image_data = result.get()
     return HttpResponse(image_data, content_type='image/jpeg')
 
 __author__ = 'edison'
