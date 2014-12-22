7f810363886297455d2b181affd18989e622946b
edison7500 <edison7500@gmail.com>
2014-12-22 10:39:22 +0800

    update

--- nut/apps/core/tasks/entity.py
+++ nut/apps/core/tasks/entity.py
@@ -24,9 +24,13 @@ def fetch_image(images, entity_id, *args, **kwargs):
         image = HandleImage(f)
         # print image.image_data
         # image.name
-        image_name = image_path + image.name
-        image_name = image_host + default_storage.save(image_name, ContentFile(image.image_data))
-        image_list.append(image_name)
+        image_name = image_path + "%s.jpg" % image.name
+        if default_storage.exists(image_name):
+            image_name = image_host + image_name
+            image_list.append(image_name)
+        else:
+            image_name = image_host + default_storage.save(image_name, ContentFile(image.image_data))
+            image_list.append(image_name)
 
     try:
         entity = Entity.objects.get(pk = entity_id)
