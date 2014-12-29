09c07d277cd3a129346081c66488e88494b036ca
edison7500 <edison7500@gmail.com>
2014-12-29 23:21:01 +0800

    update

--- nut/apps/management/urls/note.py
+++ nut/apps/management/urls/note.py
@@ -4,7 +4,7 @@ urlpatterns = patterns(
     'apps.management.views.note',
     url(r'^$', 'list', name='management_note_list'),
     url(r'^(?P<note_id>\d+)/edit/$', 'edit', name='management_note_edit'),
-    url(r'^create/$', 'create', name='management_note_create'),
+    url(r'^create/(?P<entity_id>\d+)/$', 'create', name='management_note_create'),
 )
 
 urlpatterns += patterns(
