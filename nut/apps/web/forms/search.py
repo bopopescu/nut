1d9e9d8a55c6e2f8f33cc77e7bff0b9aa2216387
edison7500 <edison7500@gmail.com>
2014-12-30 23:15:17 +0800

    update

--- nut/apps/web/forms/search.py
+++ nut/apps/web/forms/search.py
@@ -1,8 +1,11 @@
-# from django import forms
+from django import forms
+from django.utils.translation import gettext_lazy as _
 # from haystack.forms import SearchForm
-# from django.utils.log import getLogger
+from django.utils.log import getLogger
+from apps.core.models import GKUser
+
 #
-# log = getLogger('django')
+log = getLogger('django')
 #
 #
 # class EntitySearchForm(SearchForm):
@@ -19,5 +22,26 @@
 #
 #         return sqs
 #
-#
-# __author__ = 'edison'
+
+class SearchForm(forms.Form):
+    q = forms.CharField(required=False, label=_('Search'),
+                        widget=forms.TextInput(attrs={'type': 'search'}))
+
+    t = forms.CharField(required=False, label=_('type'),
+                        widget=forms.TextInput())
+    
+    def search(self):
+        _keyword = self.get_keyword()
+        # log.info(self.cleaned_data.get('q'))
+        # log.info("OKOKOKO")
+        # pass
+        # _keyword = self.cleaned_data.get('q')
+        sqs = GKUser.search.query(_keyword)
+        return sqs
+
+    def get_keyword(self):
+        self.keyword = self.cleaned_data.get('q')
+
+        return self.keyword
+
+__author__ = 'edison'
