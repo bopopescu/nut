e522b4d9089e4bbc69542b3ea00c34c5a7e131d6
edison7500 <edison7500@gmail.com>
2015-01-06 13:13:27 +0800

    update

--- nut/apps/management/views/category.py
+++ nut/apps/management/views/category.py
@@ -3,36 +3,73 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from apps.core.models import Category
-from apps.core.extend.paginator import ExtentPaginator, InvalidPage, EmptyPage
+from apps.core.forms.category import EditCategoryForm
+from apps.core.extend.paginator import ExtentPaginator, PageNotAnInteger, EmptyPage
 
-def list(request, category_id=1, template='management/category/list.html'):
+def list(request, template='management/category/list.html'):
     #
     # c = request.GET.get('c', '1')
     page = request.GET.get('page', 1)
 
     categories = Category.objects.all()
 
-    category = Category.objects.get(pk = category_id)
-    sub_category_list = category.sub_categories.all()
+    # category = Category.objects.get(pk = category_id)
+    # sub_category_list = category.sub_categories.all()
 
-    paginator = ExtentPaginator(sub_category_list, 30)
+    paginator = ExtentPaginator(categories, 30)
 
+    # try:
+    #     sub_categories = paginator.page(page)
+    # except PageNotAnInteger:
+    #     sub_categories = paginator.page(1)
+    # except EmptyPage:
+    #      raise Http404
     try:
-        sub_categories = paginator.page(page)
-    except InvalidPage:
-        sub_categories = paginator.page(1)
+        category_list = paginator.page(page)
+    except PageNotAnInteger:
+        category_list = paginator.page(1)
     except EmptyPage:
-         raise Http404
+        raise Http404
+
 
     return render_to_response(
         template,
         {
-            'current_category_id': int(category_id),
-            'categories': categories,
-            'sub_categories': sub_categories,
+            # 'current_category_id': int(category_id),
+            'category_list': category_list,
+            # 'sub_categories': sub_categories,
         },
         context_instance = RequestContext(request)
     )
 
+
+def edit(request, cid, template="management/category/edit.html"):
+
+    try:
+        category = Category.objects.get(pk = cid)
+    except Category.DoesNotExist:
+        raise Http404
+
+    if request.method == "POST":
+        _forms = EditCategoryForm(category=category, data=request.POST)
+        if _forms.is_valid():
+            _forms.save()
+            return 
+    else:
+        _forms = EditCategoryForm(
+            category=category,
+            initial={
+                'title': category.title,
+                'status': category.status,
+            }
+        )
+
+    return render_to_response(
+        template,
+        {
+            'forms':_forms,
+        },
+        context_instance = RequestContext(request)
+    )
 
 __author__ = 'edison'
