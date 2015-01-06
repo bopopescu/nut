41c6fa3684a349b3454140b90a46531931337174
edison7500 <edison7500@gmail.com>
2015-01-06 21:55:48 +0800

    update

--- nut/apps/management/urls/category.py
+++ nut/apps/management/urls/category.py
@@ -4,10 +4,11 @@ urlpatterns = patterns(
     'apps.management.views.category',
     # url(r'^(?P<category_id>\d+)/$', 'list', name='management_category_list'),
     url(r'^$', 'list', name='management_category_list'),
-    url(r'^(?P<cid>\d+)/sub/category/', 'sub_category_list', name='management_sub_category_list'),
     url(r'^create/$', 'create', name='management_category_create'),
     url(r'^(?P<cid>\d+)/edit/$', 'edit', name='management_category_edit'),
 
+    url(r'^(?P<cid>\d+)/sub/category/$', 'sub_category_list', name='management_sub_category_list'),
+    url(r'^(?P<scid>\d+)/sub/category/edit/$', 'sub_category_edit', name='management_sub_category_edit')
 )
 
 __author__ = 'edison'
