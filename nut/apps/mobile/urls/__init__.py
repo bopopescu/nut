5b281136096004adb49fbec59838eceb7a01c58a
谢 家欣 <edison7500@gmail.com>
2015-01-07 16:56:50 +0800

    update

--- nut/apps/mobile/urls/__init__.py
+++ nut/apps/mobile/urls/__init__.py
@@ -1,1 +1,10 @@
+from django.conf.urls import url, patterns, include
+
+
+urlpatterns = patterns(
+    'apps.mobile.views',
+    url(r'^homepage/$', 'homepage', name='mobile_homepage')
+)
+
+
 __author__ = 'edison7500'
