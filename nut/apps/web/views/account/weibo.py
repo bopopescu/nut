0a82d9790a52bcdc538d44d5167236d25d248173
edison7500 <edison7500@gmail.com>
2015-01-08 23:32:31 +0800

    update

--- nut/apps/web/views/account/weibo.py
+++ nut/apps/web/views/account/weibo.py
@@ -16,9 +16,12 @@ log = getLogger('django')
 def login_by_sina(request):
     request.session['auth_source'] = "login"
     # next_url = request.GET.get('next', None)
-    next_url = request.META.get('HTTP_REFERER')
+    next_url = request.META.get('HTTP_REFERER', reverse('web_selection'))
+    if 'login' in next_url:
+        next_url = reverse('web_selection')
+    log.info(next_url)
     if next_url:
-        request.session['auth_next_url'] = next_url
+        request.session['next_url'] = next_url
     return HttpResponseRedirect(sina.get_login_url())
 
 
@@ -28,21 +31,36 @@ def auth_by_sina(request):
     error_uri = request.GET.get('error_uri', None)
     if code:
         _sina_data = sina.get_auth_data(code)
-        next_url = request.session.get('auth_next_url', reverse("web_selection"))
+        next_url = request.session.get('next_url', reverse("web_selection"))
+        del request.session['next_url']
 
         try:
             weibo = Sina_Token.objects.get(sina_id = _sina_data['sina_id'])
         except Sina_Token.DoesNotExist:
             raise
 
-        login_without_password(request, weibo.user)
-        return HttpResponseRedirect(next_url)
+        log.info(_sina_data)
+
+        is_bind = request.session.get('is_bind', None)
+        if request.user.is_authenticated() and is_bind:
+            del request.session['next_url']
+            del request.session['is_bind']
+            Sina_Token.objects.create(
+                user = request.user,
+                sina_id =  _sina_data['sina_id'],
+            )
+            return HttpResponseRedirect(next_url)
+
+        if weibo.user_id:
+            login_without_password(request, weibo.user)
+
+            return HttpResponseRedirect(next_url)
 
 
 @require_GET
 @login_required
 def bind(request):
-    next_url = request.META.get('HTTP_REFERER', None)
+    next_url = request.META.get('HTTP_REFERER', reverse('web_selection'))
     if next_url:
         log.info(next_url)
         request.session['next_url'] = next_url
@@ -56,8 +74,8 @@ def bind(request):
 @login_required
 def unbind(request):
     next_url = request.META.get('HTTP_REFERER', None)
-    if next_url is None:
-        raise Http404
+    # if next_url is None:
+    #     raise Http404
 
     try:
         token = Sina_Token.objects.get(user=request.user)
