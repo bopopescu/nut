7d8448094e77e3a8635806d826ba65b249cde8cf
edison7500 <edison7500@gmail.com>
2015-01-08 20:24:58 +0800

    update

--- nut/apps/web/lib/account/utils.py
+++ nut/apps/web/lib/account/utils.py
@@ -6,7 +6,7 @@ from django.contrib.auth import login as auth_login
 def login_without_password(request, user):
     _user = user
     backend = get_backends()[0]
-    _user.backend = "%s.%s" % (backend.__module__, backend.__class__.__name___)
+    _user.backend = "%s.%s" % (backend.__module__, backend.__class__.__name__)
     auth_login(request, user)
 
 
