12be1083a723f48239f16e051cb3cdcad0c9557f
edison7500 <edison7500@gmail.com>
2015-01-08 21:01:04 +0800

    update

--- nut/apps/notifications/templatetags/notifications_tags.py
+++ nut/apps/notifications/templatetags/notifications_tags.py
@@ -5,12 +5,17 @@ from django.template import Node
 
 register = Library()
 
+
+from django.utils.log import getLogger
+log = getLogger('django')
+
 @register.assignment_tag(takes_context=True)
 def notifications_unread(context):
     if 'user' not in context:
         return ''
-    
+
     user = context['user']
+    # login.info(user)
     if user.is_anonymous():
         return ''
     return user.notifications.unread().count()
