02353228a35b8385f55fe923cdcfdf0778b09e1e
edison7500 <edison7500@gmail.com>
2015-01-08 20:21:04 +0800

    update

---
+++ nut/apps/web/lib/account/utils.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import get_backends, authenticate
+from django.contrib.auth import login as auth_login
+# from django.contrib.auth import authenticate
+
+
+def login_without_password(request, user):
+    _user = user
+    backend = get_backends()[0]
+    _user.backend = "%s.%s" % (backend.__module__, backend.__class__.__name___)
+    auth_login(request, user)
+
+
+__author__ = 'edison'
