bedfd62a89f4fd63d8cfbfb0364bd501abfd8718
edison7500 <edison7500@gmail.com>
2015-01-10 19:47:39 +0800

    update

--- nut/apps/web/urls/event.py
+++ nut/apps/web/urls/event.py
@@ -4,7 +4,7 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns(
     'apps.web.views.event',
     url(r'^$', 'home', name='web_event_home'),
-    # url(r'^(?P<slug>\d+)/$', 'event', name='web_event'),
+    url(r'^(?P<slug>\d+)/$', 'event', name='web_event'),
 )
 
 __author__ = 'edison'
