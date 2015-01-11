8b9ff97d903214e36b021107ea5b22dac6eac674
edison7500 <edison7500@gmail.com>
2015-01-11 11:09:09 +0800

    update

--- nut/apps/web/views/event.py
+++ nut/apps/web/views/event.py
@@ -14,15 +14,7 @@ from apps.core.models import Tag, Entity, Entity_Like, Entity_Tag
 from apps.core.utils.http import JSONResponse
 from datetime import datetime
 
-# from base.models import NoteSelection, Show_Event_Banner, Show_Editor_Recommendation, Event
-# from base.note import Note
-# from base.entity import Entity
-# from base.tag import Tag
-# from base.user import User
-# from base.extend.paginator import ExtentPaginator, PageNotAnInteger, EmptyPage
-# from web.forms.account import SignInAccountForm
-# # from utils.paginator import Paginator
-# from utils.http import JSONResponse
+
 
 from django.utils.log import getLogger
 log = getLogger('django')
@@ -46,89 +38,34 @@ def event(request, slug, template='web/events/home'):
     except Event.DoesNotExist:
         raise Http404
 
-    if request.user.is_authenticated():
-        # _request_user_id = request.user.id
-        _request_user_context = request.user
-        _request_user_like_entity_set = Entity.like_set_of_user(request.user.id)
-    else:
-        # _request_user_id = None
-        _request_user_context = None
-        _request_user_like_entity_set = []
-
-    # log.info(event.tag)
     tag = Tag.objects.get(tag_hash=event.tag)
     _entity_id_list = Entity_Tag.objects.filter(tag=tag).values_list('entity_id', flat=True)
     # log.info(_entity_id_list)
     # _entity_id_list = Tag.find_tag_entity(event.tag) # 双十一标签 hash
     # log.info(_entity_id_list)
-    entities = Entity.objects.filter(id__in=_entity_id_list, status=Entity.selection)
+    _entity_list = Entity.objects.filter(id__in=_entity_id_list, status=Entity.selection)
     # log.info(entities)
     # _page_num = request.GET.get('p', 1)
     # _paginator = Paginator(_page_num, 24, len(_entity_id_list))
 
-    _paginator = ExtentPaginator(entities, 30)
+    _paginator = ExtentPaginator(_entity_list, 30)
 
     try:
-        _entity_list = _paginator.page(_page_num)
+        _entities = _paginator.page(_page_num)
     except PageNotAnInteger:
-        _entity_list = _paginator.page(1)
+        _entities = _paginator.page(1)
     except EmptyPage:
         raise Http404
-    # _entities = []
-    # for _entity_id in _entity_id_list[_paginator.offset : _paginator.offset + _paginator.count_in_one_page]:
-    #     try:
-    #         _entity_context = Entity(_entity_id).read()
-    #         _entity_context['is_user_already_like'] = True if _entity_id in _request_user_like_entity_set else False
-    #         _entities.append(_entity_context)
-    #     except Exception, e:
-    #         log.error(e.message)
-    #
-    #
-    # log.info(_entities)
-    # _page_num = request.GET.get('p', 1)
-    # _time_filter  = request.GET.get('t', datetime.now())
-    # _hdl = NoteSelection.objects.filter(post_time__lt = _time_filter, entity_id__in=_entity_id_list)
-    # _hdl.order_by('-post_time')
 
-    # _paginator = ExtentPaginator(_hdl, 30)
-    # _paginator = ExtentPaginator(_page_num, 30, _hdl)
-    # _note_selection_list = _hdl[_paginator.offset : _paginator.offset + _paginator.count_in_one_page]
-    # try:
-    #     _note_selection_list = _paginator.page(_page_num)
-    # except PageNotAnInteger:
-    #     _note_selection_list = _paginator.page(1)
-    # except Exception, e:
-    #     log.error("Error: %s" % e.message)
-    #     raise Http404
 
-    # log.info(_note_selection_list)
-    # _selection_list = []
-    # _entity_id_list = []
-    # for _note_selection in _note_selection_list:
-    #     try:
-    #         _selection_note_id = _note_selection['note_id']
-    #         _entity_id = _note_selection['entity_id']
-    #         _entity_context = Entity(_entity_id).read()
-    #         _note = Note(_selection_note_id)
-    #         _note_context = _note.read()
-    #         _creator_context = User(_note_context['creator_id']).read()
-    #         _is_user_already_like = True if _entity_id in _request_user_like_entity_set else False
-    #         _selection_list.append(
-    #             {
-    #                 'is_user_already_like': _is_user_already_like,
-    #                 'entity_context': _entity_context,
-    #                 'note_context': _note_context,
-    #                 'creator_context': _creator_context,
-    #             }
-    #         )
-    #         _entity_id_list.append(_entity_id)
-    #     except Exception, e:
-    #         log.error(e.message)
+    el = list()
+    if request.user.is_authenticated():
+        e = _entities.object_list
+        el = Entity_Like.objects.filter(entity_id__in=list(e), user=request.user).values_list('entity_id', flat=True)
+        # el =
 
-    # log.info(_selection_list)
 
     _show_event_banners = Show_Event_Banner.objects.filter(event=event, position__gt=0)
-
     _show_editor_recommendations = Show_Editor_Recommendation.objects.filter(event=event, position__gt=0)
 
     if request.is_ajax():
@@ -140,21 +77,19 @@ def event(request, slug, template='web/events/home'):
         if _entity_list:
             _t = loader.get_template('web/main/partial/selection_item_list.html')
             _c = RequestContext(request, {
-                'entities': _entity_list,
+                'user_entity_likes': el,
+                'entities': _entities,
             })
             _data = _t.render(_c)
 
-            # _ret = {
-            #     'status' : '1',
-            #     'data' : _data
-            # }
-            return JSONResponse(
-                    data={
-                        'status' : '1',
-                    'data' : _data
-                },
-                    content_type='text/html; charset=utf-8',
-            )
+            _ret = {
+                'status' : '1',
+                'data' : _data
+            }
+        return JSONResponse(
+                data=_ret,
+                content_type='text/html; charset=utf-8',
+        )
 
     log.info('tag text %s', event.tag)
     return render_to_response(
@@ -164,15 +99,8 @@ def event(request, slug, template='web/events/home'):
             'tag_text': tag.tag,
             'show_event_banners': _show_event_banners,
             'show_editor_recommendations': _show_editor_recommendations,
-            # 'paginator': _paginator,
-            # 'page_num' : _page_num,
-                # 'curr_category_id' : _category_id,
-            'user_context': _request_user_context,
-            'entity_list': _entity_list,
-                # 'category_list' : _old_category_list,
-            # 'selection_list' : _selection_list,
-            # "entities": _entities,
-            # "paginator" : _paginator
+            'entities': _entities,
+            'user_entity_likes': el,
         },
         context_instance=RequestContext(request)
     )
