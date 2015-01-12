477e7044d2100f1b4bcc69283564b512abf689fc
edison7500 <edison7500@gmail.com>
2015-01-12 20:26:30 +0800

    update

---
+++ nut/apps/mobile/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from apps.core.models import GKUser
+
+class Apps(models.Model):
+    user = models.ForeignKey(GKUser)
+    app_name = models.CharField(u'应用名称',max_length=30, unique=True)
+    desc = models.TextField()
+    api_key = models.CharField(max_length=64)
+    api_secret = models.CharField(max_length=32)
+    created_time = models.DateTimeField(auto_now=True)
+    objects = AppsManager()
+    class Meta:
+        ordering = ['-created_time']
+
+    def __unicode__(self):
+        # app_label = 'mobile'
+        return self.app_name
+
+
+class Session_Key(models.Model):
+    user = models.ForeignKey(GKUser, related_name = "mobile_client_session")
+    app = models.ForeignKey(Apps)
+    session_key = models.CharField(max_length = 64, unique = True, editable = False)
+    create_time = models.DateTimeField(auto_now_add = True)
+    objects = SessionKeyManager()
+
+    def __unicode__(self):
+        return self.session_key
+
+
+__author__ = 'edison'
