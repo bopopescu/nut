2a41a947df2c3aea392c8101fdb1728d639daab5
edison7500 <edison7500@gmail.com>
2015-01-12 13:14:10 +0800

    update

--- nut/apps/web/urls/user.py
+++ nut/apps/web/urls/user.py
@@ -9,6 +9,7 @@ urlpatterns = patterns(
     url(r'^(?P<user_id>\d+)/$', 'index', name='web_user_index' ),
     url(r'^(?P<user_id>\d+)/like/$', 'entity_like', name='web_user_entity_like'),
     url(r'^(?P<user_id>\d+)/note/$', 'post_note', name='web_user_post_note'),
+    url(r'^(?P<user_id>\d+)/tags/$', 'tag', name='web_user_tag'),
     url(r'^(?P<user_id>\d+)/fans/$', 'fans', name='web_user_fans'),
     url(r'^(?P<user_id>\d+)/followings/$', 'following', name='web_user_followings'),
 )
