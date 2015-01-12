e4cf7cf55d00a55446978a374b838da3122fe81b
edison7500 <edison7500@gmail.com>
2015-01-12 16:02:05 +0800

    update

--- nut/apps/core/manager/tag.py
+++ nut/apps/core/manager/tag.py
@@ -1,6 +1,5 @@
 from django.db import models, connection
 from django.db.models import Count
-
 from django.core.cache import cache
 
 from django.utils.log import getLogger
@@ -24,7 +23,7 @@ class EntityTagManager(models.Manager):
 
     def user_tags(self, user):
         c = connection.cursor()
-        sql = "SELECT tag_id, core_tag.tag, count(tag_id) as tcount \
+        sql = "SELECT tag_id, core_tag.tag, core_tag.tag_hash, count(tag_id) as tcount \
                   from core_entity_tag join core_tag on tag_id = core_tag.id \
                    where user_id=%s group by tag_id ORDER BY tcount DESC" % user
 
@@ -34,9 +33,8 @@ class EntityTagManager(models.Manager):
         #     c.execute(sql)
         # finally:
         #     c.close()
-        return dictfetchall(c)
-        # return self.raw("SELECT tag_id, core_tag.tag, count(tag_id) as tcount from core_entity_tag join core_tag on tag_id = core_tag.id where user_id =1 group by tag_id", translations=name_map)
-        # return self.get_queryset().user_tags(user)
+        res = dictfetchall(c)
+        return res
 
 
 __author__ = 'edison'
