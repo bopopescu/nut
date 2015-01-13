d0f88787b8712060c670526b79a3143b788664d0
edison7500 <edison7500@gmail.com>
2015-01-13 22:45:31 +0800

    update

--- nut/apps/mobile/urls/category.py
+++ nut/apps/mobile/urls/category.py
@@ -4,6 +4,7 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns(
     'apps.mobile.views.category',
     url(r'^$', 'list', name='mobile_category_list'),
+    url(r'^(?P<category_id>\d+)/stat/', 'category_stat', name='mobile_category_stat'),
 )
 
 __author__ = 'edison'
--- nut/apps/mobile/views/entity.py
+++ nut/apps/mobile/views/entity.py
@@ -1,10 +1,24 @@
 from apps.core.utils.http import SuccessJsonResponse
 from apps.mobile.lib.sign import check_sign
+from datetime import datetime
 
 
 @check_sign
 def list(request):
 
+    _timestamp = request.GET.get('timestamp', None)
+    if _timestamp != None:
+        _timestamp = datetime.fromtimestamp(float(_timestamp))
+
+    _sort_by = request.GET.get('sort', 'novus_time')
+    _reverse = request.GET.get('reverse', '0')
+    if _reverse == '0':
+        _reverse = False
+    else:
+        _reverse = True
+
+    _offset = int(request.GET.get('offset', '0'))
+    _count = int(request.GET.get('count', '30'))
 
 
     return SuccessJsonResponse()
