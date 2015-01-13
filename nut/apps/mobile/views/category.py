5bb2f8729e63132133d657bc6471281acf94bcfd
edison7500 <edison7500@gmail.com>
2015-01-13 14:55:09 +0800

    update

--- nut/apps/mobile/views/category.py
+++ nut/apps/mobile/views/category.py
@@ -1,11 +1,24 @@
 from apps.core.utils.http import SuccessJsonResponse
 
+from apps.core.models import Category, Sub_Category
 
 
 def list(request):
 
+    categories = Category.objects.all()
+    res = []
+    for category in categories:
+        res.append(
+            {
+                'group_id' : category.id,
+                'title' : category.title,
+                'status' : category.status,
+                'category_count': category.sub_category_count,
+            }
+        )
 
-    return SuccessJsonResponse()
+
+    return SuccessJsonResponse(res)
 
 
 __author__ = 'edison'
