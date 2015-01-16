22abc4e968773e5a2175e028860f09eef5773444
谢 家欣 <edison7500@gmail.com>
2015-01-16 12:08:12 +0800

    update

--- nut/apps/management/views/selection.py
+++ nut/apps/management/views/selection.py
@@ -14,7 +14,7 @@ def selection_list(request, template='management/selection/list.html'):
     _page = request.GET.get('page', 1)
 
     s = Selection_Entity.objects.all().values_list('entity_id', flat=True)
-    log.info(s.query)
+    # log.info(s.query)
     paginator = ExtentPaginator(s, 30)
 
     try:
@@ -24,7 +24,7 @@ def selection_list(request, template='management/selection/list.html'):
     except EmptyPage:
         raise Http404
 
-    log.info(selections.object_list)
+    # log.info(selections.object_list)
     # innqs = selections.object_list
     _entities = Entity.objects.filter(id__in=list(selections.object_list))
 
