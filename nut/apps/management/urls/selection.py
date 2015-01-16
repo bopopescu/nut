b16c5c10a78157158a9ce7dbaf3abf8917cc07d7
谢 家欣 <edison7500@gmail.com>
2015-01-16 14:54:37 +0800

    update

--- nut/apps/management/urls/selection.py
+++ nut/apps/management/urls/selection.py
@@ -4,6 +4,7 @@ urlpatterns = patterns(
     'apps.management.views.selection',
 
     url(r'^$', 'selection_list', name='management_selection_list'),
+    url(r'^pending/$', 'pending', name='management_selection_pending'),
     url(r'^publish/(?P<sid>\d+)/edit/$', 'edit_publish', name='management_selection_edit_publish'),
 )
 
