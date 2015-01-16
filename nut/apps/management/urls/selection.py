af633b8d3206756d9f75d7510c33699ec8827fff
谢 家欣 <edison7500@gmail.com>
2015-01-16 10:04:08 +0800

    update

--- nut/apps/management/urls/selection.py
+++ nut/apps/management/urls/selection.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, patterns
 
 urlpatterns = patterns(
     'apps.management.views.selection',
+    url(r'^$', 'list', name='management_selection_list'),
 )
 
 __author__ = 'edison7500'
--- nut/apps/management/views/selection.py
+++ nut/apps/management/views/selection.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from apps.core.models import Selection_Entity
 
+
+
+def list(request, template='management/selection/list.html'):
+
+
+
+    return render_to_response(
+        template,
+        {
+
+        },
+        context_instance = RequestContext(request)
+    )
 
 
 __author__ = 'edison7500'
