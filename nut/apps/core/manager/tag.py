ce25b9105a17a8a5893ec209f3753ba72ecf4379
谢 家欣 <edison7500@gmail.com>
2015-01-18 11:48:17 +0800

    update

--- nut/apps/core/manager/tag.py
+++ nut/apps/core/manager/tag.py
@@ -37,4 +37,20 @@ class EntityTagManager(models.Manager):
         return res
 
 
+    def v3_user_tags(self, user):
+        c = connection.cursor()
+        sql = "SELECT tag_id, core_tag.tag, core_tag.tag_hash, count(tag_id) as entity_count \
+                  from core_entity_tag join core_tag on tag_id = core_tag.id \
+                   where user_id=%s group by tag_id ORDER BY entity_count DESC" % user
+
+        # log.info(sql)
+        c.execute(sql)
+        # try:
+        #     c.execute(sql)
+        # finally:
+        #     c.close()
+        res = dictfetchall(c)
+        return res
+
+
 __author__ = 'edison'
