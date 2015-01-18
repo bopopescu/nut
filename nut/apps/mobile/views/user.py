e6ece8bccd1a5fcd4e91459678cd49bc07f7e6c5
谢 家欣 <edison7500@gmail.com>
2015-01-18 11:29:17 +0800

    update

--- nut/apps/mobile/views/user.py
+++ nut/apps/mobile/views/user.py
@@ -1,11 +1,36 @@
 from apps.core.utils.http import SuccessJsonResponse, ErrorJsonResponse
 from apps.mobile.lib.sign import check_sign
-from apps.core.models import Entity_Tag, GKUser
+from apps.core.models import Entity_Tag, GKUser, Entity_Like, Note
+from apps.core.extend.paginator import ExtentPaginator, EmptyPage, PageNotAnInteger
+
 from django.utils.log import getLogger
+from datetime import datetime
+
 
 
 log = getLogger('django')
 
+@check_sign
+def detail(request, user_id):
+    try:
+        _user = GKUser.objects.get(pk=user_id)
+    except GKUser.DoesNotExist:
+        raise ErrorJsonResponse(status=404)
+
+    _last_like = Entity_Like.objects.filter(user=_user).last()
+    _last_note = Note.objects.filter(user=_user).last()
+    # log.info(last_like)
+    # log.info(last_note)
+
+
+    res = dict()
+    res['user'] = _user.v3_toDict()
+    if _last_like:
+        res['last_like'] = _last_like.entity.v3_toDict()
+    if _last_note:
+        res['last_note'] = _last_note.v3_toDict()
+
+    return SuccessJsonResponse(res)
 
 @check_sign
 def tag_detail(request, user_id, tag):
@@ -37,6 +62,46 @@ def entity_like(request, user_id):
 
 
 
-    return 
+    return
+
+@check_sign
+def entity_note(request, user_id):
+    try:
+        _user = GKUser.objects.get(pk=user_id)
+    except GKUser.DoesNotExist:
+        return ErrorJsonResponse(status=404)
+
+    _offset = int(request.GET.get('offset', '0'))
+    _count = int(request.GET.get('count', '30'))
+
+    _offset = _offset / _count + 1
+
+    _timestamp = request.GET.get('timestamp', None)
+    if _timestamp != None:
+        _timestamp = datetime.fromtimestamp(float(_timestamp))
+
+
+
+    note_list = Note.objects.filter(user=_user)
+
+    paginator = ExtentPaginator(note_list, _count)
+    try:
+        notes = paginator.page(_offset)
+    except PageNotAnInteger:
+        notes = paginator.page(1)
+    except EmptyPage:
+        return ErrorJsonResponse(status=404)
+
+
+    res = []
+
+    for note in notes.object_list:
+        log.info(note)
+        res.append({
+            'note':note.v3_toDict(),
+            'entity':note.entity.v3_toDict(),
+        })
+
+    return SuccessJsonResponse(res)
 
 __author__ = 'edison7500'
