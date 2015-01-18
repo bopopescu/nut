0c4cf000c5aeb7a17b543ec0dbe9f0faf124f36b
谢 家欣 <edison7500@gmail.com>
2015-01-18 10:49:38 +0800

    update

--- nut/apps/mobile/urls/user.py
+++ nut/apps/mobile/urls/user.py
@@ -4,6 +4,7 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns(
     'apps.mobile.views.user',
     url(r'^(?P<user_id>\d+)/tag/(?P<tag>\w+)/$', 'tag_detail', name='mobile_user_tag_detail'),
+    url(r'^user/(?P<user_id>\d+)/like/$', 'entity_like', name='mobile_user_entity_like'),
 )
 
 
