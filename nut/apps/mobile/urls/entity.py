e909d835d712aa8b820b17f544d4fe9a1e43d717
谢 家欣 <edison7500@gmail.com>
2015-01-19 15:48:08 +0800

    update

--- nut/apps/mobile/urls/entity.py
+++ nut/apps/mobile/urls/entity.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, patterns
 
 urlpatterns = patterns(
     'apps.mobile.views.entity',
-    url(r'^$', 'list', name='mobile_entity_list'),
+    url(r'^$', 'entity_list', name='mobile_entity_list'),
     url(r'^guess/$', 'guess', name='mobile_entity_guess'),
     url(r'^(?P<entity_id>\d+)/$', 'detail', name='mobile_entity_detail'),
     url(r'^(?P<entity_id>\d+)/like/(?P<target_status>\d+)/$', 'like_action', name='mobile_entity_like_action'),
