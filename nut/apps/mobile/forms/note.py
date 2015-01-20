5578bb629717636a245d03245e3f9c7f6ee922fe
edison7500 <edison7500@gmail.com>
2015-01-20 21:37:18 +0800

    update

--- nut/apps/mobile/forms/note.py
+++ nut/apps/mobile/forms/note.py
@@ -1,3 +1,4 @@
+# coding=utf8
 from django import forms
 from apps.core.models import Note
 from apps.mobile.models import Session_Key
@@ -5,6 +6,7 @@ from apps.mobile.models import Session_Key
 from django.utils.log import getLogger
 
 log = getLogger('django')
+
 
 
 class PostNoteForms(forms.Form):
@@ -35,6 +37,7 @@ class PostNoteForms(forms.Form):
         _user_id = self.cleaned_data.get('session')
 
         # log.info("user id %s" % _user_id)
+        _note_text = _note_text.replace(u"＃", "#")
 
         note = Note(
             user_id = _user_id,
@@ -47,9 +50,7 @@ class PostNoteForms(forms.Form):
 
 
 class UpdateNoteForms(forms.Form):
-    # session = forms.CharField(
-    #     widget=forms.TextInput()
-    # )
+
     note = forms.CharField(
         widget=forms.TextInput()
     )
@@ -58,18 +59,10 @@ class UpdateNoteForms(forms.Form):
         self.note_cache = note
         super(UpdateNoteForms, self).__init__(*args, **kwargs)
 
-    # def clean_session(self):
-    #     _key = self.cleaned_data.get('session')
-    #     try:
-    #         _session = Session_Key.objects.get(session_key=_key)
-    #     except Session_Key.DoesNotExist:
-    #         raise forms.ValidationError(
-    #             'user not login'
-    #         )
-    #     return _session.user_id
-
     def update(self):
         _note_text = self.cleaned_data.get('note')
+        _note_text = _note_text.replace(u"＃", "#")
+
         # _user_id = self.cleaned_data.get('session')
         self.note_cache.note = _note_text
         self.note_cache.save()
