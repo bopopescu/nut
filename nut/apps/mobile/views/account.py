b29758132ea93b7feec5145907cc320e180bef6d
edison7500 <edison7500@gmail.com>
2015-01-22 00:07:41 +0800

    finished three part login

--- nut/apps/mobile/views/account.py
+++ nut/apps/mobile/views/account.py
@@ -1,6 +1,7 @@
 from apps.core.utils.http import SuccessJsonResponse, ErrorJsonResponse
-from apps.mobile.forms.account import MobileUserSignInForm, MobileUserSignUpForm, MobileUserSignOutForm
+from apps.mobile.forms.account import MobileUserSignInForm, MobileUserSignUpForm, MobileUserSignOutForm, MobileWeiboLoginForm, MobileTaobaoLogin
 from apps.mobile.lib.sign import check_sign
+
 # from apps.mobile.models import Session_Key
 
 from django.views.decorators.csrf import csrf_exempt
@@ -62,6 +63,7 @@ def register(request):
 
     return ErrorJsonResponse(status=400)
 
+
 @csrf_exempt
 @check_sign
 def logout(request):
@@ -74,6 +76,33 @@ def logout(request):
 
     return ErrorJsonResponse(status=400)
 
+
+@csrf_exempt
+@check_sign
+def login_by_weibo(request):
+    if request.method == "POST":
+        _forms = MobileWeiboLoginForm(request.POST)
+        if _forms.is_valid():
+            res = _forms.login()
+            return SuccessJsonResponse(res)
+        return ErrorJsonResponse(status=409, data={
+            'type':'sina_id',
+        })
+    return ErrorJsonResponse(status=400)
+
+
+@csrf_exempt
+@check_sign
+def login_by_taobao(request):
+    if request.method == "POST":
+        _forms = MobileTaobaoLogin(request.POST)
+        if _forms.is_valid():
+            res = _forms.login()
+            return SuccessJsonResponse(res)
+        return ErrorJsonResponse(status=409, data={
+            'type':'taobao_id',
+        })
+    return ErrorJsonResponse(status=400)
 
 
 @csrf_exempt
