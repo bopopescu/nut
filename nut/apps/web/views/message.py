4031e96abf55bd1acb36f41e14b9c4d4fb9a4a47
edison7500 <edison7500@gmail.com>
2015-01-23 14:29:25 +0800

    update

--- nut/apps/web/views/message.py
+++ nut/apps/web/views/message.py
@@ -3,7 +3,11 @@ from django.shortcuts import render_to_response
 from django.views.decorators.http import require_GET
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
+from django.template import loader
+
 from apps.core.extend.paginator import ExtentPaginator, PageNotAnInteger, EmptyPage
+from apps.core.utils.http import JSONResponse
+from datetime import datetime
 
 
 @require_GET
@@ -11,8 +15,12 @@ from apps.core.extend.paginator import ExtentPaginator, PageNotAnInteger, EmptyP
 def messages(request, template='web/messages/message.html'):
     _user = request.user
     _page = request.GET.get('page', 1)
-
-    message_list = _user.notifications.all()
+    _timestamp = request.GET.get('timestamp',None)
+    if _timestamp != None:
+        _timestamp = datetime.fromtimestamp(float(_timestamp))
+    else:
+        _timestamp = datetime.now()
+    message_list = _user.notifications.filter(timestamp__lt=_timestamp)
 
     paginator = ExtentPaginator(message_list, 10)
 
@@ -22,6 +30,24 @@ def messages(request, template='web/messages/message.html'):
         _messages = paginator.page(1)
     except EmptyPage:
         raise Http404
+
+
+    if request.is_ajax():
+        res = {
+            'status' : 0,
+            'msg' : 'no data'
+        }
+        if len(res) > 0:
+            _t = loader.get_template('web/messages/partial/ajax_notification.html')
+            _c = RequestContext(request, {
+                    'messages': _messages,
+            })
+            _data = _t.render(_c)
+            res = {
+                'status': 1,
+                'data': _data,
+            }
+            return JSONResponse(data=res, content_type='text/html; charset=utf-8',)
 
     return render_to_response(
         template,
