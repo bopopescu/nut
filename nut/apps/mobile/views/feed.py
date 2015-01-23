fe15428cd5403c1149447d4c40bce26dbad6e61f
edison7500 <edison7500@gmail.com>
2015-01-23 13:08:09 +0800

    update

--- nut/apps/mobile/views/feed.py
+++ nut/apps/mobile/views/feed.py
@@ -1,10 +1,13 @@
 from apps.core.utils.http import SuccessJsonResponse, ErrorJsonResponse
 from apps.core.models import Note
+from apps.core.extend.paginator import ExtentPaginator, PageNotAnInteger, EmptyPage
 from apps.mobile.lib.sign import check_sign
 from apps.mobile.models import Session_Key
 from apps.notifications.models import Notification
 
+
 from django.contrib.contenttypes.models import ContentType
+from datetime import datetime
 from django.utils.log import getLogger
 
 
@@ -14,9 +17,15 @@ log = getLogger('django')
 @check_sign
 def activity(request):
 
-    # log.info(request.GET)
-
+    log.info(request.GET)
+    _timestamp = request.GET.get('timestamp', None)
+    if _timestamp != None:
+        _timestamp = datetime.fromtimestamp(float(_timestamp))
+    _offset = int(request.GET.get('offset', '0'))
+    _count = int(request.GET.get('count', '30'))
     _key = request.GET.get('session', None)
+
+    _offset = _offset / _count + 1
 
     try:
         _session = Session_Key.objects.get(session_key = _key)
@@ -26,12 +35,20 @@ def activity(request):
     note_model = ContentType.objects.get_for_model(Note)
     # log.info(type(note_model))
 
-    feeds = Notification.objects.filter(actor_object_id__in=_session.user.following_list, action_object_content_type=note_model)
+    feed_list = Notification.objects.filter(actor_object_id__in=_session.user.following_list, action_object_content_type=note_model, timestamp__lte=_timestamp)
+
+    paginator = ExtentPaginator(feed_list, _count)
+    try:
+        feeds = paginator.page(_offset)
+    except PageNotAnInteger:
+        feeds = paginator.page(1)
+    except EmptyPage:
+        return ErrorJsonResponse(status=404)
 
     # log.info(feeds)
 
     res = []
-    for row in feeds:
+    for row in feeds.object_list:
         res.append(
             {
                 'type': 'entity',
