19b6c7fb3adb8e425c2f9a2cea27e2ac11ca93b0
edison7500 <edison7500@gmail.com>
2015-01-24 14:32:18 +0800

    update

--- nut/apps/management/urls/tags.py
+++ nut/apps/management/urls/tags.py
@@ -3,6 +3,7 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns(
     'apps.management.views.tags',
     url(r'^$', 'list', name='management_tag_list'),
+    url(r'^(?P<tag_id>\d+)/edit/$', 'edit', name='management_tag_edit'),
 )
 
 __author__ = 'edison'
