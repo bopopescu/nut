7f0870d88268ee83c4c74fb23bec87a2e056b42d
edison7500 <edison7500@gmail.com>
2015-01-25 16:24:31 +0800

    update

--- nut/apps/management/decorators.py
+++ nut/apps/management/decorators.py
@@ -11,11 +11,11 @@ def staff_only(func=None):
 
 
 def admin_only(func=None):
-    def staff_wrapped(request, *args, **kwargs):
+    def admin_wrapped(request, *args, **kwargs):
         if not request.user.is_staff:
             raise Http404
         return func(request, *args, **kwargs)
-    return staff_wrapped
+    return admin_wrapped
 
 
 __author__ = 'edison'
