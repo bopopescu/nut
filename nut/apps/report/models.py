8e41c87f5960f9fb5cdc3037180ae5ae8d4b563b
edison7500 <edison7500@gmail.com>
2015-01-27 21:07:35 +0800

    update

--- nut/apps/report/models.py
+++ nut/apps/report/models.py
@@ -4,6 +4,21 @@ from django.contrib.contenttypes.generic import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
 
+class BaseModel(models.Model):
+
+    class Meta:
+        abstract = True
+
+    def toDict(self):
+        fields = []
+        for f in  self._meta.fields:
+            fields.append(f.column)
+        d = {}
+        for attr in fields:
+            d[attr] = "%s" % getattr(self, attr)
+        return d
+
+
 class Report(models.Model):
     reporter = models.ForeignKey(settings.AUTH_USER_MODEL, blank=False, related_name='reporter')
     comment = models.TextField()
@@ -20,5 +35,11 @@ class Report(models.Model):
         return self.reporter
 
 
+class Selection(BaseModel):
+    selected_total = models.IntegerField(default=0)
+    pub_date = models.DateField(db_index=True, editable=False)
+
+    class Meta:
+        ordering = ['pub_date']
 
 __author__ = 'edison'
