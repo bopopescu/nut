e99c52b39e2ab5f4da82869e47dcf3ccc263f2e7
edison7500 <edison7500@gmail.com>
2015-01-27 21:10:02 +0800

    update

--- nut/script/report_selection.py
+++ nut/script/report_selection.py
@@ -12,29 +12,35 @@ day = timedelta(days=1)
 
 b = dt - day
 date_string = dt.strftime("%Y-%m-%d")
+selection = Selection_Entity.objects.raw("select id, count(*) as count from core_selection_entity where is_published = 1 and pub_time BETWEEN '%s' and '%s'" % (b.strftime("%Y-%m-%d"), date_string))
 
+s = Selection(
+    selected_total = selection[0].count,
+    pub_date = date_string,
+)
+s.save()
 
-print b.strftime("%Y-%m-%d")
-
-while True:
-
-    selection = Selection_Entity.objects.raw("select id, count(*) as count from core_selection_entity where is_published = 1 and pub_time BETWEEN '%s' and '%s'" % (b.strftime("%Y-%m-%d"), date_string))
-    if selection[0].count == 0:
-        break
-    s = Selection(
-        selected_total = selection[0].count,
-        pub_date = date_string,
-    )
-    s.save()
-    # print date_string
-    # print selection.query
-    # print selection[0].count
-    dt -= day
-    b -= day
-    date_string = dt.strftime("%Y-%m-%d")
-    print b.strftime("%Y-%m-%d"), date_string
-    # print b
-    if selection[0].count == 0:
-        break
+# print b.strftime("%Y-%m-%d")
+#
+# while True:
+#
+#     selection = Selection_Entity.objects.raw("select id, count(*) as count from core_selection_entity where is_published = 1 and pub_time BETWEEN '%s' and '%s'" % (b.strftime("%Y-%m-%d"), date_string))
+#     if selection[0].count == 0:
+#         break
+#     s = Selection(
+#         selected_total = selection[0].count,
+#         pub_date = date_string,
+#     )
+#     s.save()
+#     # print date_string
+#     # print selection.query
+#     # print selection[0].count
+#     dt -= day
+#     b -= day
+#     date_string = dt.strftime("%Y-%m-%d")
+#     print b.strftime("%Y-%m-%d"), date_string
+#     # print b
+#     if selection[0].count == 0:
+#         break
 
 __author__ = 'edison'
