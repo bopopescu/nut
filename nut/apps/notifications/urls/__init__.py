fb784ffdd9501ff4d493d39151c0309a941055db
谢 家欣 <edison7500@gmail.com>
2015-01-28 12:55:38 +0800

    update

--- nut/apps/notifications/urls/__init__.py
+++ nut/apps/notifications/urls/__init__.py
@@ -1,4 +1,9 @@
 from django.conf.urls import url, patterns, include
 
+urlpatterns = patterns(
+    'apps.notifications.views',
+    url(r'^$', 'messages', name='web_messages'),
+)
+
 
 __author__ = 'edison7500'
