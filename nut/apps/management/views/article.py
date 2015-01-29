5b6a969244dedfd1343abe5eaa8f94cf71503abc
edison7500 <edison7500@gmail.com>
2015-01-29 10:18:54 +0800

    update

--- nut/apps/management/views/article.py
+++ nut/apps/management/views/article.py
@@ -2,15 +2,20 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 from django.utils.log import getLogger
+
 
 from apps.core.forms.article import CreateArticleForms, EditArticleForms
 from apps.core.extend.paginator import ExtentPaginator, PageNotAnInteger, EmptyPage
 from apps.core.models import Article
+from apps.management.decorators import staff_only
 
 log = getLogger('django')
 
 
+@login_required
+@staff_only
 def list(request, template="management/article/list.html"):
 
     _page = request.GET.get('page', 1)
@@ -34,16 +39,18 @@ def list(request, template="management/article/list.html"):
     )
 
 
+@login_required
+@staff_only
 def create(request, template="management/article/create.html"):
 
     if request.method == "POST":
-        _forms = CreateArticleForms(request.POST, request.FILES)
+        _forms = CreateArticleForms(user=request.user, data=request.POST, files=request.FILES)
         if _forms.is_valid():
             article = _forms.save()
             # return HttpResponse("OK")
             return HttpResponseRedirect(reverse('management_article_edit', args=[article.pk]))
     else:
-        _forms = CreateArticleForms()
+        _forms = CreateArticleForms(user=request.user)
 
     return render_to_response(
         template,
@@ -54,6 +61,8 @@ def create(request, template="management/article/create.html"):
     )
 
 
+@login_required
+@staff_only
 def edit(request, article_id, template="management/article/edit.html"):
 
     try:
@@ -86,6 +95,8 @@ def edit(request, article_id, template="management/article/edit.html"):
     )
 
 
+@login_required
+@staff_only
 def preview(request, article_id, template="management/article/preview.html"):
 
     try:
