d1cbbd1c38d50be0dd5bd6208bf9f9ef6db67968
edison7500 <edison7500@gmail.com>
2015-01-31 22:42:03 +0800

    update

--- nut/apps/core/utils/taobaoapi/utils.py
+++ nut/apps/core/utils/taobaoapi/utils.py
@@ -1,8 +1,14 @@
 from django.conf import settings
+from django.core.cache import cache
 from item import TaobaoItem
 from shop import TaobaoShop
 from taobaoke import TaobaokeMobileItem
-import json
+from hashlib import md5
+# import json
+
+from django.utils.log import getLogger
+
+log = getLogger('django')
 
 APP_KEY = getattr(settings, "TAOBAO_APP_KEY", None)
 APP_SECRET = getattr(settings, "TAOBAO_APP_SECRET", None)
@@ -23,10 +29,22 @@ def load_taobao_shop_info_from_api(shop_nick):
     return shop.get_shop_info(shop_nick)
 
 def taobaoke_mobile_item_convert(num_iid, outer_code = "", fields = None):
+    key_string = 'taobaoke_%s' % num_iid
+    key = md5(key_string.encode('utf-8')).hexdigest()
+
+    url = cache.get(key)
+    if url:
+        return url
     request = TaobaokeMobileItem(APP_KEY, APP_SECRET)
     res = request.convert_items(num_iid, outer_code)
+    log.info(res)
+
     try:
-        return res['tbk_mobile_items_convert_response']['tbk_items']['tbk_item'][0]
-    except:
+        url = res['tbk_mobile_items_convert_response']['tbk_items']['tbk_item'][0]
+        cache.set(key, url, timeout=86400)
+
+    except KeyError, e:
+        log.error("Error: %s" % e.message)
         return None
-    
+
+    return url
