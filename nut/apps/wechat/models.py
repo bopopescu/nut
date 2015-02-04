9fd33096bbc80af7873ba24b8c751deb31589002
edison7500 <edison7500@gmail.com>
2015-02-05 00:03:09 +0800

    update

--- nut/apps/wechat/models.py
+++ nut/apps/wechat/models.py
@@ -1,4 +1,21 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
+
+class Robots(models.Model):
+    text, image, voice, video, music, news = range(6)
+    TYPE_CHOICES = (
+        (text, _('text')),
+        (image, _('image')),
+        (voice, _('voice')),
+        (video, _('video')),
+        (music, _('music')),
+        (news, _('news')),
+    )
+
+    token = models.CharField(max_length=64)
+    type = models.IntegerField(choices=TYPE_CHOICES, default=text)
+    content = models.TextField()
 
 
 
