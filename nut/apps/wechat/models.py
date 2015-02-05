12d7642daf30a611d42186dcd4d799a5adef1d89
谢 家欣 <edison7500@gmail.com>
2015-02-05 16:19:37 +0800

    update

--- nut/apps/wechat/models.py
+++ nut/apps/wechat/models.py
@@ -15,9 +15,10 @@ class Robots(models.Model):
     )
 
     # token = models.CharField(max_length=64)
-    accept = models.CharField(max_length=255)
+    accept = models.CharField(max_length=255, unique=True)
     type = models.IntegerField(choices=TYPE_CHOICES, default=text)
     content = models.TextField()
+    # created_datetime = models.da
 
 
 
