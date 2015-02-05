50fe4d69ca93348def9df81e73f1657f40844c04
谢 家欣 <edison7500@gmail.com>
2015-02-05 12:49:56 +0800

    update

---
+++ nut/apps/management/urls/wechat.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url, patterns
+
+urlpatterns = patterns(
+    'apps.management.views.wechat',
+    # url()
+)
+
+__author__ = 'edison7500'
---
+++ nut/apps/management/views/wechat.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render_to_response
+
+
+
+
+__author__ = 'edison7500'
