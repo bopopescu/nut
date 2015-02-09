79881f5da1e3af0fb562f0d1f437347745bae617
edison7500 <edison7500@gmail.com>
2015-02-09 22:19:20 +0800

    update

--- nut/apps/web/views/category.py
+++ nut/apps/web/views/category.py
@@ -22,83 +22,42 @@ class CategoryListView(ListView):
     context_object_name = "categories"
 
 
-# class CategroyGroupListView(TemplateResponseMixin, ContextMixin, View):
-#     http_method_names = ['get']
-#     template_name = 'web/category/detail.html'
-#
-#     def get(self, request, *args, **kwargs):
-#         # log.info(kwargs)
-#         gid = kwargs.pop('gid', None)
-#
-#         sub_categories = Sub_Category.objects.filter(group=gid).values_list('id', flat=True)
-#
-#         # log.info(sub_categories)
-#
-#         entity_list = Entity.objects.filter(category_id__in=list(sub_categories), status=Entity.selection)
-#
-#         log.info(entity_list)
-#
-#         context = {
-#             # 'entities':_entities,
-#             # 'user_entity_likes': el,
-#             # 'categories': _categories,
-#         }
-#         return self.render_to_response(context)
-
-
-class CategroyGroupListView(ListView):
+class CategroyGroupListView(TemplateResponseMixin, ContextMixin, View):
     http_method_names = ['get']
     template_name = 'web/category/detail.html'
-    context_object_name = "entities"
-    # paginator_class = ExtentPaginator
-    page_kwarg = "page"
-    paginate_by = 28
 
-    def get_queryset(self):
-        log.info(self.kwargs)
-        self.gid = self.kwargs.pop('gid', None)
-        sub_categories = Sub_Category.objects.filter(group=self.gid).values_list('id', flat=True)
-        entity_list = Entity.objects.filter(category_id__in=list(sub_categories), status=Entity.selection)
-        log.info(entity_list)
+    def get(self, request, *args, **kwargs):
+        # log.info(kwargs)
 
-        return entity_list[:60]
+        gid = kwargs.pop('gid', None)
+        _page = request.GET.get('page', 1)
+        category = Category.objects.get(pk = gid)
+        sub_categories = Sub_Category.objects.filter(group=gid).values_list('id', flat=True)
 
-    def get_context_data(self, **kwargs):
-        context = super(CategroyGroupListView, self).get_context_data(**kwargs)
-        category = Category.objects.get(pk = self.gid)
-        context['sub_category'] = category
-        return context
-    #     self.con
-    #     return
+        # log.info(sub_categories)
 
-# @require_GET
-# def list(request, template='web/category/list.html'):
-#
-#     _categories = Category.objects.filter(status=True)
-#
-#     return render_to_response(
-#         template,
-#         {
-#             'categories': _categories,
-#         },
-#         context_instance = RequestContext(request),
-#     )
+        _entity_list = Entity.objects.filter(category_id__in=list(sub_categories), status=Entity.selection)
+        paginator = ExtentPaginator(_entity_list, 24)
+        try:
+            _entities = paginator.page(_page)
+        except PageNotAnInteger:
+            _entities = paginator.page(1)
+        except EmptyPage:
+            raise Http404
+        # log.info(entity_list)
 
+        el = []
+        if request.user.is_authenticated():
+            e = _entities.object_list
+            el = Entity_Like.objects.filter(entity_id__in=tuple(e), user=request.user).values_list('entity_id', flat=True)
 
+        context = {
+            'entities':_entities,
+            'user_entity_likes': el,
+            'sub_category': category,
+        }
+        return self.render_to_response(context)
 
-
-# @require_GET
-# def group(request, cid, template="web/category/group.html"):
-#
-#
-#
-#     return render_to_response(
-#         template,
-#         {
-#
-#         },
-#         context_instance = RequestContext(request),
-#     )
 
 @require_GET
 def detail(request, cid, template='web/category/detail.html'):
