3373d2d563fd0e1b3d8e9f35659cd950f21a9deb
edison7500 <edison7500@gmail.com>
2015-02-09 20:15:50 +0800

    update

--- nut/apps/web/urls/category.py
+++ nut/apps/web/urls/category.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, patterns
+from apps.web.views.category import CategoryListView
+
 
 urlpatterns = patterns(
     'apps.web.views.category',
-    url(r'^$', 'list', name='web_category_list'),
+    # url(r'^$', 'list', name='web_category_list'),
+    url(r'^$', CategoryListView.as_view(), name='web_category_list'),
     url(r'^(?P<cid>\d+)/$', 'detail', name='web_category_detail'),
 )
 
