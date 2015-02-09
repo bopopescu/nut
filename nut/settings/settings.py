feae8ae227e7738412cff1b5db66763be9ca15a6
edison7500 <edison7500@gmail.com>
2015-02-09 14:53:29 +0800

    update

--- nut/settings/settings.py
+++ nut/settings/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.formtools',
     'django.contrib.sitemaps',
+    'django.contrib.humanize',
     'rest_framework',
     # 'rest_framework.authtoken',
     # 'haystack',
