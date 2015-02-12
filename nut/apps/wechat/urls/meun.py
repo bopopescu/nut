1dcf71a453225764dfff4947bdfdb31cf919b745
谢 家欣 <edison7500@gmail.com>
2015-02-12 10:26:34 +0800

    updat

---
+++ nut/apps/wechat/urls/meun.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url, patterns
+from apps.wechat.views.menu import MenuCreateView
+
+urlpatterns = patterns(
+    'apps.wechat.views.menu',
+    url(r'^create/$', MenuCreateView.as_view(), name='wechat_menu_create'),
+
+)
+
+
+__author__ = 'edison7500'
