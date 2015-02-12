921c53392e5990a8cca7fc6f4a2e362fc39550fb
edison7500 <edison7500@gmail.com>
2015-02-12 22:41:09 +0800

    update

--- nut/apps/wechat/handle.py
+++ nut/apps/wechat/handle.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 # from apps.wechat.models import Robots
-from apps.core.models import Entity
-
+from apps.core.models import Entity, Selection_Entity, Entity_Like
+from datetime import datetime
 from django.utils.log import getLogger
 
 log = getLogger('django')
@@ -19,5 +19,22 @@ def handle_reply(content):
     log.info(res)
     return res
 
+
+def handle_event(content):
+    log.info(content)
+    items = []
+    if content['EventKey'] == "V1001_SELECTION":
+        _refresh_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        entities = Selection_Entity.objects.published().filter(pub_time__lte=_refresh_datetime)
+
+        for row in entities[:5]:
+            items.append(row.entity)
+        # return items
+    elif content['EventKey'] == "V1002_POPULAR":
+        popular_list = Entity_Like.objects.popular_random()
+        entities = Entity.objects.filter(id__in=popular_list)
+        items = entities[:5]
+        # items = entities[:5]
+    return items
 
 __author__ = 'edison'
