3fb7ff977a0ddd30f2d911fedb24a810485b1a48
edison7500 <edison7500@gmail.com>
2015-03-19 14:06:46 +0800

    fixed bug

--- nut/apps/mobile/models.py
+++ nut/apps/mobile/models.py
@@ -1,6 +1,50 @@
 from django.db import models
+from django.core.cache import cache
 from apps.core.models import GKUser
 from apps.mobile.manager.mobile_manager import SessionKeyManager, AppsManager
+from hashlib import md5
+
+from django.utils.log import getLogger
+
+log = getLogger('django')
+
+
+
+class APIUser(GKUser):
+
+    class Meta:
+        proxy = True
+
+    @property
+    def fans_list(self):
+        # log.info("cache cache")
+        # return self.fans.all().values_list('follower_id', flat=True)
+        key_string = "user_fans_%s" % self.id
+        key = md5(key_string.encode('utf-8')).hexdigest()
+
+        res = cache.get(key)
+        if res:
+            log.info("hit hit")
+            return res
+        res = super(APIUser, self).fans_list
+        cache.set(key, res, timeout=3600)
+        return res
+
+
+    @property
+    def following_list(self):
+        log.info("cache cache")
+        key_string = "user_follow_%s" % self.id
+        key = md5(key_string.encode('utf-8')).hexdigest()
+
+        res = cache.get(key)
+        if res:
+            log.info("hit hit")
+            return res
+        res = super(APIUser, self).following_list
+        cache.set(key, res, timeout=3600)
+        return res
+
 
 
 class Apps(models.Model):
@@ -11,6 +55,7 @@ class Apps(models.Model):
     api_secret = models.CharField(max_length=32)
     created_time = models.DateTimeField(auto_now=True)
     objects = AppsManager()
+
     class Meta:
         ordering = ['-created_time']
 
@@ -20,7 +65,7 @@ class Apps(models.Model):
 
 
 class Session_Key(models.Model):
-    user = models.ForeignKey(GKUser, related_name = "mobile_client_session")
+    user = models.ForeignKey(APIUser, related_name = "mobile_client_session")
     app = models.ForeignKey(Apps)
     session_key = models.CharField(max_length = 64, unique = True, editable = False)
     create_time = models.DateTimeField(auto_now_add = True)
@@ -28,6 +73,13 @@ class Session_Key(models.Model):
 
     def __unicode__(self):
         return self.session_key
+    #
+    # @property
+    # def user(self):
+    #     log.info(self.user_id)
+    #     _user = APIUser.objects.get(pk = self.user_id)
+    #     return _user
+        # return self.user
 
 
 class V3_User(GKUser):
