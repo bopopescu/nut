7d956c48cac96b66e745e014218dad6599bbcbe8
edison7500 <edison7500@gmail.com>
2015-03-30 13:41:11 +0800

    fixed bug

--- nut/apps/management/urls/comments.py
+++ nut/apps/management/urls/comments.py
@@ -3,6 +3,7 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns(
     'apps.management.views.note_comment',
     url(r'^$', 'list', name='management_comment_list'),
+    url(r'^(?P<comment_id>\d+)/del/$', 'delete', name='management_comment_del'),
 )
 
 __author__ = 'edison'
