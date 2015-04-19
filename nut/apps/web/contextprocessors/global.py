c3d3f6bec39abb042a85ac6c0e0e52e4dc056796
An Chen <laohan.msa@gmail.com>
2015-04-19 21:55:18 +0800

    remove red dot on event link , if the event is read by user. still need to update setting.py , discuss with jiaxin later.

---
+++ nut/apps/web/contextprocessors/global.py
@@ -0,0 +1,17 @@
+
+from apps.core.models import Event
+# from django.core.cache import  cache
+
+def lastslug(request):
+    "A global context processor for current event slug"
+
+
+    events = Event.objects.filter(status=True)
+    slug = ''
+    if len(events) > 0:
+        event = events[0]
+        slug = event.slug
+
+    return {
+        'newest_event_slug': slug,
+    }
