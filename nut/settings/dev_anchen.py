664e82ec805eaa299c68ef223ac5c41637878ba3
An Chen <anchen@guoku.com>
2015-05-07 16:53:46 +0800

    useless

--- nut/settings/dev_anchen.py
+++ nut/settings/dev_anchen.py
@@ -7,6 +7,21 @@ TEMPLATE_CONTEXT_PROCESSORS += (
 
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'guoku',
+        'USER': 'root',
+        'PASSWORD': 'mypass740323',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {
+            'use_unicode':'utf-8',
+            'init_command':'SET storage_engine=INNODB',
+        }
+    },
+}
+
 
 
 __author__ = 'an chen '
