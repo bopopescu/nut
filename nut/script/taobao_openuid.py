60e0876225af68a69c9c84e2b4daa4981b247262
谢 家欣 <edison7500@gmail.com>
2015-05-23 23:10:23 +0800

    update

--- nut/script/taobao_openuid.py
+++ nut/script/taobao_openuid.py
@@ -12,15 +12,18 @@ app_key = getattr(settings, 'TAOBAO_APP_KEY')
 app_secret = getattr(settings, 'TAOBAO_APP_SECRET')
 
 
-tusers = Taobao_Token.objects.filter(open_uid__isnull = True)
+tusers = Taobao_Token.objects.filter(open_uid__isnull = False)
 print tusers.count()
-for row in tusers:
-    print row.id, row.taobao_id, row.screen_name
-    if (row.taobao_id):
-        t = TaobaoOpenUid(app_key, app_secret)
-        openuid = t.get_open_id(row.taobao_id)
-        print openuid
-        row.open_uid = openuid
-        row.save()
+# for row in tusers:
+#     print row.id, row.taobao_id, row.screen_name
+#     if (row.taobao_id):
+#         t = TaobaoOpenUid(app_key, app_secret)
+#         openuid = t.get_open_id(row.taobao_id)
+#         print openuid
+#         row.open_uid = openuid
+#         row.save()
+
+for row in tusers[0:10]:
+    print row.open_uid
 
 __author__ = 'edison7500'
