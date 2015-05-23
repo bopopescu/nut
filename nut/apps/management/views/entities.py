49a07ecb65e96cd84199a15fc1b120f98997fb46
谢 家欣 <edison7500@gmail.com>
2015-05-23 11:53:21 +0800

    fixed fetch image bug

--- nut/apps/management/views/entities.py
+++ nut/apps/management/views/entities.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse
 # from django.views.generic.list import ListView
 # from django.core.paginator import Paginator, EmptyPage, InvalidPage
 from django.utils.log import getLogger
-from django.core.files.storage import default_storage
+# from django.core.files.storage import default_storage
 from django.views.decorators.csrf import csrf_exempt
 from django.core.cache import cache
 from django.contrib.auth.decorators import login_required
@@ -14,6 +14,7 @@ from apps.core.models import Entity, Buy_Link
 from apps.core.forms.entity import EditEntityForm, EntityImageForm, BuyLinkForm, CreateEntityForm, load_entity_info
 from apps.core.extend.paginator import ExtentPaginator, EmptyPage, PageNotAnInteger
 from apps.core.utils.http import SuccessJsonResponse, ErrorJsonResponse
+from apps.core.tasks.entity import fetch_image
 
 from hashlib import md5
 
@@ -196,6 +197,18 @@ def remove_buy_link(request, bid):
         return ErrorJsonResponse(status=403)
     b.delete()
     return SuccessJsonResponse()
+
+@login_required
+def refetch_image(request, entity_id):
+    try:
+        _entity = Entity.objects.get(pk=entity_id)
+    except Entity.DoesNotExist:
+        raise Http404
+
+    log.info(_entity.images)
+
+    fetch_image(_entity.images, _entity.pk)
+    return SuccessJsonResponse(data={'status':'ok'})
 
 
 @login_required
