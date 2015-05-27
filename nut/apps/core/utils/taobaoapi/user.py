1550771d618a8a278dcf2eda98456d39d20c5601
edison7500 <edison7500@gmail.com>
2015-05-28 00:07:27 +0800

    update

--- nut/apps/core/utils/taobaoapi/user.py
+++ nut/apps/core/utils/taobaoapi/user.py
@@ -42,52 +42,22 @@ class TaobaoOpenUid():
         if (resp.has_key('opensecurity_uid_get_response')):
             return resp['opensecurity_uid_get_response']['open_uid']
 
-<<<<<<< HEAD
-class BaichuanUid():
-=======
 
 class TaobaoOpenIsvUID():
 
->>>>>>> 72e43586aba0244e68985f37dafbfd46d5a59172
     def __init__(self, app_key, app_secret):
         self.req = OpensecurityIsvUidGetRequest()
         self.req.set_app_info(appinfo(app_key, app_secret))
 
-<<<<<<< HEAD
-    def change_openuid(self, uid):
-        self.req.open_uid = uid
-=======
     def get_isv_uid(self, open_uid):
         self.req.open_uid = open_uid
->>>>>>> 72e43586aba0244e68985f37dafbfd46d5a59172
+
         try:
             resp = self.req.getResponse()
             print resp
         except Exception, e:
-<<<<<<< HEAD
-            print e.message
-            raise
-        # if (resp.has_key('opensecurity_isv_uid_get_response')):
-        #     return resp['opensecurity_isv_uid_get_response']['open_uid_isv']
-
-
-
-if __name__=="__main__":
-    t = TaobaoOpenUid('12313170', '90797bd8d5859aac971f8cc9d4e51105')
-    open_uid = t.get_open_id('56454267')
-    print open_uid
-    # t.get_open_uid('72913763')
-    #
-    b = BaichuanUid('12313170', '90797bd8d5859aac971f8cc9d4e51105')
-    print b.change_openuid('AAEM6ZQCAASs0HrMGWO7bvZ-')
-
-    c = BaichuanUid('23093827', '7db5a8b0fb21e5d3b9910bf8b9feba38')
-    print  c.change_openuid('AAEELit1AB1sujxQQZ64d8D1')
-
-    # b = BaichuanUid('12313170', '90797bd8d5859aac971f8cc9d4e51105')
-    # print b.change_openuid('AAEELit1AB1sujxQQZ64d8D1')
-=======
             print e
+            raise
 
         if (resp.has_key('opensecurity_isv_uid_get_response')):
             return resp['opensecurity_isv_uid_get_response']['open_uid_isv']
@@ -96,6 +66,11 @@ if __name__=="__main__":
     t = TaobaoOpenIsvUID('12313170', '90797bd8d5859aac971f8cc9d4e51105')
     open_uid = t.get_isv_uid('AAEa6ZQCAASs0HrMGWO8Wjpb')
     print open_uid
+
+
+    from apps.core.models import Taobao_Token
+
+    taobao = Taobao_Token.objects.get(user_id = 1062)
+    print taobao
     # t.get_open_uid('72913763')
 
->>>>>>> 72e43586aba0244e68985f37dafbfd46d5a59172
--- nut/apps/v4/urls/__init__.py
+++ nut/apps/v4/urls/__init__.py
@@ -31,7 +31,10 @@ urlpatterns += patterns(
 
 # TODO: taobao
     url(r'^taobao/login/$', 'taobao.login_by_taobao', name='v4_login_by_taobao'),
-    url(r'^taobao/register/$', 'taobao.register_by_taobao', name='v4_register_by_taobao')
+    url(r'^taobao/register/$', 'taobao.register_by_taobao', name='v4_register_by_taobao'),
+
+    url(r'^baichuan/login', 'baichuan.login', name='v4_login_by_baichuan'),
+
     # url(r'')
 )
 
