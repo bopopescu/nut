8927913487affa02afd5ef17f89e345b2204dd7e
An Chen <anchen@guoku.com>
2015-05-30 22:18:35 +0800

    detail page button , fix for 5 digit likes

--- nut/settings/dev_anchen.py
+++ nut/settings/dev_anchen.py
@@ -3,21 +3,21 @@ from stage import *
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
 
+#
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+#         'LOCATION': '/var/tmp/django_cache',
+#     }
+# }
+#
+# TEMPLATE_CONTEXT_PROCESSORS += (
+#     # 'apps.web.contextprocessors.global.lastslug',
+# )
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': '/var/tmp/django_cache',
-    }
-}
-
-TEMPLATE_CONTEXT_PROCESSORS += (
-    # 'apps.web.contextprocessors.global.lastslug',
-)
-
-Current_Dbhost = 'localhost'
+# Current_Dbhost = 'localhost'
 # Current_Dbhost = '10.0.1.110'
-# Current_Dbhost = '10.0.2.90'
+Current_Dbhost = '10.0.2.90'
 
 
 DATABASES = {
