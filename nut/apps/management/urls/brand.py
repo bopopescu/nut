fb4a5a1af7dbbc556561cbc27f51c8d705b51795
edison7500 <edison7500@gmail.com>
2015-06-06 20:30:51 +0800

    update brand page

--- nut/apps/management/urls/brand.py
+++ nut/apps/management/urls/brand.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, patterns
 
-from apps.management.views.brand import BrandListView
+from apps.management.views.brand import BrandListView, BrandEntityListView, BrandStatView
 
 urlpatterns = patterns(
     'apps.management.views.brand',
     url(r'^$', BrandListView.as_view(), name='management_brand_list'),
+    url(r'^stat/$', BrandStatView.as_view(), name='management_brand_stat'),
+    url(r'^(?P<brand>.+)/$', BrandEntityListView.as_view(), name='management_brand_entity_list'),
     # url(r'^add/$', 'create', name='management_banner_create'),
     # url(r'^(?P<banner_id>\d+)/edit/$', 'edit', name='management_banner_edit'),
 )
--- nut/apps/management/views/brand.py
+++ nut/apps/management/views/brand.py
@@ -1,24 +1,97 @@
 # from django.shortcuts import render_to_response
-from django.views.generic.base import View, TemplateResponseMixin, ContextMixin
+# from django.views.generic.base import View, TemplateResponseMixin, ContextMixin
+
+from django.http import Http404
 from apps.core.models import Brand
 # import random
 # from django.db.models import Count
-# from apps.core.models import Entity, Entity_Like, Sub_Category
+from apps.core.models import Entity
+from apps.core.views import BaseListView
+from apps.core.extend.paginator import ExtentPaginator, EmptyPage, InvalidPage
+
 from django.utils.log import getLogger
 
 log = getLogger('django')
 
+class BrandStatView(BaseListView):
+    template_name = "management/brand/stat.html"
 
+    def get_queryset(self):
 
-class BrandListView(TemplateResponseMixin, ContextMixin, View):
-    template_name = "management/brand/list.html"
+        entities_stat = Entity.objects.raw("select id, brand, count(*) as b  from core_entity where brand !='' and status != -1  group by brand ORDER BY b DESC")
+        return entities_stat
 
     def get(self, request):
-        _brand_list = Brand.objects.all()
+
+        _brand_stat_list = self.get_queryset()
+        page = request.GET.get('page', 1)
+
+        paginator = ExtentPaginator(list(_brand_stat_list), 30)
+
+        try:
+            _brand_stat = paginator.page(page)
+        except InvalidPage:
+            _brand_stat = paginator.page(1)
+        except EmptyPage:
+            raise Http404
+
         context = {
-            'brands':_brand_list,
+            'brand_stat': _brand_stat,
+        }
+
+        return self.render_to_response(context)
+
+class BrandListView(BaseListView):
+    template_name = "management/brand/list.html"
+    # queryset = Brand.objects.all()
+
+    def get_queryset(self):
+
+        return Brand.objects.all()
+
+    def get(self, request):
+        _brand_list = self.get_queryset()
+        page = request.GET.get('page', 1)
+
+        paginator = ExtentPaginator(_brand_list, 30)
+
+        try:
+            _banrds = paginator.page(page)
+        except InvalidPage:
+            _banrds = paginator.page(1)
+        except EmptyPage:
+            raise Http404
+
+        context = {
+            'brands':_banrds,
         }
         return self.render_to_response(context)
 
+
+class BrandEntityListView(BaseListView):
+    template_name = 'management/brand/entities.html'
+
+    def get_queryset(self, **kwargs):
+        name = kwargs.pop('brand_name')
+        return Entity.objects.filter(brand__contains=name)
+
+    def get(self, request, brand):
+        _entity_list = self.get_queryset(brand_name=brand)
+        page = request.GET.get('page', 1)
+
+        paginator = ExtentPaginator(_entity_list, 30)
+
+        try:
+            _entities = paginator.page(page)
+        except InvalidPage:
+            _entities = paginator.page(1)
+        except EmptyPage:
+            raise Http404
+
+        context = {
+            'brand': brand,
+            'entities': _entities,
+        }
+        return self.render_to_response(context)
 
 __author__ = 'edison'
