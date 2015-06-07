ce8f1dc71109929733cab7e636532bdb2f2eabd9
edison7500 <edison7500@gmail.com>
2015-06-07 13:57:53 +0800

    fixed notification

--- nut/apps/notifications/models.py
+++ nut/apps/notifications/models.py
@@ -191,8 +191,8 @@ def push_notification(sender, instance, created, **kwargs):
 
                 push.platform = jpush.platform(_platform)
                 push.audience = jpush.registration_id(reg.rid)
-
-                ios_msg = jpush.ios(alert=verb.encode('utf8'), badge="+1", extras={'entity':'v1'})
+                # log.info("%d" % instance.recipient.notifications.unread().count())
+                ios_msg = jpush.ios(alert=verb.encode('utf8'), badge=instance.recipient.notifications.unread().count(), extras={'url':'guoku://entity/%s/' % instance.target.pk})
                 push.notification = jpush.notification(alert=verb.encode('utf8'), ios=ios_msg)
                 push.options = {"time_to_live":86400, "apns_production":_production}
                 push.send()
