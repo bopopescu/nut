54e02d3e5ef8ca7be45db3be4ec91d1a0aa0d79c
edison7500 <edison7500@gmail.com>
2015-06-07 23:17:37 +0800

    update

--- nut/apps/management/urls/brand.py
+++ nut/apps/management/urls/brand.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, patterns
 
-from apps.management.views.brand import BrandListView, BrandEntityListView, BrandStatView, BrandEditView, BrandCreateView
+from apps.management.views.brand import \
+    BrandListView, BrandEntityListView, \
+    BrandStatView, BrandEditView, \
+    BrandCreateView, BrandNameEditView
 
 urlpatterns = patterns(
     'apps.management.views.brand',
@@ -9,7 +12,7 @@ urlpatterns = patterns(
     url(r'^create/$', BrandCreateView.as_view(), name='management_brand_create'),
 
     url(r'^(?P<brand_id>\d+)/edit/$', BrandEditView.as_view(), name='management_brand_edit'),
-
+    url(r'^/edit/(?P<brand>>+)/$', BrandNameEditView.as_view(), name='management_brand_name_edit'),
     url(r'^(?P<brand>.+)/$', BrandEntityListView.as_view(), name='management_brand_entity_list'),
     # url(r'^add/$', 'create', name='management_banner_create'),
     # url(r'^(?P<banner_id>\d+)/edit/$', 'edit', name='management_banner_edit'),
