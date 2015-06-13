2c3396de203034925a34329d71c98d4e0749919a
An Chen <anchen@guoku.com>
2015-06-14 02:07:28 +0800

    update

--- nut/apps/management/urls/article.py
+++ nut/apps/management/urls/article.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url, patterns
+from apps.management.views.article import SelectionArticleList
+
 
 urlpatterns = patterns(
     'apps.management.views.article',
@@ -7,6 +9,8 @@ urlpatterns = patterns(
     url(r'^(?P<article_id>\d+)/edit/$', 'edit', name='management_article_edit'),
     url(r'^(?P<article_id>\d+)/cover/upload/$', 'upload_cover', name='management_article_cover_upload'),
     url(r'^(?P<article_id>\d+)/preview/$', 'preview', name='management_article_preview'),
+    # by An , handle selection Article
+    url(r'^selection/', SelectionArticleList.as_view(), name='management_selection_article_list')
 )
 
 __author__ = 'edison'
