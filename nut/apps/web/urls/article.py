885ff9c643c4a32d9655a16c9c67a5c8395a7d80
An Chen <anchen@guoku.com>
2015-06-17 11:39:30 +0800

    update

--- nut/apps/web/urls/article.py
+++ nut/apps/web/urls/article.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, patterns
 
-from apps.web.views.article import ArticleList, EditorArticleList, EditorArticleCreate,EditorArticleEdit
+from apps.web.views.article import ArticleList, EditorArticleList, EditorArticleCreate,EditorArticleEdit,ArticleDetail
 
 urlpatterns = patterns(
     'apps.web.views.article',
@@ -8,6 +8,7 @@ urlpatterns = patterns(
     url(r'^editor/$', EditorArticleList.as_view(), name='web_editor_article_list'),
     url(r'^create/$',EditorArticleCreate.as_view(),name='web_editor_article_create'),
     url(r'^(?P<pk>\d+)/edit/$',EditorArticleEdit.as_view(), name='web_editor_article_edit'),
+    url(r'^(?P<pk>\d+)/page/',ArticleDetail.as_view(),name='web_article_page'),
 )
 
 __author__ = 'edison'
