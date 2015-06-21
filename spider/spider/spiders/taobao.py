0e78a837ee77b9d77d327f5f0c50d5c7806dff5f
edison7500 <edison7500@gmail.com>
2015-06-21 21:26:18 +0800

    add spider

---
+++ spider/spider/spiders/taobao.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class TaobaoSpider(scrapy.Spider):
+    name = "taobao"
+    allowed_domains = ["taobao.com"]
+    start_urls = (
+        'http://www.taobao.com/',
+    )
+
+    def parse(self, response):
+        pass
