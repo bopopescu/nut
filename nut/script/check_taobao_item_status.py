77e304fd923adae0c9939a49ed360461f0e4fb9f
jiaxin <edison7500@gmail.com>
2015-06-25 08:47:02 +0800

    update

--- nut/script/check_taobao_item_status.py
+++ nut/script/check_taobao_item_status.py
@@ -1,7 +1,7 @@
 import os, sys
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(BASE_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.stage'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.production'
 
 from apps.core.models import Buy_Link, Entity
 import requests
@@ -19,8 +19,8 @@ def crawl(item_id):
     return res.json()
 
 links = Buy_Link.objects.filter(origin_source='taobao.com', entity__status=Entity.selection).exclude(status=Buy_Link.remove).order_by('-id')
-
-for row in links[:1000]:
+# print links.query
+for row in links:
     # print row.origin_id
     print row.origin_id, crawl(item_id=row.origin_id)
     time.sleep(10)
