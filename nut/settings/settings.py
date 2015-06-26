cc8e93a8203b3fab72155368c80dd4db5058c0cb
An Chen <anchen@guoku.com>
2015-06-26 08:15:35 +0800

    add captcha difficulty

--- nut/settings/settings.py
+++ nut/settings/settings.py
@@ -285,7 +285,7 @@ JPUSH_SECRET = 'a0529d3efa544d1da51405b7'
 
 
 # for django-simple-captcha
-CAPTCHA_NOISE_FUNCTIONS = ('captcha.helpers.noise_dots',)
-
+CAPTCHA_NOISE_FUNCTIONS = ('captcha.helpers.noise_arcs','captcha.helpers.noise_dots',)
+CAPTCHA_LENGTH = 5
 # for debug server popular  category test
 DEFAULT_POPULAR_SCALE =  7
