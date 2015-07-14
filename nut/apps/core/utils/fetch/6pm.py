8e7e4479b576046430971c8e703b232b75e54837
jiaxin <edison7500@gmail.com>
2015-07-14 16:13:00 +0800

    update

--- nut/apps/core/utils/fetch/6pm.py
+++ nut/apps/core/utils/fetch/6pm.py
@@ -12,4 +12,31 @@ class SixPM(Spider):
         key = md5(self.url).hexdigest()
         return key
 
+    @property
+    def nick(self):
+        return '6pm'
+
+    @property
+    def brand(self):
+        brand = self.soup.select("#productStage h1 .brand")
+        return brand[0].string
+
+    @property
+    def images(self):
+        images = list()
+        optimages = self.soup.select("#productImages ul li a span img")
+        if len(optimages) > 0:
+            for row in optimages:
+                image = row.attrs.get('src')
+                # print image
+                images.append(image)
+        return images
+        # print optimages
+
+
+if __name__=="__main__":
+
+    pm = SixPM("http://www.6pm.com/michael-antonio-avalon-brown")
+    print pm.brand
+
 __author__ = 'edison'
