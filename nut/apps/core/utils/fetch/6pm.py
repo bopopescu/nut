b44c58b14dd311c4f8c9b684cbabfed6809cd617
jiaxin <edison7500@gmail.com>
2015-07-14 13:31:34 +0800

    update

--- nut/apps/core/utils/fetch/6pm.py
+++ nut/apps/core/utils/fetch/6pm.py
@@ -1,8 +1,15 @@
 from spider import Spider
+from hashlib import md5
 
 class SixPM(Spider):
 
     def __init__(self, url):
         super(SixPM, self).__init__(url)
+
+
+    @property
+    def origin_id(self):
+        key = md5(self.url).hexdigest()
+        return key
 
 __author__ = 'edison'
