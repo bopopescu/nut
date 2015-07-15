3aa6dffd165b0ac2d7a2679c50263e442c0a45da
An Chen <anchen@guoku.com>
2015-07-15 20:05:22 +0800

    related article update , pull new related when reach bottom of the page

---
+++ nut/static/js/web/refresh.js
@@ -0,0 +1,19 @@
+(function($, window, document){
+
+    var RefreshApp=Class.extend({
+        init: function(){
+            this.currentPageNum = 1 ;
+            this.requestUrl = ''
+            this.initEvents();
+        },
+        initEvents: function(){
+            few
+        },
+
+    });
+
+
+    $(function(){
+
+    });
+})(jQuery, window, document)
