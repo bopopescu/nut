7f758ef88fc0430d8d130ffad23f68435a87395c
jiaxin <edison7500@gmail.com>
2015-07-16 16:44:56 +0800

    update tag

--- nut/apps/tag/urls/__init__.py
+++ nut/apps/tag/urls/__init__.py
@@ -1,1 +1,11 @@
+from django.conf.urls import url, patterns
+from apps.tag.views import TagListView, TagEntityView
+
+
+urlpatterns = patterns(
+    'apps.tag.views',
+    url(r'^$', TagEntityView.as_view(), name='tag_list_url'),
+
+)
+
 __author__ = 'xiejiaxin'
--- nut/apps/tag/views/__init__.py
+++ nut/apps/tag/views/__init__.py
@@ -1,1 +1,20 @@
+from django.views.generic import ListView, DetailView
+from apps.tag.models import Tags, Content_Tags
+
+
+class TagListView(ListView):
+    queryset = Tags.objects.all()
+    http_method_names = ['get']
+
+
+class TagEntityView(ListView):
+
+    http_method_names = ['get']
+    template_name = 'tag/entities.html'
+
+    def get(self, tag_name, **kwargs):
+
+
+        return self.render_to_response(context={})
+
 __author__ = 'xiejiaxin'
