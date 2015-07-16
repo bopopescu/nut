18e85ca73ea4cf3560b1683511d79282f1563a51
jiaxin <edison7500@gmail.com>
2015-07-16 17:58:47 +0800

    update

--- nut/apps/tag/views/__init__.py
+++ nut/apps/tag/views/__init__.py
@@ -1,20 +1,48 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 from apps.tag.models import Tags, Content_Tags
+from apps.core.extend.paginator import ExtentPaginator
+
+from django.utils.log import getLogger
+
+log = getLogger('django')
 
 
 class TagListView(ListView):
     queryset = Tags.objects.all()
     http_method_names = ['get']
+    template_name = 'tag/list.html'
+
 
 
 class TagEntityView(ListView):
 
     http_method_names = ['get']
     template_name = 'tag/entities.html'
+    paginate_by = 30
+    paginator_class = ExtentPaginator
 
-    def get(self, tag_name, **kwargs):
+    def get_queryset(self):
+        self.tag = Tags.objects.get(name=self.tag_name)
+        self.queryset = Content_Tags.objects.filter(tag=self.tag)
+        return self.queryset
 
+    def get_context_data(self, **kwargs):
+        res = super(TagEntityView, self).get_context_data(**kwargs)
+        res.update(
+            {
+                'tag': self.tag,
+            }
+        )
+        return res
 
-        return self.render_to_response(context={})
+    def get(self, request, *args, **kwargs):
+        self.tag_name = kwargs.pop('tag_name', None)
+        assert self.tag_name is not None
+
+        return super(TagEntityView, self).get(request, *args, **kwargs)
+        # return self.render_to_response(context={})
+
+class TagArticleView(ListView):
+    http_method_names = ['get']
 
 __author__ = 'xiejiaxin'
