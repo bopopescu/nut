0597669f7e1e3c55b0ad72768943073b7b33dc6e
jiaxin <edison7500@gmail.com>
2015-07-18 12:06:39 +0800

    merge

--- nut/apps/core/models.py
+++ nut/apps/core/models.py
@@ -1373,14 +1373,13 @@ def user_like_notification(sender, instance, created, **kwargs):
 post_save.connect(user_like_notification, sender=Entity_Like, dispatch_uid="user_like_action_notification")
 
 
+from django.core import serializers
+from apps.tag.tasks import generator_tag
 def user_post_note_notification(sender, instance, created, **kwargs):
-    # log.info(created)
-<<<<<<< HEAD
-=======
-    data = serializers.serialize('json', [instance])
-    generator_tag(data=data)
 
->>>>>>> dev
+    data = serializers.serialize('json', [instance])
+    generator_tag.delay(data=data)
+
     if issubclass(sender, Note) and created:
         # log.info(instance.user)
         instance.entity.innr_note()
