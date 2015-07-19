09051721e4a7672effb67cf7d39423f84dec862a
jiaxin <edison7500@gmail.com>
2015-07-20 00:07:58 +0800

    update

--- nut/apps/web/urls/category.py
+++ nut/apps/web/urls/category.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, patterns
-from apps.web.views.category import CategoryListView, CategroyGroupListView
+from apps.web.views.category import CategoryListView, CategroyGroupListView, OldCategory
 
 
 urlpatterns = patterns(
@@ -7,6 +7,7 @@ urlpatterns = patterns(
     # url(r'^$', 'list', name='web_category_list'),
     url(r'^$', CategoryListView.as_view(), name='web_category_list'),
     url(r'^group/(?P<gid>\d+)/$', CategroyGroupListView.as_view(), name='web_category_group'),
+    url(r'c/(?P<cid>\d)/$', OldCategory.as_view(), name='web_category_old_url'),
     url(r'^(?P<cid>\d+)/$', 'detail', name='web_category_detail'),
     url(r'^(?P<cid>\d+)/olike/$','detail_like', name='web_category_detail_olike'),
 )
