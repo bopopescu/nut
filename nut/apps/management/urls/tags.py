c814b40ae02a08b3f41c2c4e746ca68f89909022
jiaxin <edison7500@gmail.com>
2015-07-22 22:15:40 +0800

    update

--- nut/apps/management/urls/tags.py
+++ nut/apps/management/urls/tags.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, patterns
-from apps.management.views.tags import TagListView, TagEntitiesView, EditTagFormView
+from apps.management.views.tags import TagListView, TagEntitiesView, EditTagFormView, ArticleTagListView
 
 urlpatterns = patterns(
     'apps.management.views.tags',
     # url(r'^$', 'list', name='management_tag_list'),
     url(r'^$', TagListView.as_view(), name='management_tag_list'),
-    url(r'^(?P<tag_name>\w+)/$', TagEntitiesView.as_view(), name='management_tag_entities'),
+    url(r'^(?P<tag_name>(\w|-)+)/$', TagEntitiesView.as_view(), name='management_tag_entities'),
+    url(r'^article/(?P<tag_name>(\w|\-)+)/$', ArticleTagListView.as_view(), name='management_tag_articles'),
     url(r'^(?P<tag_id>\d+)/edit/$', EditTagFormView.as_view(), name='management_tag_edit'),
 )
 
