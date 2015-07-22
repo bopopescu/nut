f105e485892e6761a640454a464bb0a8a8222838
jiaxin <edison7500@gmail.com>
2015-07-22 15:09:49 +0800

    merge

--- nut/settings/production.py
+++ nut/settings/production.py
@@ -4,7 +4,7 @@ from settings import *
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-STATIC_URL = 'http://static.guoku.com/static/v4/5d4ab2ab2f60aa37457f6d1deeb56cf871ea36e8/'
+STATIC_URL = 'http://static.guoku.com/static/v4/69ca0f460933174f34e67704fc346302f806d96c/'
 
 LANGUAGE_CODE = 'zh-cn'
 
