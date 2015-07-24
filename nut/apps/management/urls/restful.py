b7453a9662d2c158fd45c57083c016dc826a61ed
An Chen <anchen@guoku.com>
2015-07-24 15:29:36 +0800

    in the middle of restful management

---
+++ nut/apps/management/urls/restful.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, patterns
+
+from apps.management.views.users import RESTfulUserListView
+from apps.management.views.article import RESTfulArticleListView, RESTfulArticleDetail
+
+urlpatterns = patterns(
+   '',
+    url(r'^users/$', RESTfulUserListView.as_view() , name='restful_user_list'),
+    url(r'^articles/$', RESTfulArticleListView.as_view() , name='restful_article_list'),
+    url(r'^articles/(?P<pk>\d+)/$', RESTfulArticleDetail.as_view() , name='restful_article_detail'),
+
+)
