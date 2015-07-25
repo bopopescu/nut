a0fdc5d08daa44dd9a6ce3a27742cf7df047d963
jiaxin <edison7500@gmail.com>
2015-07-25 23:27:38 +0800

    update

--- nut/apps/tag/urls/__init__.py
+++ nut/apps/tag/urls/__init__.py
@@ -6,6 +6,7 @@ urlpatterns = patterns(
     'apps.tag.views',
     url(r'^$', TagListView.as_view(), name='tag_list_url'),
     url(r'^(?P<tag_name>\w+)/$', TagEntityView.as_view(), name='tag_entities_url'),
+    url(r'^articles/(?P<tag_name>\w+)/$', TagEntityView.as_view(), name='tag_entities_url'),
 )
 
 __author__ = 'xiejiaxin'
