5d146a9c1befc451343b7be957487cfdf321127a
jiaxin <edison7500@gmail.com>
2015-08-04 00:02:35 +0800

    update

--- nut/apps/core/search_indexes.py
+++ nut/apps/core/search_indexes.py
@@ -5,6 +5,7 @@ from apps.tag.models import Tags
 
 class EntityIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
+    # id = indexes.IntegerField(model_attr='id')
     title = indexes.CharField(model_attr='title', boost=1.125)
     user = indexes.CharField(model_attr='user')
     created_time = indexes.DateTimeField(model_attr='created_time')
@@ -21,6 +22,9 @@ class EntityIndex(indexes.SearchIndex, indexes.Indexable):
 
 class UserIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
+    # id = indexes.IntegerField(model_attr='id')
+    nickname = indexes.CharField(model_attr='user__profile__nickname', boost=1.125)
+    bio = indexes.CharField(model_attr='user__profile__bio')
     # email = indexes.CharField(model_attr='email', boost=1.125)
 
     def get_model(self):
@@ -39,11 +43,16 @@ class TagIndex(indexes.SearchIndex, indexes.Indexable):
     def index_queryset(self, using=None):
         return self.get_model().objects.all()
 #
-# #
-# class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
-#     text = indexes.CharField(document=True, use_template=True)
-#     title = indexes.CharField(model_attr='title', boost=1.125)
+#
+class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    title = indexes.CharField(model_attr='title', boost=1.125)
 
+    def get_model(self):
+        return Article
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.filter(publish=Article.published)
 
 
 __author__ = 'edison'
