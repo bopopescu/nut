ff1ef63782461de76e415edcb22e25d8d5277e76
jiaxin <edison7500@gmail.com>
2015-08-18 00:37:45 +0800

    update

--- nut/apps/management/urls/search.py
+++ nut/apps/management/urls/search.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, patterns
-from apps.management.views.search import ManageSearchView
+from apps.management.views.search import ManageSearchView, AutoCompleteView
 
 
 urlpatterns = patterns(
     'apps.management.views.search',
     url(r'^$', ManageSearchView.as_view(), name='management_search'),
+    url(r'^autocomplete/$', AutoCompleteView.as_view(), name='management_autocomplete'),
 )
 
 
