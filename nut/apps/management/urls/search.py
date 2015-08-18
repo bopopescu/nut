101559b34cdeb683bb87e6405eb26714e0c2f740
jiaxin <edison7500@gmail.com>
2015-08-18 10:45:33 +0800

    build new index schema

--- nut/apps/management/urls/search.py
+++ nut/apps/management/urls/search.py
@@ -5,7 +5,7 @@ from apps.management.views.search import ManageSearchView, AutoCompleteView
 urlpatterns = patterns(
     'apps.management.views.search',
     url(r'^$', ManageSearchView.as_view(), name='management_search'),
-    url(r'^autocomplete/$', AutoCompleteView.as_view(), name='management_autocomplete'),
+    url(r'^autocomplete/?$', AutoCompleteView.as_view(), name='management_autocomplete'),
 )
 
 
