2e8fb3c187722ceea268ed7fa577678d83dc2682
Judy <judy.programer@gmail.com>
2015-08-18 15:43:28 +0800

    添加商品时通过汇率计算； amazon商品图片优先取大图；

--- spider/spider/settings.py
+++ spider/spider/settings.py
@@ -14,8 +14,11 @@ BOT_NAME = 'spider'
 SPIDER_MODULES = ['spider.spiders']
 NEWSPIDER_MODULE = 'spider.spiders'
 
-redis_host = 'localhost'
-redis_port = 6379
+# config of site in redis.
+# config_redis_host = 'localhost'
+config_redis_host = '10.0.2.95'
+config_redis_port = 6379
+config_redis_db = 10
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
