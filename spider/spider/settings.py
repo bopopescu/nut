c1bc20c0e13722a69d5b20ecb1c4791632f481c0
jiaxin <edison7500@gmail.com>
2015-08-21 00:11:01 +0800

    merge

--- spider/spider/settings.py
+++ spider/spider/settings.py
@@ -70,7 +70,7 @@ DOWNLOAD_DELAY=3
 ITEM_PIPELINES = {
    'spider.pipelines.ErrorPipeLine': 100,
    'spider.pipelines.DuplicatesPipeline': 300,
-   'spider.pipelines.SQLStorePipeline': 500,
+   # 'spider.pipelines.SQLStorePipeline': 500,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
