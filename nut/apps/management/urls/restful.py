557f73ae35197c525bacc9f26ac94e82f4efe4f0
An Chen <anchen@guoku.com>
2015-08-21 16:33:53 +0800

    restful backend ready

--- nut/apps/management/urls/restful.py
+++ nut/apps/management/urls/restful.py
@@ -1,10 +1,18 @@
 from django.conf.urls import url, patterns
+from rest_framework.urlpatterns import format_suffix_patterns
+
 
 from apps.management.views.users import RESTfulUserListView
 from apps.management.views.article import RESTfulArticleListView, RESTfulArticleDetail
 
+from apps.management.views.sidebar_banner import RFSidebarBannerListView, RFSidebarBannerDetailView
+
 urlpatterns = patterns(
-   '',
+    '',
+    url(r'^sbbanners/$', RFSidebarBannerListView.as_view() , name='restful_sidebar_banner_list'),
+    url(r'^sbbanners/(?P<pk>[0-9]+)/$', RFSidebarBannerDetailView.as_view() , name='restful_sidebar_banner_detail'),
+
+
     # disabled by AnChen, to be continued
     # url(r'^users/$', RESTfulUserListView.as_view() , name='restful_user_list'),
     # url(r'^articles/$', RESTfulArticleListView.as_view() , name='restful_article_list'),
