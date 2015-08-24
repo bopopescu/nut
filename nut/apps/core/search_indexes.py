ae1cb99efe51ac1507db8b8b68204997e00fad28
jiaxin <edison7500@gmail.com>
2015-08-24 22:04:35 +0800

    update

--- nut/apps/core/search_indexes.py
+++ nut/apps/core/search_indexes.py
@@ -1,12 +1,25 @@
 from haystack import indexes
-from apps.core.models import Entity, GKUser, Article
+from apps.core.models import Entity, GKUser, Article, Sub_Category
 from apps.tag.models import Tags
+
+
+class CategoryIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    title = indexes.CharField(model_attr='title')
+
+    title_auto = indexes.NgramField(model_attr='title')
+
+    def get_model(self):
+        return Sub_Category
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all().using('slave')
 
 
 class EntityIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     entity_id = indexes.IntegerField(model_attr='id')
-    title = indexes.CharField(model_attr='title', boost=1.125)
+    title = indexes.CharField(model_attr='title', boost=1.25, faceted=True)
     user = indexes.CharField(model_attr='user')
     created_time = indexes.DateTimeField(model_attr='created_time')
     price = indexes.FloatField(model_attr='price')
