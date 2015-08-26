dcf9934e5651ae521adf52cd5244efa7aa7166d2
jiaxin <edison7500@gmail.com>
2015-08-27 00:13:59 +0800

    update

---
+++ nut/apps/core/templatetags/pinyin.py
@@ -0,0 +1,15 @@
+from django import template
+from django.utils.log import getLogger
+# from django.conf import settings
+from xpinyin import Pinyin
+
+register = template.Library()
+log = getLogger('django')
+
+
+def pinyin(value):
+    p = Pinyin()
+    return p.get_pinyin(value, '')
+register.filter(pinyin)
+
+__author__ = 'xiejiaxin'
