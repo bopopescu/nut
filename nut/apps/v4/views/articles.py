090a8c447568eb533772a6a55290b2393229e168
jiaxin <edison7500@gmail.com>
2015-08-29 22:33:37 +0800

    update

--- nut/apps/v4/views/articles.py
+++ nut/apps/v4/views/articles.py
@@ -3,21 +3,34 @@ from apps.core.views import BaseJsonView
 from apps.v4.models import APISeletion_Articles
 from apps.mobile.lib.sign import check_sign
 
+from django.core.paginator import Paginator
 
 class ArticlesListView(BaseJsonView):
     http_method_names = ['get']
 
     def get_data(self, context):
-        sla = APISeletion_Articles.objects.published()[:10]
+        sla_list = APISeletion_Articles.objects.published().order_by('-pub_time')
+
+        paginator = Paginator(sla_list, self.size)
 
         res = []
-        for row in sla:
+        try:
+            sla = paginator.page(self.page)
+        except Exception:
+            return res
+
+        for row in sla.object_list:
             # print row.article
             res.append(
                 row.api_article.v4_toDict()
             )
 
         return res
+
+    def get(self, request, *args, **kwargs):
+        self.page = request.GET.get('page', 1)
+        self.size = request.GET.get('size', 10)
+        return super(ArticlesListView, self).get(request, *args, **kwargs)
 
     # @check_sign
     def dispatch(self, request, *args, **kwargs):
