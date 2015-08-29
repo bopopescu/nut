8ee6280eeb0fca9d15dc0dbf776d5547e60070aa
jiaxin <edison7500@gmail.com>
2015-08-30 03:21:07 +0800

    merge

--- nut/settings/settings.py
+++ nut/settings/settings.py
@@ -201,7 +201,7 @@ REST_FRAMEWORK = {
 # EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
 # MAILGUN_ACCESS_KEY = 'key-7n8gut3y8rpk1u-0edgmgaj7vs50gig8'
 EMAIL_BACKEND = 'sendcloud.SendCloudBackend'
-MAIL_APP_USER = 'guoku_test_7LOIZp'
+MAIL_APP_USER = 'guoku_hi'
 MAIL_APP_KEY = 'DLq9W6TiDZAWOLNv'
 RESET_PASSWORD_TEMPLATE = 'forget_password'
 VERFICATION_EMAIL_TEMPLATE = 'verify_email'
@@ -253,10 +253,6 @@ CELERY_DISABLE_RATE_LIMITS = True
 # CELERY_RESULT_SERIALIZER = 'json'
 
 
-#
-# SPHINX_API_VERSION = 0x116
-# SPHINX_SERVER = '10.0.2.50'
-# SPHINX_PORT = 3312
 
 
 # taobao
