60b8a61bee563581c440013110bee46e5083caae
jiaxin <edison7500@gmail.com>
2015-08-29 21:58:09 +0800

    add articles api

--- nut/apps/v4/views/articles.py
+++ nut/apps/v4/views/articles.py
@@ -1,11 +1,21 @@
 from apps.core.views import BaseJsonView
-
+# from apps.core.models import Selection_Article
+from apps.v4.models import APISeletion_Articles
 
 class ArticlesListView(BaseJsonView):
+    http_method_names = ['get']
 
     def get_data(self, context):
+        sla = APISeletion_Articles.objects.published()[:10]
 
-        return context
+        res = []
+        for row in sla:
+            # print row.article
+            res.append(
+                row.api_article.v4_toDict()
+            )
+
+        return res
 
 
 __author__ = 'xiejiaxin'
