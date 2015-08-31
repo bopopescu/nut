1ee6cb3b4c3d3194e787dcf1e8e249f0878e68ed
An Chen <anchen@guoku.com>
2015-08-31 18:14:06 +0800

    bleach article content for feed

--- nut/apps/web/feeds.py
+++ nut/apps/web/feeds.py
@@ -145,7 +145,8 @@ class ArticlesFeeds(Feed):
         return item.pub_time
 
     def item_description(self, item):
-        content = strip_tags(item.article.bleached_content)
+        content = strip_tags(item.article.content)
+        # content = strip_tags(item.article.bleached_content)
         desc = content.split(u'。')
         # return "<![CDATA[%s]]>" % (desc[0] + u'。')
         return desc[0] + u'。'
@@ -155,7 +156,8 @@ class ArticlesFeeds(Feed):
 
         extra.update(
             {
-                'content_encoded': "<![CDATA[%s]]>" % item.article.bleached_content,
+                'content_encoded': "<![CDATA[%s]]>" % item.article.content,
+                # 'content_encoded': "<![CDATA[%s]]>" % item.article.bleached_content,
             }
         )
 
