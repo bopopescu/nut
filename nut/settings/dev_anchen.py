2d1d36604c005f543f67e480c612b732089e6d88
An Chen <anchen@guoku.com>
2015-09-06 00:32:29 +0800

    add article detail edit view

--- nut/settings/dev_anchen.py
+++ nut/settings/dev_anchen.py
@@ -2,11 +2,11 @@ from stage import *
 DEBUG = True
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 #
-# IMAGE_HOST = 'http://127.0.0.1:9766/'
-# AVATAR_HOST = 'http://imgcdn.guoku.com/'
-
-IMAGE_HOST = 'http://imgcdn.guoku.com/'
+IMAGE_HOST = 'http://127.0.0.1:9766/'
 AVATAR_HOST = 'http://imgcdn.guoku.com/'
+
+# IMAGE_HOST = 'http://imgcdn.guoku.com/'
+# AVATAR_HOST = 'http://imgcdn.guoku.com/'
 
 INTRANET_IMAGE_SERVER = 'http://images.hello.new/'
 
