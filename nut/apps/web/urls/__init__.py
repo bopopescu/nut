a69aead301ff3b039fd0c5950020e51de84080e6
jiaxin <edison7500@gmail.com>
2015-09-05 16:50:08 +0800

    update

--- nut/apps/web/urls/__init__.py
+++ nut/apps/web/urls/__init__.py
@@ -98,7 +98,7 @@ urlpatterns += patterns(
     url(r'^account/', include('apps.web.urls.account')),
     url(r'^u/', include('apps.web.urls.user')),
     url(r'^event/', include('apps.web.urls.event')),
-    url(r'^t/', include('apps.web.urls.tag')),
+    # url(r'^t/', include('apps.web.urls.tag')),
     # url(r'^tag/(?P<tag_text>\w+)/$', 'tag.text_to_detail', name='web_tag_text',),
     url(r'^articles/',include('apps.web.urls.article')),
 
--- nut/apps/web/urls/tag.py
+++ nut/apps/web/urls/tag.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, patterns
+from apps.web.views.tag import TagHashView
 
 urlpatterns = patterns(
     'apps.web.views.tag',
-    url(r'^(?P<hash>\w+)/$', 'detail', name='web_tag_detail'),
+    # url(r'^(?P<hash>\w+)/$', 'detail', name='web_tag_detail'),
+    url(r'^(?P<hash>\w+)/$', TagHashView.as_view(), name='web_tag_detail'),
 )
 
 __author__ = 'edison'
