66b54878754559b01118f7ffe5424dcefca3f297
An Chen <anchen@guoku.com>
2015-09-08 12:38:41 +0800

    fix global context bug

--- nut/apps/web/templatetags/common.py
+++ nut/apps/web/templatetags/common.py
@@ -32,9 +32,9 @@ def format_time(value):
     elif time_interval < 60 * 60 * 72:
         return "前天"
     elif time_interval < 60*60*168:
-        return "%d天前"%(time_interval/(60*60*24) + 1)
+        return "%d天前"%(time_interval/(60*60*24))
     else:
-        return "%d周前"%(time_interval/(60*60*24*7) + 1)
+        return "%d周前"%(time_interval/(60*60*24*7))
     # NEVER GO HERE
     return "很久很久以前"
 
