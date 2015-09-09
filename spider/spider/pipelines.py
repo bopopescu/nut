c4a88f478e153d99d8249ae6d88cdbc472c0a2af
Judy <judy.programer@gmail.com>
2015-09-09 16:37:09 +0800

    need to fix ssl poblem first.

--- spider/spider/pipelines.py
+++ spider/spider/pipelines.py
@@ -12,7 +12,6 @@ import MySQLdb.cursors
 
 
 class OriginIdPipeline(object):
-
     def process_item(self, item, spider):
         # print "KOKOKOKOKO",item
         # if item.has_key('origin_id'):
@@ -21,6 +20,7 @@ class OriginIdPipeline(object):
             return item
         else:
             raise DropItem(item)
+
 
 class ErrorPipeLine(object):
     def process_item(self, item, spider):
@@ -31,7 +31,6 @@ class ErrorPipeLine(object):
 
 
 class DuplicatesPipeline(object):
-
     def __init__(self):
         self.ids_seen = set()
 
@@ -44,30 +43,42 @@ class DuplicatesPipeline(object):
 
 
 class SQLStorePipeline(object):
-
     def __init__(self):
         self.dbpool = adbapi.ConnectionPool('MySQLdb', db='core',
-                                            host='10.0.2.90',
-                                            user='guoku', passwd='guoku!@#', cursorclass=MySQLdb.cursors.DictCursor,
+                                            host='127.0.0.1',
+                                            port='3306',
+                                            user='root', passwd='',
+                                            cursorclass=MySQLdb.cursors.DictCursor,
                                             charset='utf8', use_unicode=True)
 
     def process_item(self, item, spider):
-        query = self.dbpool.runInteraction(self._conditional_update, item, spider)
+        query = self.dbpool.runInteraction(self._conditional_update, item,
+                                           spider)
         query.addErrback(self.handle_error, spider)
         return query
 
-    def _conditional_update(self, tx, item, spider, selection=False):
+    def _conditional_update(self, tx, item, spider):
         print '=' * 80
         import pdb; pdb.set_trace()
         try:
+            if item['update_selection_status']:
+                sql = 'UPDATE core_selection_entity SET is_published=0 where' \
+                      ' entity_id = (SELECT entity_id FROM core.core_buy_link ' \
+                      'where origin_id = %s);' % item['origin_id']
             if item['status'] == 0:
-                sql = 'UPDATE core_buy_link SET status = %s where origin_id = %s' % (item['status'], item['origin_id'])
-            elif item['price'] ==0:
-                sql = 'UPDATE core_buy_link SET status = %s, shop_link = "%s", seller="%s" where origin_id = %s' % \
-                      (item['status'], item['shop_link'], item['seller'], item['origin_id'] )
+                sql = 'UPDATE core_buy_link SET status = %s where origin_id ='\
+                      ' %s' % (item['status'], item['origin_id'])
+            elif item['price'] == 0:
+                sql = 'UPDATE core_buy_link SET status = %s,' \
+                      ' shop_link = "%s", seller="%s" where origin_id = %s' % \
+                      (item['status'], item['shop_link'], item['seller'],
+                       item['origin_id'] )
             else:
-                sql = 'UPDATE core_buy_link SET status = %s, shop_link = "%s", seller="%s", price="%s" where origin_id = %s' % \
-                      (item['status'], item['shop_link'], item['seller'], item['price'], item['origin_id'] )
+                sql = 'UPDATE core_buy_link SET status = %s,' \
+                      ' shop_link = "%s", seller="%s", price="%s" ' \
+                      'where origin_id = %s' % \
+                      (item['status'], item['shop_link'], item['seller'],
+                       item['price'], item['origin_id'] )
 
             spider.log(sql)
 
@@ -80,12 +91,12 @@ class SQLStorePipeline(object):
     def handle_error(self, e, spider):
         spider.log(e)
 
-# class JsonWriterPipeline(object):
-#
-#     def __init__(self):
-#         self.file = open('items.jl', 'ab')
-#
-#     def process_item(self, item, spider):
-#         line = json.dumps(dict(item)) + "\n"
-#         self.file.write(line)
-#         return item
+        # class JsonWriterPipeline(object):
+        #
+        # def __init__(self):
+        #         self.file = open('items.jl', 'ab')
+        #
+        #     def process_item(self, item, spider):
+        #         line = json.dumps(dict(item)) + "\n"
+        #         self.file.write(line)
+        #         return item
--- spider/spider/spiders/taobao.py
+++ spider/spider/spiders/taobao.py
@@ -3,6 +3,7 @@ import scrapy
 import re
 from spider.items import GItem
 
+
 IMG_POSTFIX = "_\d+x\d+.*\.jpg|_b\.jpg"
 
 
@@ -10,8 +11,10 @@ class TaobaoSpider(scrapy.Spider):
     name = "taobao"
     allowed_domains = ["taobao.com"]
 
-    def __init__(self, item_id, selection=False, *args, **kwargs):
+    def __init__(self, item_id, update_selection_status=False, *args,
+                 **kwargs):
         super(TaobaoSpider, self).__init__(*args, **kwargs)
+        self.update_selection_status = update_selection_status
         self.item_id = item_id
         self.start_urls = [
             "http://item.taobao.com/item.html?id=%s" % self.item_id
@@ -21,6 +24,8 @@ class TaobaoSpider(scrapy.Spider):
         # self.log(response.headers)
         item = GItem()
 
+        item['update_selection_status'] = self.update_selection_status
+        item['entity_id'] = s
         try:
             item['origin_id'] = response.headers['At_Itemid']
         except KeyError:
@@ -28,6 +33,7 @@ class TaobaoSpider(scrapy.Spider):
         item['link'] = response.url
 
         item['status'] = self.get_item_status(response)
+        item['update_selection_status'] = self.update_selection_status
 
         if item['status'] == 0:
             return item
@@ -49,8 +55,8 @@ class TaobaoSpider(scrapy.Spider):
 
         if shopId != 0:
             item['shop_link'] = "http://shop%s.taobao.com" % shopId
-                # shopId = m.group()
-        #     print re.findall('shopId:"(\d+)', row)
+            # shopId = m.group()
+            # print re.findall('shopId:"(\d+)', row)
             # self.log(row)
         # self.log(shopId)
         # price = self.get_price(response)
@@ -60,7 +66,7 @@ class TaobaoSpider(scrapy.Spider):
         # optimgs = response.xpath('//ul[@id="J_UlThumb"]/li/div/a/img/@data-src').extract()
         # # self.log(optimgs)
         # if len(optimgs) > 1:
-        #     origin_images += optimgs[1:]
+        # origin_images += optimgs[1:]
         # for img in origin_images:
         #     if 'http' not in img:
         #         img = 'http:' + img
@@ -87,7 +93,8 @@ class TaobaoSpider(scrapy.Spider):
         item['cid'] = cat_id
         item['image_urls'] = self.get_images(response)
 
-        soldout = response.xpath('//p[@class="tb-hint"]/strong/text()').extract()
+        soldout = response.xpath(
+            '//p[@class="tb-hint"]/strong/text()').extract()
         if len(soldout) > 0:
             item['status'] = 1
         # item['link'] = response.url
@@ -96,8 +103,10 @@ class TaobaoSpider(scrapy.Spider):
 
     def get_images(self, response):
         images = list()
-        origin_images = response.xpath('//img[@id="J_ImgBooth"]/@data-src').extract()
-        optimgs = response.xpath('//ul[@id="J_UlThumb"]/li/div/a/img/@data-src').extract()
+        origin_images = response.xpath(
+            '//img[@id="J_ImgBooth"]/@data-src').extract()
+        optimgs = response.xpath(
+            '//ul[@id="J_UlThumb"]/li/div/a/img/@data-src').extract()
         # self.log(optimgs)
         if len(optimgs) > 1:
             origin_images += optimgs[1:]
@@ -110,7 +119,8 @@ class TaobaoSpider(scrapy.Spider):
         return images
 
     def get_price(self, response):
-        price = response.xpath('//strong[@id="J_StrPrice"]/em[@class="tb-rmb-num"]/text()').extract()
+        price = response.xpath(
+            '//strong[@id="J_StrPrice"]/em[@class="tb-rmb-num"]/text()').extract()
         if len(price) == 0:
             return 0
             # price = response.xpath('//dl[@id="J_StrPriceModBox"]/dd').extract()
@@ -128,10 +138,12 @@ class TaobaoSpider(scrapy.Spider):
         if 'noitem.htm' in response.url:
             return 0
             # return item
-        soldout = response.xpath('//p[@class="tb-hint"]/strong/text()').extract()
+        soldout = response.xpath(
+            '//p[@class="tb-hint"]/strong/text()').extract()
         if len(soldout) > 0:
             return 1
-        soldout = response.xpath('//strong[@class="sold-out-tit"]/text()').extract()
+        soldout = response.xpath(
+            '//strong[@class="sold-out-tit"]/text()').extract()
         if len(soldout) > 0:
             return 1
 
