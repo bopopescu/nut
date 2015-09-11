24a34173c530928ad4e4bb1edf501775c3f5b47d
An Chen <anchen@guoku.com>
2015-09-11 21:25:09 +0800

    update api permissions

--- nut/apps/api/permissions/__init__.py
+++ nut/apps/api/permissions/__init__.py
@@ -3,4 +3,6 @@ from  rest_framework import permissions
 
 class Admin_And_Editor_Only(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user.is_admin or request.user.is_editor
+        return (hasattr(request.user,'is_admin') and \
+               hasattr(request.user,'is_editor')) and \
+               (request.user.is_admin or request.user.is_editor)
--- nut/apps/api/views/friendly_link.py
+++ nut/apps/api/views/friendly_link.py
@@ -1,16 +1,16 @@
 from apps.core.models import Friendly_Link
 from apps.api.serializers.friendly_link import Friendly_Link_Serializer
-
+from rest_framework.permissions import  IsAdminUser
 from rest_framework import generics
 from apps.api.permissions import Admin_And_Editor_Only
 
 class RFFriendlyLinkListView(generics.ListCreateAPIView):
-    permission_classes = (Admin_And_Editor_Only,)
+    permission_classes = (IsAdminUser,)
     queryset =  Friendly_Link.objects.all()
     serializer_class = Friendly_Link_Serializer
 
 class RFFriendlyLinkDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (Admin_And_Editor_Only)
+    permission_classes = (IsAdminUser,)
     queryset = Friendly_Link.objects.all()
     serializer_class = Friendly_Link_Serializer
 
