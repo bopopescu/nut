02932031b03486ba9c4c767e3a9b12f8945900e8
An Chen <anchen@guoku.com>
2015-09-24 18:44:09 +0800

    image counter in the middle

--- nut/apps/counter/urls/__init__.py
+++ nut/apps/counter/urls/__init__.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include, patterns
 
-from apps.counter.views import Counter
+from apps.counter.views import Counter, ArticleImageCounter
 
 urlpatterns = patterns(
     'apps.counter.views',
     url(r'^$',Counter.as_view() ,name='backend_counter'),
+    url(r'feed/article/(?P<aid>\d+)/guoku_banner.jpg', ArticleImageCounter.as_view(),name='article_image_count')
     )
