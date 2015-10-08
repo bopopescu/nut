e7c64a00bed2dce26695a403fd7a9307029224e1
An Chen <anchen@guoku.com>
2015-10-08 13:27:33 +0800

    add article related prodcuts , manytomany relation

--- nut/settings/dev_anchen.py
+++ nut/settings/dev_anchen.py
@@ -48,7 +48,7 @@ CACHES = {
 def removeDebugToolBar(theList):
     return [x  for x in theList if x!='debug_toolbar']
 
-# INSTALLED_APPS = removeDebugToolBar(INSTALLED_APPS)
+INSTALLED_APPS = removeDebugToolBar(INSTALLED_APPS)
 
 #-------------------------debug end --------------------
 
