2f4b80eeb9448934193a8f31407e8a34cd1c32a6
An Chen <anchen@guoku.com>
2015-10-08 15:34:01 +0800

    update

--- nut/static/js/management/tag/tag.js
+++ nut/static/js/management/tag/tag.js
@@ -1,5 +1,32 @@
 var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
 
-elems.forEach(function(html) {
-  var switchery = new Switchery(html);
+function postSuccess(data){
+    console.log(data);
+}
+function postFail(data){
+    console.log(data);
+}
+
+function switchChange(){
+    console.log(this);
+    console.log(this['data-id']);
+    console.log($(this).prop('checked'));
+    console.log($(this).attr('data-id'));
+    var tag_id = $(this).attr('data-id');
+    var url = '/management/t/' + tag_id + '/topArticleTag/' ;
+    var data =  {isTopArticleTag: $(this).prop('checked')};
+
+    when($.ajax({
+        type:'POST',
+        url: url,
+        data: data,
+    })).then(
+        postSuccess,
+        postFail
+    );
+}
+
+elems.forEach(function(ele) {
+  var switchery = new Switchery(ele);
+      ele.onchange = switchChange;
 });
