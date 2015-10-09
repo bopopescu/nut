54b2bdcc093d3037b86d8bd765aac3d41b6b99cc
An Chen <anchen@guoku.com>
2015-10-09 17:39:29 +0800

    update tmall

--- nut/apps/web/utils/datatools.py
+++ nut/apps/web/utils/datatools.py
@@ -2,6 +2,9 @@ import re
 
 def get_entity_list_from_article_content(content):
     regstr = r"data_entity_hash=(\w+)"
+    regObj = re.compile(regstr, re.MULTILINE)
+    result = regObj.match(content)
+    return result.groups()
 
-
-
+if __name__ == '__main__':
+    content = 'content'
