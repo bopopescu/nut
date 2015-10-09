e76eaf4359518c9881c3a7215be14012f3f5e029
An Chen <anchen@guoku.com>
2015-10-09 18:55:25 +0800

    add related article when save article

--- nut/apps/web/utils/datatools.py
+++ nut/apps/web/utils/datatools.py
@@ -1,10 +1,12 @@
 import re
 
 def get_entity_list_from_article_content(content):
-    regstr = r"data_entity_hash=(\w+)"
+    regstr = r"data_entity_hash=\"(\w+)\""
     regObj = re.compile(regstr, re.MULTILINE)
-    result = regObj.match(content)
-    return result.groups()
+    result = regObj.findall(content)
+    return result
 
 if __name__ == '__main__':
-    content = 'content'
+    content = '<div guoku_ele="True" class="guoku-card container-fluid" data_entity_hash="9041c999"><div guoku_ele="True" class="guoku-card container-fluid" data_entity_hash="90423c999">'
+    print(get_entity_list_from_article_content(content))
+
