13abd3806c96d97d87f999adf1e9e174470aa038
An Chen <anchen@guoku.com>
2015-10-14 13:44:33 +0800

    update article top tag style

--- nut/apps/tag/urls/__init__.py
+++ nut/apps/tag/urls/__init__.py
@@ -6,8 +6,8 @@ urlpatterns = patterns(
     'apps.tag.views',
     url(r'^$', TagListView.as_view(), name='tag_list_url'),
     # url(r'^(?P<tag_name>.*)/$', TagEntityView.as_view(), name='tag_entities_url'),
-    url(r'^(?P<tag_hash>\w+)/$', TagEntityView.as_view(), name='tag_entities_url'),
     url(r'^articles/(?P<tag_name>.*)/$', NewTagArticleView.as_view(), name='tag_articles_url'),
+    url(r'^(?P<tag_hash>\w+)/$', TagEntityView.as_view(), name='tag_entities_url'),
 
 
 )
