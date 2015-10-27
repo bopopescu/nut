708ecf05a3ae3718361d92be0fd6428e4dc680af
An Chen <anchen@guoku.com>
2015-10-27 11:30:58 +0800

    update

--- nut/static/js/web/app/utils/EntityLike.js
+++ nut/static/js/web/app/utils/EntityLike.js
@@ -1,8 +1,11 @@
-define(['libs/Class'], function(Class){
+define(['libs/Class','jquery','fastdom'], function(Class,jQuery,fastdom){
 
     var AppEntityLike = Class.extend({
         init: function(){
             console.log('app entity like functions');
+            console.log(jQuery);
+            console.log(fastdom);
+
 
         }
     });
