1be31bd20c3241690aa03dc0cd2e38c768811433
An Chen <anchen@guoku.com>
2015-11-13 15:49:44 +0800

    entity page new front end start

---
+++ nut/static/js/web/app/entity_detail_app.js
@@ -0,0 +1,27 @@
+require([
+        'libs/polyfills',
+        'jquery',
+        'subapp/page',
+        'subapp/topmenu',
+        'subapp/gotop',
+        'subapp/detailsidebar',
+        'subapp/entitylike',
+        'libs/csrf',
+
+    ],
+    function (polyfill,
+              jQuery,
+              Page,
+              Menu,
+              GoTop,
+              SideBarManager,
+              EntityLike
+
+    ){
+        var page = new Page();
+        var menu = new Menu();
+        var goto = new GoTop();
+        var sidebar = new SideBarManager();
+        var entityLike  =new EntityLike();
+        console.log("entity detail init!！");
+});
