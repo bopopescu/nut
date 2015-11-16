439a21b9209c2547cf139061bdbd76e9fcbe41a5
An Chen <anchen@guoku.com>
2015-11-16 15:03:35 +0800

    entity report working again

--- nut/static/js/web/app/subapp/entityreport.js
+++ nut/static/js/web/app/subapp/entityreport.js
@@ -10,6 +10,7 @@ define(['jquery',
 
     var EntityReport = Class.extend({
         init: function(){
+             var that  = this ;
              $('#report_trigger').click(function(){
                     var url = $(this).attr('report-url');
                     $.when($.ajax({
@@ -25,7 +26,7 @@ define(['jquery',
                                     success:{
                                         label:'发送',
                                         className:'btn-primary',
-                                        callback: sendReport
+                                        callback: that.sendReport.bind(that)
                                     },
                                 }
                             });
@@ -33,7 +34,29 @@ define(['jquery',
                             console.log('get report form fail ');
                         });
                 });
-        }
+        },
+        sendReport: function(){
+            var _form = $('#report_form_wrapper form');
+                if (_form.length){
+                    $.when($.ajax({
+                        url: _form.attr('action'),
+                        data: _form.serialize(),
+                        method: 'POST',
+                    })).then(
+                        function reportSuccess(data){
+                            return bootbox.alert({
+                                size: 'small',
+                                message: '举报发送成功，感谢您的关心。'
+                            }) ;
+                        },
+                        function reportFail(){
+                            return ;
+                        }
+                    );
+                }
+            }
+
+
     });
 
         return EntityReport;
