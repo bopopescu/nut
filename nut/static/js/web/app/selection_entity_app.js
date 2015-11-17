0797d0370c72141758803be1d2e42825278da301
Judy <judy.programer@gmail.com>
2015-11-17 14:39:33 +0800

    add bower

--- nut/static/js/web/app/selection_entity_app.js
+++ nut/static/js/web/app/selection_entity_app.js
@@ -1,57 +1,23 @@
-requirejs.config({
-    baseUrl:base_url,
-    paths: {
-        libs: './libs',
-        utils: './utils',
-        subapp: './subapp',
-        jquery: 'libs/jquery-1.11.1.min',
-        bootstrap: 'libs/bootstrap.min',
-        fastdom: 'libs/fastdom.ant',
-        csrf:'libs/csrf',
-        underscore:'libs/underscore.ant',
-        cookie: 'libs/jquery.cookie'
-    },
-
-    shim: {
-// shim won't handle script load , you still need require script in your source
-        'cookie':{
-            deps:['jquery']
-        },
-        'csrf':{
-            deps:['jquery']
-        },
-        'bootstrap':{
-            deps:['jquery']
-        },
-        'jquery':{
-            exports:'jQuery'
-        },
-        'underscore':{
-            exports: '_'
-        }
-    }
-});
-
 require([
         'libs/polyfills',
         'jquery',
         'utils/EntityLike',
         'subapp/topmenu',
-        'subapp/loadentity',
+        //'subapp/loadentity',
         'subapp/gotop',
+        'subapp/scrollview_selection',
     ],
+
     function (polyfill,
               jQuery,
               AppEntityLike,
               Menu,
-              LoadEntity,
+              ScrollEntity,
               GoTop) {
 // TODO : check if csrf work --
 // TODO : make sure bind is usable
         var menu = new Menu();
         var app_like = new AppEntityLike();
-        var app_loadEntity = new LoadEntity();
+        var app_scrollentity = new ScrollEntity();
         var goto = new GoTop();
     });
-
-
