1290984e7ca69671606dada29b47399a84861503
jiaxin <edison7500@gmail.com>
2015-11-20 14:51:22 +0800

    update

--- nut/apps/mobile/forms/__init__.py
+++ nut/apps/mobile/forms/__init__.py
@@ -3,6 +3,9 @@ from apps.core.utils.image import HandleImage
 
 from django import forms
 from django.utils.translation import gettext_lazy as _
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+
 from django.conf import settings
 
 image_path = getattr(settings, 'MOGILEFS_MEDIA_URL', 'images/')
@@ -15,7 +18,7 @@ class LaunchBoardForm(forms.Form):
         (1, _('yes')),
     )
     # forms.EmailField
-    launchImage = forms.FileField(widget=forms.FileInput(), required=False)
+    launchImage = forms.ImageField(widget=forms.FileInput(attrs={'class':'controls'}))
     title = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     description = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     action = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
@@ -28,6 +31,10 @@ class LaunchBoardForm(forms.Form):
         initial=0,
     )
 
+    def clean_status(self):
+        _status = self.cleaned_data.get('status')
+        return int(_status)
+
     def save(self):
         pass
 
@@ -35,17 +42,21 @@ class LaunchBoardForm(forms.Form):
 class CreateLaunchBoardForm(LaunchBoardForm):
 
     def save(self):
-        _image = self.cleaned_data.get('image')
+        _image = self.cleaned_data.get('launchImage')
         _title = self.cleaned_data.get('title')
         _description = self.cleaned_data.get('description')
-
-        # if _image:
-        launch_image =  HandleImage(_image)
+        _action = self.cleaned_data.get('action')
+        _status = self.cleaned_data.get('status')
+        # print _image
+        launch_image = HandleImage(_image)
         image_name = image_path + "%s.jpg" % launch_image.name
+        default_storage.save(image_name, ContentFile(launch_image.image_data))
         launch = LaunchBoard()
         launch.launchImage = image_name
         launch.title = _title
         launch.description = _description
+        launch.action = _action
+        launch.status = _status
         launch.save()
 
 
