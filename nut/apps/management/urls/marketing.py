d55239efae6e4863445fbedbb765764a5b8bd5b6
jiaxin <edison7500@gmail.com>
2015-11-21 21:36:13 +0800

    update

--- nut/apps/management/urls/marketing.py
+++ nut/apps/management/urls/marketing.py
@@ -5,5 +5,5 @@ urlpatterns = patterns(
     'apps.management.views.marketing',
     url(r'^$', LaunchBoardListView.as_view(), name='management_marketing'),
     url(r'^create/$', NewLaunchBoardView.as_view(), name='management_create_launch_image'),
-    url(r'^(?P<lid>\d+)/edit/$', EditLaunchBoardView.as_view(), name='management_edit_launch_image'),
+    url(r'^(?P<pk>\d+)/edit/$', EditLaunchBoardView.as_view(), name='management_edit_launch_image'),
 )
