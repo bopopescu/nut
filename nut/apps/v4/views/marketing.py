65ac0cbb9cc825ae0ad74746264333e51069de9d
jiaxin <edison7500@gmail.com>
2015-11-22 20:33:54 +0800

    update

--- nut/apps/v4/views/marketing.py
+++ nut/apps/v4/views/marketing.py
@@ -1,11 +1,21 @@
 from apps.mobile.models import LaunchBoard
 from apps.core.views import BaseJsonView
+from apps.mobile.lib.sign import check_sign
 
 
 class LaunchBoardView(BaseJsonView):
 
     def get_data(self, context):
+        res = {}
+        launch = LaunchBoard.objects.filter(status = True).first()
+        if launch:
+            res['title'] = launch.title
+            res['description'] = launch.description
+            res['action'] = launch.action
+        return res
 
-        return context
+    # @check_sign
+    def dispatch(self, request, *args, **kwargs):
+        return super(LaunchBoardView, self).dispatch(request, *args, **kwargs)
 
 __author__ = 'edison'
