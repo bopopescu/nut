9d72a7cb0253f933c195aedd381473cddf84a91f
jiaxin <edison7500@gmail.com>
2015-11-24 13:28:30 +0800

    update

--- nut/apps/v4/views/marketing.py
+++ nut/apps/v4/views/marketing.py
@@ -1,6 +1,7 @@
 from apps.mobile.models import LaunchBoard
 from apps.core.views import BaseJsonView
 from apps.mobile.lib.sign import check_sign
+# from apps.core.utils.http import ErrorJsonResponse
 
 
 class LaunchBoardView(BaseJsonView):
@@ -12,12 +13,13 @@ class LaunchBoardView(BaseJsonView):
             res['launch_id'] = launch.pk
             res['title'] = launch.title
             res['description'] = launch.description
+            res['action_title'] = launch.action_title
             res['action'] = launch.action
             res['launch_image_url'] = launch.launch_image_url
+            return res
+        return None
 
-        return res
-
-    # @check_sign
+    @check_sign
     def dispatch(self, request, *args, **kwargs):
         return super(LaunchBoardView, self).dispatch(request, *args, **kwargs)
 
