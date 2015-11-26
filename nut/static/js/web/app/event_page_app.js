4c1a95dc2ed96f8a48d6b19702cc82f28fefe057
An Chen <anchen@guoku.com>
2015-11-26 11:08:37 +0800

    add event page app

---
+++ nut/static/js/web/app/event_page_app.js
@@ -0,0 +1,12 @@
+require(['jquery',
+         'subapp/topmenu',
+          'subapp/gotop'],
+    function($,Menu,GoTop){
+
+
+
+        var menu = new Menu();
+        var goto = new GoTop();
+
+
+    });
