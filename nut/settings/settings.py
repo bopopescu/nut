70599f25581cb04def5f012669f0a8e1072869d9
jiaxin <edison7500@gmail.com>
2015-11-26 14:58:36 +0800

    merge

--- nut/settings/settings.py
+++ nut/settings/settings.py
@@ -68,7 +68,7 @@ HAYSTACK_CONNECTIONS = {
     'default': {
         # 'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
         'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
-        'URL': 'http://10.0.2.120:8983/solr/',
+        'URL': 'http://10.0.2.110:8983/solr/',
         'INCLUDE_SPELLING': True,
         # 'PATH': os.path.join(os.path.dirname(__file__), '../whoosh_index'),
     }
