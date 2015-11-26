ae5cbb839faa8360f8bc629acda1601638c542e2
An Chen <anchen@guoku.com>
2015-11-26 14:55:23 +0800

    update

--- nut/static/js/web/jsbuild/discover_app_build.js
+++ nut/static/js/web/jsbuild/discover_app_build.js
@@ -1002,8 +1002,6 @@ define('subapp/topmenu',['bootstrap', 'libs/Class','underscore','jquery', 'fastd
             //$(window).scroll(_.debounce(this.show.bind(this), 100));
         },
         scheduleHeaderMove:function(){
-
-
             var that = this;
             if (!this.read){
                 this.read = fastdom.read(function(){
@@ -1016,11 +1014,6 @@ define('subapp/topmenu',['bootstrap', 'libs/Class','underscore','jquery', 'fastd
             }
 
             this.write = fastdom.write(this.moveHeader.bind(this));
-
-            //console.log('onscroll');
-            //var t = new Date();
-            //console.log(t.getMilliseconds());
-            //console.log()
         },
         moveHeader:function(){
             //console.log('move header');
