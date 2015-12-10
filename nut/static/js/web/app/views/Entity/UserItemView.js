a7ffd4a110db1f22af6232b962c5b30f4f629ef5
An Chen <anchen@guoku.com>
2015-12-10 19:24:48 +0800

    update userItemView

--- nut/static/js/web/app/views/Entity/UserItemView.js
+++ nut/static/js/web/app/views/Entity/UserItemView.js
@@ -1,9 +1,29 @@
-define(['views/base/ItemView'], function(
-    ItemView
+define(['views/base/ItemView', 'jquery', 'underscore'], function(
+    ItemView,
+    $,
+    _
 ){
     var UserItemView = ItemView.extend({
-        template:'#user_cell_template',
         tagName: 'li',
+        className: 'user-icon-cell',
+        template: _.template($('#user_cell_template').html()),
+        initialize: function(){
+        },
+        render:function(){
+            this.sizingAvatar();
+            return ItemView.prototype.render.call(this);
+
+        },
+        sizingAvatar: function(){
+            var user = this.model.get('user');
+            var avatar = user['avatar_url'];
+            if ('imgcdn.guoku.com' in avatar){
+                avatar = avatar.replace('/avatar','/avatar/180');
+                user['avatar_url'] = avatar;
+                this.model.set('user', user);
+            }
+        }
+
     });
     return UserItemView;
 
