cc2447c9334d5ee6eee7efaa8b09209342621ef3
AzureRay <476468781@qq.com>
2015-12-18 14:56:59 +0800

    backup

---
+++ nut/apps/seller/views/web.py
@@ -0,0 +1,11 @@
+from django.views.generic import TemplateView
+from django.forms import ModelForm, TextInput
+from django.forms.fields import IntegerField
+from django.forms.widgets import TextInput
+from django.core.exceptions import ValidationError
+from apps.seller.models import Seller_Profile
+from apps.core.models import GKUser
+
+
+class SellerView(TemplateView):
+    template_name = 'web/seller/web_seller.html'
