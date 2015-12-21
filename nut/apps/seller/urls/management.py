7f7307b7354e3eabf8c74156926a417e08b2b5d5
An Chen <anchen@guoku.com>
2015-12-21 20:31:10 +0800

    update seller profile templates and views

--- nut/apps/seller/urls/management.py
+++ nut/apps/seller/urls/management.py
@@ -7,12 +7,10 @@ from apps.seller.views.management import SellerCreateView, \
                                          SellerSectionUpdateView, \
                                          SellerSectionListView
 urlpatterns = patterns(
-    '',
-    url(r'^/$', SellerListView.as_view() , name='management_seller_list'),
-    url(r'^edit/(?P<eid>\d+)/$', SellerUpdateView.as_view() , name='management_seller_update'),
+    'apps.seller.views.management',
+    url(r'^$', SellerListView.as_view() , name="management_seller_list"),
+    url(r'^edit/(?P<pk>\d+)/$', SellerUpdateView.as_view() , name='management_seller_update'),
     url(r'^new/$', SellerCreateView.as_view() , name='management_seller_create'),
-
-#     section management
 
     url(r'^section/edit/(?P<eid>\d+)/$', SellerSectionUpdateView.as_view() , name='management_seller_section_update'),
     url(r'^section/new/$', SellerSectionCreateView.as_view() , name='management_seller_section_create'),
--- nut/apps/seller/views/management.py
+++ nut/apps/seller/views/management.py
@@ -1,9 +1,10 @@
 from django.views.generic import CreateView, UpdateView, ListView
 from django.forms import ModelForm, TextInput
-from django.forms.fields import IntegerField
+from django.forms.fields import IntegerField,ImageField
 from django.forms.widgets import TextInput
 from django.core.exceptions import ValidationError
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.utils.functional import lazy
 
 from apps.seller.models import Seller_Profile, Seller_Section
 from apps.core.models import GKUser
@@ -13,7 +14,8 @@ from apps.core.models import GKUser
 
 
 class SellerForm(ModelForm):
-    seller_user_id = IntegerField(required=False)\
+    seller_user_id = IntegerField(required=False)
+    seller_logo_image = ImageField(label='Select an Image', help_text='for Seller logo size: Max size (那姐提供)')
 
     def __init__(self, *args, **kwargs):
         super(SellerForm, self).__init__(*args, **kwargs)
@@ -25,11 +27,15 @@ class SellerForm(ModelForm):
         self.fields['shop_desc'].widget.attrs.update({'class':'form-control'})
         self.fields['status'].widget.attrs.update({'class':'form-control'})
         self.fields['business_section'].widget.attrs.update({'class':'form-control'})
+        self.fields['seller_logo_image'].widget.attrs.update({'class':'form-control'})
+        self.fields['gk_stars'].widget.attrs.update({'class':'form-control'})
 
     class Meta:
         model = Seller_Profile
-        fields = ['seller_user_id','seller_name','logo','shop_title',\
-                  'shop_link', 'shop_desc', 'status', 'business_section']
+        fields = ['seller_user_id','seller_name','logo','seller_logo_image',\
+                  'shop_title','shop_link', 'shop_desc', 'status', 'business_section',\
+                  'gk_stars'
+                  ]
 
         # widgets = {
         #     'seller_user_id': TextInput(attrs={'class': 'user-id-input form-control'}),
@@ -57,8 +63,8 @@ class SellerCreateForm(SellerForm):
 
 
 class SellerCreateView(CreateView):
-    success_url = '/'
-
+    def get_success_url(self):
+        return reverse('management_seller_list')
     form_class = SellerCreateForm
     template_name = 'management/seller/create.html'
     model = Seller_Profile
@@ -68,7 +74,8 @@ class SellerCreateView(CreateView):
 
 class SellerListView(ListView):
     model = Seller_Profile
-    template_name = '/management/seller/section_list.html'
+    template_name = 'management/seller/list.html'
+    context_object_name = 'sellers'
     def get_queryset(self):
         return Seller_Profile.objects.filter(status=Seller_Profile.active)
 
