c213f117b75948ada58eb76bcc9b4f161d022e20
An Chen <anchen@guoku.com>
2015-12-21 21:43:52 +0800

    upate seller form

--- nut/apps/seller/urls/management.py
+++ nut/apps/seller/urls/management.py
@@ -2,19 +2,14 @@ from django.conf.urls import url, patterns
 
 from apps.seller.views.management import SellerCreateView, \
                                          SellerListView, \
-                                         SellerUpdateView,\
-                                         SellerSectionCreateView,\
-                                         SellerSectionUpdateView, \
-                                         SellerSectionListView
+                                         SellerUpdateView
+
 urlpatterns = patterns(
     'apps.seller.views.management',
     url(r'^$', SellerListView.as_view() , name="management_seller_list"),
     url(r'^edit/(?P<pk>\d+)/$', SellerUpdateView.as_view() , name='management_seller_update'),
     url(r'^new/$', SellerCreateView.as_view() , name='management_seller_create'),
 
-    url(r'^section/edit/(?P<eid>\d+)/$', SellerSectionUpdateView.as_view() , name='management_seller_section_update'),
-    url(r'^section/new/$', SellerSectionCreateView.as_view() , name='management_seller_section_create'),
-    url(r'^section/$', SellerSectionListView.as_view() , name='management_seller_section_list'),
 
 
 )
