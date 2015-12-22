6c74f99d0b5c77905c224bab5099a441b89a12ab
An Chen <anchen@guoku.com>
2015-12-22 14:41:03 +0800

    hide page

--- nut/settings/dev_anchen.py
+++ nut/settings/dev_anchen.py
@@ -8,8 +8,6 @@ DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
 IMAGE_HOST = 'http://imgcdn.guoku.com/'
 
-
-
 #
 # LOCAL_IMG_DEBUG=True
 # IMAGE_HOST = 'http://127.0.0.1:9766/'
