801e9cf07d7f5c4045b4ffb05907590032f6ce71
jiaxin <edison7500@gmail.com>
2015-12-24 17:00:22 +0800

    update

--- nut/apps/management/views/tbrecommend.py
+++ nut/apps/management/views/tbrecommend.py
@@ -13,16 +13,21 @@ class RecommendView(TemplateView):
         context = super(RecommendView, self).get_context_data(**kwargs)
         payload = {
             'keyword':self.keyword,
+            'mall':self.mall,
         }
         r = requests.get("http://10.0.2.115:10050/recommend", params=payload)
         data = r.json()
         context.update(
-                {'object_list': data['result']}
+            {
+                'object_list': data['result'],
+                'keyword': self.keyword,
+            }
         )
         return context
 
     def get(self, request, *args, **kwargs):
         self.keyword = request.GET.get('keyword', None)
+        self.mall = request.GET.get('mall', False)
         assert self.keyword is not None
 
         return super(RecommendView, self).get(request, *args, **kwargs)
