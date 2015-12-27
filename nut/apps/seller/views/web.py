6657e3ff35c0f73844ea92816fb8cb22bb490522
An Chen <anchen@guoku.com>
2015-12-27 22:13:45 +0800

    update year seller page

--- nut/apps/seller/views/web.py
+++ nut/apps/seller/views/web.py
@@ -4,18 +4,16 @@ from apps.core.models import Entity, Article
 
 
 
-class SellerView(ListView):
+class SellerView(TemplateView):
 
     template_name = 'web/seller/web_seller.html'
-
-    model = Seller_Profile
 
     def get_seller_by_business_section(self,section):
         return Seller_Profile.objects.filter(business_section=section)
 
     def get_seller_entities(self, seller_queryset):
         article_list = Article.objects.filter(related_seller__in=seller_queryset)
-        entities = Entity.objects.filter(related_articles__in=article_list).distinct('id')
+        entities = Entity.objects.filter(related_articles__in=article_list)
         return entities
 
     def get_seller_section_entities(self, section):
@@ -29,7 +27,7 @@ class SellerView(ListView):
         context['food_entities'] = self.get_seller_section_entities(Seller_Profile.food)
 
         context['culture_sellers'] = self.get_seller_by_business_section(Seller_Profile.culture)
-        context['culture_entities'] = self.get_seller_section_entities(Seller_Profile.food)
+        context['culture_entities'] = self.get_seller_section_entities(Seller_Profile.culture)
 
 
         context['cloth_sellers'] = self.get_seller_by_business_section(Seller_Profile.cloth)
@@ -42,8 +40,10 @@ class SellerView(ListView):
         context['blank_sellers'] = self.get_seller_by_business_section(Seller_Profile.blank)
         context['blank_entities'] = self.get_seller_section_entities(Seller_Profile.blank)
 
-        context['sellers'] = Seller_Profile.objects.all()
+        context['sellers'] = Seller_Profile.objects.all().order_by('-gk_stars')
+        context['star_range'] = xrange(5)
 
+        return context
 
 
 
