64bd0ecdca5fc71b6db69b21836d7c8629c6e8ff
An Chen <anchen@guoku.com>
2015-12-27 17:19:34 +0800

    seller view

--- nut/apps/seller/views/web.py
+++ nut/apps/seller/views/web.py
@@ -1,18 +1,52 @@
-from django.views.generic import TemplateView
-from django.forms import ModelForm, TextInput
-from django.forms.fields import IntegerField
-from django.forms.widgets import TextInput
-from django.core.exceptions import ValidationError
+from django.views.generic import TemplateView, ListView
 from apps.seller.models import Seller_Profile
-from apps.core.models import GKUser
+from apps.core.models import Entity, Article
 
 
-class SellerView(TemplateView):
+
+class SellerView(ListView):
 
     template_name = 'web/seller/web_seller.html'
 
+    model = Seller_Profile
+
+    def get_seller_by_business_section(self,section):
+        return Seller_Profile.objects.filter(business_section=section)
+
+    def get_seller_entities(self, seller_queryset):
+        article_list = Article.objects.filter(related_seller__in=seller_queryset)
+        entities = Entity.objects.filter(related_articles__in=article_list).distinct('id')
+        return entities
+
+    def get_seller_section_entities(self, section):
+        seller_queryset = self.get_seller_by_business_section(section)
+        return self.get_seller_entities(seller_queryset)
+
     def get_context_data(self, **kwargs):
-        super(SellerView, self).get_context_data(**kwargs)
+        context = super(SellerView, self).get_context_data(**kwargs)
+
+        context['food_sellers'] = self.get_seller_by_business_section(Seller_Profile.food)
+        context['food_entities'] = self.get_seller_section_entities(Seller_Profile.food)
+
+        context['culture_sellers'] = self.get_seller_by_business_section(Seller_Profile.culture)
+        context['culture_entities'] = self.get_seller_section_entities(Seller_Profile.food)
+
+
+        context['cloth_sellers'] = self.get_seller_by_business_section(Seller_Profile.cloth)
+        context['cloth_entities'] = self.get_seller_section_entities(Seller_Profile.cloth)
+
+
+        context['life_sellers'] = self.get_seller_by_business_section(Seller_Profile.life)
+        context['life_entities'] = self.get_seller_section_entities(Seller_Profile.life)
+
+        context['blank_sellers'] = self.get_seller_by_business_section(Seller_Profile.blank)
+        context['blank_entities'] = self.get_seller_section_entities(Seller_Profile.blank)
+
+        context['sellers'] = Seller_Profile.objects.all()
+
+
+
+
 
 
 
