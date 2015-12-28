142a17229ddbe412a14738a51d976098c2c9e72d
An Chen <anchen@guoku.com>
2015-12-28 17:18:53 +0800

    update search.js in management cases

--- nut/static/js/management/manage_search.js
+++ nut/static/js/management/manage_search.js
@@ -30,6 +30,7 @@ function paramToQueryString(param){
         }
 
 function handleSearch(e){
+    //TODO : this search is  for one input , update to suite  multi input
     console.log(location);
     var path = location.pathname
     var queryString = location.href.split('?')[1];
