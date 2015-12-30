661f354dc6c8f43cca3d336b735109c6cc49b83b
An Chen <anchen@guoku.com>
2015-12-30 11:43:26 +0800

    share js

--- nut/static/js/web/app/subapp/yearseller/share.js
+++ nut/static/js/web/app/subapp/yearseller/share.js
@@ -1,4 +1,4 @@
-define(['libs/Class', 'jquery', 'underscore'], function(Class,$,_){
+define(['libs/Class', 'jquery', 'underscore','bootbox'], function(Class,$,_,bootbox){
 
     //http://open.weibo.com/blog/%E5%88%86%E4%BA%AB%E6%8C%89%E9%92%AE%E7%9A%84%E5%89%8D%E4%B8%96%E4%BB%8A%E7%94%9F-%E2%80%93-%E7%8E%A9%E8%BD%AC%E6%96%B0%E6%B5%AA%E5%BE%AE%E5%8D%9A%E5%88%86%E4%BA%AB%E6%8C%89%E9%92%AE
 
@@ -17,44 +17,58 @@ define(['libs/Class', 'jquery', 'underscore'], function(Class,$,_){
                 rnd : new Date().valueOf(),
             };
 
-
-
             this.setupShareTrigger();
+            this.setupShareBox();
 
+        },
+        setupShareBox: function(){
+            $('.seller-cross-screen .share-btn').click(this.showShareDialog.bind(this));
+        },
+
+        showShareDialog: function(){
+            bootbox.dialog({
+                title: '分享',
+                onEscape: true,
+                backdrop:true,
+                closeButton: true,
+                animate: true,
+                className: 'seller-share-dialog',
+                message:'<div>here</div>'
+            });
         },
 
         setupShareTrigger: function(){
-            this.setUpSharePage();
 
-            $('.seller-cross-screen .share-btn')
-                .each(this.setupSharePageBtn.bind(this));
+            //$('.seller-cross-screen .share-btn')
+            //    .each(this.setupSharePageBtn.bind(this));
 
             $('.sellers-share .share-btn')
                 .each(this.setupShareSellerBtn.bind(this));
 
         },
 
-        setupSharePageBtn: function(ele){
+        makeUrlQueryString : function(options){
+            var paramList = [];
+            for (var key in options){
+                paramList.push(key+'='+encodeURIComponent(options[key]));
+            }
+            return  '?' + paramList.join('&');
+        },
+
+        setupSharePageBtn: function(index,ele){
             console.log(ele);
             var options = _.clone(this.shareOptions);
                 options.title = 'this is page sharing title';
                 options.pic = 'http://static.guoku.com/static/v4/6f848cd0324e89b80d3c2c776a9d29c5ebee4005/images/gklogo2015.png';
-
-            var pList   = [];
-
-            for (var key in options){
-                pList.push(key+'='+encodeURIComponent(options[key]));
-            }
-            ele.href = this.share_service_url + '?' + pList.join('&');
+            ele.href = this.share_service_url + this.makeUrlQueryString(options);
             return;
-
         },
-        setupShareSellerBtn: function(ele){
-            console.log(ele);
+        setupShareSellerBtn: function(index,ele){
             var options = _.clone(this.shareOptions);
                 options.title = this.getSellerShareTitle(ele);
-                options.pic = this.getShellerSharePic(ele);
+                options.pic = this.getSellerSharePic(ele);
 
+                ele.href = this.share_service_url + this.makeUrlQueryString(options);
         },
 
         getSellerShareTitle: function(ele){
