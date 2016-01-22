a13cc7b83c07c81c201c190bb12af0d8249469f6
Judy <judy.programer@gmail.com>
2016-01-22 12:49:40 +0800

    finally

--- nut/apps/fetch/article/__init__.py
+++ nut/apps/fetch/article/__init__.py
@@ -1,7 +1,29 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import os
+import sys
+import json
+import random
 import requests
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.dev_judy'
+
+from faker import Faker
+from django.conf import settings
+from django.utils.log import getLogger
 from celery import Task
+from requests.exceptions import ReadTimeout
+from requests.exceptions import ConnectionError
+
+
+faker = Faker()
+search_api = 'http://weixin.sogou.com/weixinjs'
+login_url = 'https://account.sogou.com/web/login'
+log = getLogger('django')
 
 
 class Retry(Exception):
@@ -16,11 +38,119 @@ class RequestsTask(Task):
     default_retry_delay = 5
     send_error_emails = True
     max_retries = 3
+    rate_limit = '2/m'
 
     def __call__(self, *args, **kwargs):
         try:
-            super(RequestsTask, self).__call__(*args, **kwargs)
+            return super(RequestsTask, self).__call__(*args, **kwargs)
         except (requests.Timeout, requests.ConnectionError) as e:
             raise self.retry(exc=e)
         except Retry as e:
             raise self.retry(countdown=e.countdown)
+
+
+class WeiXinClient(requests.Session):
+    def __init__(self):
+        super(WeiXinClient, self).__init__()
+        self.login_url = 'https://account.sogou.com/web/login'
+        self.search_api_url = 'http://weixin.sogou.com/weixinjs'
+
+    def request(self, method, url,
+                params=None,
+                data=None,
+                headers=None,
+                cookies=None,
+                files=None,
+                auth=None,
+                timeout=None,
+                allow_redirects=True,
+                proxies=None,
+                hooks=None,
+                stream=None,
+                verify=None,
+                cert=None,
+                json=None,
+                format_json=False):
+        try:
+            resp = super(WeiXinClient, self).request(method, url, params, data,
+                                                     headers, cookies, files, auth,
+                                                     timeout, allow_redirects,
+                                                     proxies, hooks, stream, verify,
+                                                     cert, json)
+        except ConnectionError:
+            raise Retry
+        except ReadTimeout:
+            raise Retry
+        if stream:
+            return resp
+        result = resp.content.decode('utf-8')
+        result = result.rstrip('\n')
+        if result.find(u'您的访问过于频繁') >= 0:
+            log.warning(u'访问的过于频繁. url: %s', url)
+            self.cookies.clear()
+            self.headers['Cookie'] = random.choice(cookies.values)
+            self.headers['User-Agent'] = faker.user_agent()
+            self.headers['Referer'] = random.choice(referers)
+            raise Retry
+        if format_json:
+            result = self.json_response(resp)
+            if 'code' in result and result['code'] == "needlogin":
+                self.login()
+                raise Retry
+        return result
+
+    def login(self):
+        username = random.choice(settings.SOGOU_USERS)
+        password = settings.SOGOU_PASSWORD
+        headers = {
+            'Referer': 'http://news.sogou.com/?p=40030300&kw=',
+            'Origin': 'http://news.sogou.com',
+            'Host': 'account.sogou.com',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'RA-Sid': '77094B42-20140626-080311-ac7a24-cf2748',
+            'RA-Ver': '3.0.7',
+            'Upgrade-Insecure-Requests': 1,
+            'UserAgent': faker.user_agent(),
+        }
+        data = dict(username=username,
+                    password=password,
+                    autoLogin=1,
+                    client_id='2006',
+                    xd='http://news.sogou.com/jump.htm',
+                    )
+        self.request('POST',
+                     self.login_url, data=data, headers=headers)
+
+    @classmethod
+    def json_response(cls, response):
+        result = response.content.decode('utf-8')
+        result = result.rstrip('\n')
+        if result.startswith('weixin('):
+            result = result[7:-1]
+        elif result.startswith('sogou.weixin_gzhcb('):
+            result = result[19:-1]
+        try:
+            return json.loads(result)
+        except ValueError:
+            return result
+
+
+#############  Cookies  #############
+cookies = {
+    'Waser1959@gustr.com': {'ppmdig': '1453397330000000c83c143bec3f757d06144cf1697d4f4a', 'ad': 'vHEdKZllll2QoSqJlllllVzPVU7lllllLcfbOlllll6lllllVgDll5@@@@@@@@@@', 'SUID': '949B8EDB2A10950A0000000056A11548', 'ppinf': '5|1453397320|1454606920|Y2xpZW50aWQ6NDoyMDA2fGNydDoxMDoxNDUzMzk3MzIwfHJlZm5pY2s6MDp8dHJ1c3Q6MToxfHVzZXJpZDoxOTp3YXNlcjE5NTlAZ3VzdHIuY29tfHVuaXFuYW1lOjA6fA', 'SNUID': '2E213561BBBF9261CAAE5201BB372A72;', 'ABTEST': '0|1453397330|v1', 'CXID': 'CEF4CCEAE4C33B94EBE0CBB26CA8A603', 'SUV': '0043744ADB8E9B9456A115489E542674', 'PHPSESSID': 'u7lmaa86vrl9lesn6c5a6ecgb3', 'SUIR': '1453397330', 'pprdig': 'B3_zIgFj-XJHeEWSYu5UjbBcJtHlcQHQP8z4ZB5eD4ipwngIKggwrl3J9XCYaIANjHpCv9oS28JFBFkOaR4HAXP7Plv4SIBslWnBfkCUk_ZyKzPZwx3oI-SuxGEpXiynpOIZIz-IRcbwhkdbadgjF8KtU4DhrV987raKEOiwc8o', 'IPLOC': 'CN1100'},
+    'asortafairytale@fleckens.hu': {'SNUID': '76796F3AE1E4CB3BD751771CE2BCC396', 'ABTEST': '0|1453386418|v1', 'IPLOC': 'CN1100', 'ad': 'sHEdJZllll2Qon@@@@@@@@@@', 'SUID': '949B8EDB2A10950A0000000056A0EAB7', 'pprdig': 'rztHrGmvYAKsxEuSFoku0TqqMXacNMaSZqGxISh4XoT0ELieC6GGLpB7RoawOQvavrTDCxJu6VcsXJ2s8HGKkOXWok1k2nVbC2oaApIhAy0AzHvk2G0zdzBRKpZMJOKA2CBUONZ5q6IWWvtsJBWm3KOIWm2p48MFHlE2wrpTuR4', 'SUIR': '1453387763', 'sct': '9', 'CXID': 'DA3B77DAF66D29E3DEF0A1A773218CD2', 'SUV': '00161002DB8E9B9456A0EA01A7B25989', 'weixinIndexVisited': '1', 'ppinf': '5|1453396731|1454606331|Y2xpZW50aWQ6NDoyMDA2fGNydDoxMDoxNDUzMzk2NzMxfHJlZm5pY2s6MDp8dHJ1c3Q6MToxfHVzZXJpZDoyNzphc29ydGFmYWlyeXRhbGVAZmxlY2tlbnMuaHV8dW5pcW5hbWU6MDp8'},
+    'Adisaid@jourrapide.com': {'ppinf': '5|1453396873|1454606473|Y2xpZW50aWQ6NDoyMDA2fGNydDoxMDoxNDUzMzk2ODczfHJlZm5pY2s6MDp8dHJ1c3Q6MToxfHVzZXJpZDoyMjphZGlzYWlkQGpvdXJyYXBpZGUuY29tfHVuaXFuYW1lOjA6fA', 'pprdig': 'sWjqVRUbvsuMsnWg65ckxpFh8_JgPDB1-N5iclB3rvuTtC2V-hSyBKXLuAqL-wuYXxNR-BX56OmZLEVs9OjyISfB4di9-Yh33_OV1BfQTDmRGk6CqqOAMKW6EG_H0yOpHxvwfyBBgwMXEM8tMuPEIExT933kKa85yU9l_k_BbtE', 'seccodeRight': 'success', 'ppmdig': '145339674200000047be4ee76732a6aa8db180f0c46ab2d0', 'SNUID': '0F0712479B99B547CE4BE8469C2477E5', 'IPLOC': 'CN1100', 'PHPSESSID': 'bevobnbosau8picf198jcag1u2', 'successCount': '1', 'ad': 'eYENXlllll2QoSn3lllllVzP9i6lllllLcfbOllllxwlllllVgDll5@@@@@@@@@@', 'SUV': '00E0744CDB8E9B9456A1138ABCF2C154', 'weixinIndexVisited': '1', 'ABTEST': '0|1453386418|v1', 'SUID': '949B8EDB2A10950A0000000056A1138A', 'SUIR': '1453396879', 'CXID': '2CEBBC1F1DA02CB2DEAA08803805603F'},
+    'Rathe1981@rhyta.com': {'SUIR': '1453397078', 'ppinf': '5|1453397066|1454606666|Y2xpZW50aWQ6NDoyMDA2fGNydDoxMDoxNDUzMzk3MDY2fHJlZm5pY2s6MDp8dHJ1c3Q6MToxfHVzZXJpZDoxOTpyYXRoZTE5ODFAcmh5dGEuY29tfHVuaXFuYW1lOjA6fA', 'SUID': '949B8EDB2A10950A0000000056A11436', 'pprdig': 'i750ZJmx4IQxB4PsekxTvD4hgG91bmY1JfmMjSU_jlfHkRwGjntPZR183ouiPgDpDN0n8TRmZ8PZR0yrUcyWuvqUsA9fcBlWG6P5BTmswEjL4Mux2ezSriM3Wghw8v97PdpHM6vOFmLZtcnz-3ft-GhYZ-gQ2XjEWJGWJRLG2SE', 'seccodeRight': 'success', 'CXID': 'AB66F8187F49EDF50455ED71921AC8B2', 'SUV': '0034744DDB8E9B9456A11436756E7141', 'ppmdig': '145339674200000057f320ac8be269d9502639450dd8264a', 'SNUID': '67687C29F3F7DB29930ED0D0F395C810', 'ABTEST': '0|1453386418|v1', 'IPLOC': 'CN1100', 'ad': 'D@@@@@@@@@@', 'weixinIndexVisited': '1', 'PHPSESSID': 'bevobnbosau8picf198jcag1u2'},
+    'Andurn@fleckens.hu': {'ppmdig': '1453396742000000532ba8fcfa8e01b934ba03e2909ddac2', 'ad': 'hIidKyllll2QoSv1lllllVzPVatlllllLcfbOlllllolllllVgDll5@@@@@@@@@@', 'SUIR': '1453397175', 'SUID': '949B8EDB2A10950A0000000056A114B4', 'pprdig': 'w7qeRmbO5xtfoCTOJMpTl_oy8vEytQvDD4rJ41c8DLHhpE8rbMj0kMB4xXZaTkriME9mj-3G0rBGSH3cmaHE45P3WSujz5LWcFx5ariiKsjpCpDr-AlCuBAt9shxDR57_Rhq_skjOztBqAKaUmn1kT5ujPKCzO5AQKyds-koLq4', 'SNUID': '303E2B7EA5A18C7FC34ADA49A5B77A0D;', 'ABTEST': '0|1453386418|v1', 'CXID': 'D51D5E600468B0E90F70573937FB3A3E', 'SUV': '0096744BDB8E9B9456A114B4D9568399', 'PHPSESSID': 'bevobnbosau8picf198jcag1u2', 'IPLOC': 'CN1100', 'ppinf': '5|1453397172|1454606772|Y2xpZW50aWQ6NDoyMDA2fGNydDoxMDoxNDUzMzk3MTcyfHJlZm5pY2s6MDp8dHJ1c3Q6MToxfHVzZXJpZDoxODphbmR1cm5AZmxlY2tlbnMuaHV8dW5pcW5hbWU6MDp8', 'refresh': '1', 'weixinIndexVisited': '1'},
+    'sanyuanmilk@fleckens.hu': {'ppmdig': '1453396742000000f86d8b8b97a1eadaaaa73bf1af62a9ff', 'ad': 'ZYENXlllll2QoSF@lllllVzPVyGlllllLcfbOlllll6lllllVgDll5@@@@@@@@@@', 'SUIR': '1453397255', 'SUID': '949B8EDB2A10950A0000000056A114FE', 'pprdig': 'GDxSWxb_kipE0dBTgkYhCqDZH7ULWhsgiTp5N3EdouWQTYZNb64VlWzfwvUQtpTrU5X1rojFYH5094M1zNxkEgyjV-L-roRFL9dJXX4_wy8ZTYIsUIX7j2DGRNvRp9G8EfpnDMQ61HE5a5mMarfRPpPItvmZNzceoj1b1BsznAI', 'SNUID': 'A1AEB8ED36331FED5DB5229336853F78;', 'ABTEST': '0|1453386418|v1', 'CXID': 'B4B21B3D4BB68FCB64219E2B3F3EC19F', 'SUV': '0034744DDB8E9B9456A114FE76464532', 'PHPSESSID': 'bevobnbosau8picf198jcag1u2', 'IPLOC': 'CN1100', 'ppinf': '5|1453397246|1454606846|Y2xpZW50aWQ6NDoyMDA2fGNydDoxMDoxNDUzMzk3MjQ2fHJlZm5pY2s6MDp8dHJ1c3Q6MToxfHVzZXJpZDoyMzpzYW55dWFubWlsa0BmbGVja2Vucy5odXx1bmlxbmFtZTowOnw', 'weixinIndexVisited': '1'},
+}
+
+
+referers = ('http://weixin.sogou.com/',
+            'http://mp.weixin.qq.com/s?__biz=MTI0MDU3NDYwMQ==&mid=406976557&idx=3&sn=e2749cff6e7fbf1379f4d7ee5829a5aa&3rd=MzA3MDU4NTYzMw==&scene=6#rd',
+            'http://weixin.sogou.com/weixin?type=1&query=a&ie=utf8'
+            )
+
+
+
--- nut/apps/fetch/article/weixin.py
+++ nut/apps/fetch/article/weixin.py
@@ -1,202 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import json
-import os
-import random
 import re
+import os
 import sys
-import requests
-
-
+import random
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(BASE_DIR)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.dev_judy'
 
-from urlparse import urljoin, urlparse
-from faker import Faker
 from django.conf import settings
+from urlparse import urljoin, urlparse
 from celery.task import task
-from celery import group
 from django.utils.log import getLogger
-from time import sleep
 from datetime import datetime
 from bs4 import BeautifulSoup
-from requests.exceptions import ReadTimeout, ConnectionError
 
 from apps.core.models import Article, Media, Authorized_User_Profile
 from apps.fetch.common import clean_xml, queryset_iterator
-from apps.fetch.article import RequestsTask, Retry
-from settings import FETCH_INTERVAL
+from apps.fetch.article import RequestsTask, WeiXinClient
 
 
-faker = Faker()
-IMAGE_DIR = os.path.join(settings.BASE_DIR, 'images')
-search_api = 'http://weixin.sogou.com/weixinjs'
-article_list_api = 'http://weixin.sogou.com/gzhjs'
-login_url = 'https://account.sogou.com/web/login'
-account_url = 'https://account.sogou.com/web/userinfo/getuserinfo'
+SEARCH_API = 'http://weixin.sogou.com/weixinjs'
+ARTICLE_LIST_API = 'http://weixin.sogou.com/gzhjs'
 log = getLogger('django')
-image_host = getattr(settings, 'IMAGE_HOST', None)
-
+weixin_client = WeiXinClient()
 qr_code_patterns = (re.compile('biz\s*=\s*"(?P<qr_url>[^"]*)'),
                     re.compile('fakeid\s*=\s*"(?P<qr_url>[^"]*)'),
                     re.compile('appuin\s*=\s*"(?P<qr_url>[^"]*)'))
+image_host = getattr(settings, 'IMAGE_HOST', None)
 
 
-class WeiXinClient(requests.Session):
-    def __init__(self):
-        super(WeiXinClient, self).__init__()
-        self._ext = None
-        self.login_url = 'https://account.sogou.com/web/login'
-        self.logout_url = 'https://account.sogou.com/web/logout_js?client_id=2006'
-        self.account_url = 'https://account.sogou.com/web/userinfo/getuserinfo'
-        self.search_api_url = 'http://weixin.sogou.com/weixinjs'
-
-    def request(self, method, url,
-                params=None,
-                data=None,
-                headers=None,
-                cookies=None,
-                files=None,
-                auth=None,
-                timeout=None,
-                allow_redirects=True,
-                proxies=None,
-                hooks=None,
-                stream=None,
-                verify=None,
-                cert=None,
-                json=None,
-                ext=False,
-                format_json=False):
-        if ext:
-            params['ext'] = self.ext
-        if headers:
-            headers['UserAgent'] = faker.user_agent()
-        else:
-            headers = {'UserAgent': faker.user_agent()}
-        try:
-            resp = super(WeiXinClient, self).request(method, url, params, data,
-                                                     headers, cookies, files, auth,
-                                                     timeout, allow_redirects,
-                                                     proxies, hooks, stream, verify,
-                                                     cert, json)
-        except ConnectionError:
-            raise Retry
-        except ReadTimeout:
-            raise Retry
-        if stream:
-            return resp
-        result = resp.content.decode('utf-8')
-        result = result.rstrip('\n')
-        if result.find(u'当前请求已过期') >= 0:
-            self.login()
-            self.get_ext()
-            log.warning(u'ext不正确. url: %s', url)
-            raise Retry
-        elif result.find(u'您的访问过于频繁') >= 0:
-            log.warning(u'访问的过于频繁. url: %s', url)
-            self.cookies.clear()
-            self.logout()
-            self.cookies.clear()
-            sleep(FETCH_INTERVAL)
-            self.login()
-            raise Retry(3600)
-        if format_json:
-            result = self.json_response(resp)
-            if 'code' in result and result['code'] == "needlogin":
-                self.login()
-                raise Retry
-        sleep(FETCH_INTERVAL)
-        return result
-
-    @property
-    def ext(self):
-        if not self._ext:
-            self.get_ext()
-        return self._ext
-
-    def get_ext(self):
-        any_word = random.choice('abcdefghijklmnopqrstuvwxyz')
-        params = dict(type='1', ie='utf8', query=any_word, page=1)
-        item_list = self.request('GET',
-                                 url=self.search_api_url,
-                                 params=params,
-                                 format_json=True)
-        item_list = item_list['items']
-        account_xml = clean_xml(item_list[0])
-        account_xml = BeautifulSoup(account_xml, 'xml')
-        self._ext = account_xml.ext.string
-
-    def login(self):
-        username = random.choice(settings.SOGOU_USERS)
-        password = settings.SOGOU_PASSWORD
-        headers = {
-            'Referer': 'http://news.sogou.com/?p=40030300&kw=',
-            'Origin': 'http://news.sogou.com',
-            'Host': 'account.sogou.com',
-            'Connection': 'keep-alive',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'RA-Sid': '77094B42-20140626-080311-ac7a24-cf2748',
-            'RA-Ver': '3.0.7',
-            'Upgrade-Insecure-Requests': 1,
-            'UserAgent': faker.user_agent(),
-        }
-        data = dict(username=username,
-                    password=password,
-                    autoLogin=1,
-                    client_id='2006',
-                    xd='http://news.sogou.com/jump.htm',
-                    )
-        self.request('POST',
-                     self.login_url, data=data, headers=headers)
-
-    @classmethod
-    def json_response(cls, response):
-        result = response.content.decode('utf-8')
-        result = result.rstrip('\n')
-        if result.startswith('weixin('):
-            result = result[7:-1]
-        elif result.startswith('sogou.weixin_gzhcb('):
-            result = result[19:-1]
-        try:
-            return json.loads(result)
-        except ValueError:
-            return result
-
-    def logout(self):
-        self.get(self.logout_url)
-weixin_client = WeiXinClient()
-weixin_client.login()
-
-
+@task(base=RequestsTask, name='sogou.crawl_articles', rate_limit='5/m')
 def crawl_articles():
-    all_authorized_user = Authorized_User_Profile.objects.\
+    all_authorized_user = Authorized_User_Profile.objects. \
         filter(weixin_id__isnull=False)
     for user in queryset_iterator(all_authorized_user):
-        get_user_articles(user)
+        gk_user = {'weixin_openid': user.weixin_openid,
+                   'weixin_id': user.weixin_id,
+                   'pk': user.pk,
+                   'weixin_qrcode_img': user.weixin_qrcode_img}
+        get_user_articles.delay(gk_user)
 
 
+@task(base=RequestsTask, name='sogou.get_user_articles', rate_limit='5/m')
 def get_user_articles(gk_user):
-    if not gk_user.weixin_openid:
-        open_id = get_open_id(gk_user.weixin_id)
-        gk_user.weixin_openid = open_id
-        gk_user.save()
+    open_id, ext = get_open_id(gk_user['weixin_id'])
+    gk_user['weixin_openid'] = open_id
+    gk_user_instance = Authorized_User_Profile.objects.get(pk=gk_user['pk'])
+    gk_user_instance.weixin_openid = open_id
+    gk_user_instance.save()
+    fetch_article_list.delay(gk_user, open_id, ext)
 
-    fetch_article_list(gk_user)
 
-
-def fetch_article_list(gk_user):
+@task(base=RequestsTask, name='sogou.fetch_article_list', rate_limit='5/m')
+def fetch_article_list(gk_user, open_id, ext):
     page = 1
     total_pages = 1
     go_next = True
 
     while go_next:
-        params = {'openid': gk_user.weixin_openid, 'page': page}
-        response = weixin_client.request("GET", article_list_api,
+        params = {'openid': open_id, 'ext': ext, 'page': page}
+        response = weixin_client.request("GET", ARTICLE_LIST_API,
                                          params=params,
-                                         ext=True,
                                          format_json=True)
         json_items = response['items']
         if total_pages == 1:
@@ -210,7 +76,7 @@ def fetch_article_list(gk_user):
             url = urljoin('http://weixin.sogou.com/', article_link)
             item_dict[url] = article_item_xml
 
-        existed = Article.objects.values_list('origin_source').\
+        existed = Article.objects.values_list('origin_source'). \
             filter(origin_source__in=item_dict.keys())
         if existed:
             existed = [item[0] for item in existed]
@@ -229,83 +95,90 @@ def fetch_article_list(gk_user):
 
 def get_article(article_item, gk_user):
     cover = article_item.imglink.string
+    article_data = dict()
     if cover:
-        cover = fetch_images(fix_image_url(cover))
+        article_data = {'cover': cover}
     article_link = article_item.url.string
-    article_data = {'cover': cover}
-    crawl_article(article_link, gk_user, article_data)
+    crawl_article.delay(article_link, gk_user, article_data)
 
 
+@task(base=RequestsTask, name='sogou.crawl_article', rate_limit='5/m')
 def crawl_article(article_link, gk_user, article_data):
-    try:
-        url = urljoin('http://weixin.sogou.com/', article_link)
-        html_source = weixin_client.request('GET', url=url)
-        article_soup = BeautifulSoup(html_source)
-        get_qr_code(gk_user, article_soup)
+    url = urljoin('http://weixin.sogou.com/', article_link)
+    html_source = weixin_client.request('GET', url=url)
+    article_soup = BeautifulSoup(html_source)
+    get_qr_code.delay(gk_user, html_source)
 
-        title = article_soup.select('h2.rich_media_title')[0].text
-        published_time = article_soup.select('em#post-date')[0].text
-        published_time = datetime.strptime(published_time, '%Y-%m-%d')
-        content = article_soup.find('div', id='js_content')
+    title = article_soup.select('h2.rich_media_title')[0].text
+    published_time = article_soup.select('em#post-date')[0].text
+    published_time = datetime.strptime(published_time, '%Y-%m-%d')
+    content = article_soup.find('div', id='js_content')
+    creator = Authorized_User_Profile.objects.get(pk=gk_user['pk']).user
 
-        chief_image, content = parse_article_content(content)
-        article_info = dict(title=title,
-                            content=content,
-                            created_datetime=published_time,
-                            creator=gk_user.user,
-                            publish=Article.published,
-                            origin_source=url
-                            )
-        article_info.update(article_data)
-        if 'cover' not in article_info or not article_info['cover']:
-            article_info['cover'] = chief_image
-        article = Article(**article_info)
+    article_info = dict(title=title,
+                        content=content.decode_contents(formatter="html"),
+                        created_datetime=published_time,
+                        creator=creator,
+                        publish=Article.published,
+                        origin_source=url
+                        )
+    article_info.update(article_data)
+    log.info('Insert article. %s', article_info)
+    article = Article(**article_info)
+    article.save()
+    log.info('Insert article. succeed')
+    parse_article_content(article.pk).delay()
+
+
+@task(base=RequestsTask, name='sogou.parse_article', rate_limit='5/m')
+def parse_article_content(article_id):
+    article = Article.objects.get(pk=article_id)
+    content = article.content
+    cover = article.cover
+    if cover:
+        cover = fetch_image(cover)
+        article.cover = cover
         article.save()
-    except BaseException, e:
-        log.error(e.message)
 
-
-def parse_article_content(content):
-    image_tags = content.find_all('img')
-    img_src_list = []
-    content_html = ''
+    article_soup = BeautifulSoup(content)
+    image_tags = article_soup.find_all('img')
     if image_tags:
-        for image_tag in image_tags:
+        for i, image_tag in enumerate(image_tags):
             img_src = image_tag.attrs.get('src')
             if not img_src:
                 img_src = image_tag.attrs.get('data-src')
-            if img_src:
-                img_src_list.append(fix_image_url(img_src))
-
-        image_urls = []
-        for img_src in img_src_list:
-            image_url = fetch_images(img_src)
-            image_urls.append(image_url)
-
-        for img_tag, img_url in zip(image_tags, image_urls):
-            img_tag['src'] = img_url
-            img_tag['data-src'] = img_url
-        content_html = content.decode_contents(formatter="html")
-    chief_image = img_src_list[0] if img_src_list else ''
-    return chief_image, content_html
+            if img_src and img_src.find('mmbiz.qpic.cn') >= 0:
+                gk_img_rc = fetch_image(img_src)
+                if gk_img_rc:
+                    image_tag['src'] = gk_img_rc
+                    image_tag['data-src'] = gk_img_rc
+                    if not cover and i == 0:
+                        article.cover = gk_img_rc
+            content_html = article_soup.decode_contents(formatter="html")
+            article.content = content_html
+            article.save()
 
 
 def get_open_id(weixin_id):
-    open_id, total_pages = fetch_open_id(weixin_id)
+    log.info('get open_id for %s', weixin_id)
+    open_id, ext, total_pages = fetch_open_id(weixin_id)
     if not open_id:
+        log.warning('cannot find open_id on page 1')
         for page in xrange(2, total_pages + 1):
-            open_id, total_pages = fetch_open_id(weixin_id, page)
+            open_id, ext, total_pages = fetch_open_id(weixin_id)
             if open_id:
                 break
-    return open_id
+    return open_id, ext
 
 
 def fetch_open_id(weixin_id, page=1):
+    log.info('fetch open_id for %s', weixin_id)
     open_id = None
+    ext = None
     total_pages = 1
     params = dict(type='1', ie='utf8', query=weixin_id, page=page)
     response = weixin_client.request('GET',
-                                     url=search_api,
+                                     url=SEARCH_API,
                                      params=params,
                                      format_json=True)
     for item in response['items']:
@@ -313,35 +186,40 @@ def fetch_open_id(weixin_id, page=1):
         item_xml = BeautifulSoup(item_xml, 'xml')
         if item_xml.weixinhao.string == weixin_id:
             open_id = item_xml.id.string
+            ext = item_xml.ext.string
             break
     if page == 1:
         total_pages = int(response['totalPages'])
-    return open_id, total_pages
+    return open_id, ext, total_pages
 
 
-def fetch_images(image_url):
+def fetch_image(image_url):
+    image_full_name = ''
     from apps.core.utils.image import HandleImage
     r = weixin_client.request('GET',
                               url=image_url,
                               stream=True)
-    image_full_name = ''
     try:
+        try:
+            content_type = r.headers['Content-Type']
+        except KeyError:
+            content_type = 'image/jpeg'
         image = HandleImage(r.raw)
-        image_name = image.save(path=IMAGE_DIR)
+        image_name = image.save()
         image_full_name = "%s%s" % (image_host, image_name)
         Media.objects.create(
             file_path=image_name,
-            content_type=getattr(image, 'content_type', 'image/jpeg')
-        )
+            content_type=content_type)
     except BaseException, e:
-        log.error(e.message)
-    except AttributeError, e:
-        log.error(e.message)
+        print image_url
+        log.error('Handle Image Error: %s', e.message)
     return image_full_name
 
 
-def get_qr_code(gk_user, article_soup):
-    if not gk_user.weixin_qrcode_img:
+@task(base=RequestsTask, name='sogou.get_qr_code', rate_limit='5/m')
+def get_qr_code(gk_user, html_source):
+    if not gk_user['weixin_qrcode_img']:
+        article_soup = BeautifulSoup(html_source)
         scripts = article_soup.select('script')
         biz = ''
         for script in scripts:
@@ -355,11 +233,13 @@ def get_qr_code(gk_user, article_soup):
                 break
 
         scene = random.randrange(10000001, 10000007)
-        qr_code_url = 'http://mp.weixin.qq.com/mp/qrcode?scene=%s__biz=%s' %\
+        qr_code_url = 'http://mp.weixin.qq.com/mp/qrcode?scene=%s&__biz=%s' % \
                       (scene, biz)
-        qr_code_image = fetch_images(qr_code_url)
-        gk_user.weixin_qrcode_img = qr_code_image
-        gk_user.save()
+        qr_code_image = fetch_image(qr_code_url)
+
+        gk_user_instance = Authorized_User_Profile.objects.get(pk=gk_user['pk'])
+        gk_user_instance.weixin_qrcode_img = qr_code_image
+        gk_user_instance.save()
 
 
 def fix_image_url(image_url):
@@ -367,11 +247,11 @@ def fix_image_url(image_url):
     path_parts = url_parts.path.split('/')
     if path_parts[-1] == '0':
         fix_path = '/'.join(path_parts[:-1])
+        query = url_parts.query
+        if query:
+            query_parts = query.split('&')
+            query = [query for query in query_parts if query.startswith('wx_fmt=')][0]
         if not fix_path.endswith('/'):
             fix_path += '/'
-        image_url = url_parts._replace(path=fix_path).geturl()
+        image_url = url_parts._replace(path=fix_path, query=query).geturl()
     return image_url
-
-
-if __name__ == '__main__':
-    crawl_articles()
