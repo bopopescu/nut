0bbf86da0486658aeab35aa901384d460bf5b38f
An Chen <anchen@guoku.com>
2016-01-26 18:45:33 +0800

    update

--- nut/apps/fetch/models.py
+++ nut/apps/fetch/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 class SogouCookies(Model):
     cookie_string = TextField()
-    created_time = DateTimeField(default=timezone.now())
+    created_time = DateTimeField(auto_now_add=True)
     class Meta:
         ordering = ['-created_time']
 
