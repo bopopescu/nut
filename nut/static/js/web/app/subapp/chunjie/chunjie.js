5888cebcb8a15e897d063158ee905934108235b6
An Chen <anchen@guoku.com>
2016-02-02 11:55:00 +0800

    add chunjie app ;

---
+++ nut/static/js/web/app/subapp/chunjie/chunjie.js
@@ -0,0 +1,13 @@
+define(['libs/Class'], function(
+    Class
+){
+
+    var ChunjieApp = Class.extend({
+        init: function(){
+            console.log('chunjieApp init');
+
+        }
+    });
+
+    return ChunjieApp;
+});
