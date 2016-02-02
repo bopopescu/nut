2882f1451209ef1dd8f98a92fe26f7a1ac88798f
An Chen <anchen@guoku.com>
2016-02-02 17:56:36 +0800

    update

--- nut/static/js/web/app/subapp/chunjie/chunjie.js
+++ nut/static/js/web/app/subapp/chunjie/chunjie.js
@@ -5,8 +5,17 @@ define(['libs/Class'], function(
     var ChunjieApp = Class.extend({
         init: function(){
             console.log('chunjieApp init');
+            this.configWX();
 
-        }
+        },
+        configWX: function(){
+            wx.config({
+                debug: true,
+                appId:'guokuapp',
+
+
+            });
+        },
     });
 
     return ChunjieApp;
