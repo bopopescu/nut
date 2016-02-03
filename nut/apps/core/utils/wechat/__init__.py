6a25ce524820aa9dae7181e37f0c3d88bfa3c46d
An Chen <anchen@guoku.com>
2016-02-03 17:09:27 +0800

    handle ssl verify fail error

--- nut/apps/core/utils/wechat/__init__.py
+++ nut/apps/core/utils/wechat/__init__.py
@@ -30,7 +30,7 @@ def get_wechat_access_token():
         return token
 
     url = get_token_request_url()
-    res = requests.get(url)
+    res = requests.get(url,verify=False)
     token = res.json()['access_token']
     expires = res.json()['expires_in']
 
@@ -46,7 +46,7 @@ def get_jsapi_ticket():
         return ticket
 
     token = get_wechat_access_token()
-    res = requests.get(get_jsapi_ticket_request_url(token))
+    res = requests.get(get_jsapi_ticket_request_url(token),verify=False)
     ticket = res.json()['ticket']
     errcode = res.json()['errcode']
     expires = res.json()['expires_in']
