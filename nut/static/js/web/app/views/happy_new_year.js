bbf68c85f6e1b7133ee1f48d19508072bff979f5
AzureRay <476468781@qq.com>
2016-02-04 10:01:09 +0800

    add share dialog so happy

--- nut/static/js/web/app/views/happy_new_year.js
+++ nut/static/js/web/app/views/happy_new_year.js
@@ -1,41 +1,41 @@
-define(['libs/Class', 'jquery', 'underscore','bootbox'], function(Class,$,_,bootbox){
-
-    var ShareHanlder = Class.extend({
-        init: function(){
-
-            this.share_weixin_modal_content = $('#share_friends_modal_content').html();
-
-            this.weixinShareOptions ={
-
-            };
-
-            //what
-            this.setupShareTrigger();
-
-        },
-
-        showWeixinShareDialog: function(){
-            bootbox.hideAll();
-            bootbox.dialog({
-                title: '分享 share with friends',
-                onEscape: true,
-                backdrop:true,
-                closeButton: true,
-                animate: true,
-                className: 'share-friends-dialog',
-                message: this.share_weixin_modal_content,
-
-            });
-        },
-
-        setupShareTrigger: function(){
-
-            $('#share').setupWeixinShareSellerBtn.bind(this);
-        },
-
-        setupWeixinShareSellerBtn: function(index, ele){
-                $(ele).click(this.showWeixinShareDialog.bind(this));
-        },
-    });
-    return ShareHanlder
-});
+//define(['libs/Class', 'jquery', 'underscore','bootbox'], function(Class,$,_,bootbox){
+//
+//    var ShareHanlder = Class.extend({
+//        init: function(){
+//
+//            this.share_weixin_modal_content = $('#share_friends_modal_content').html();
+//
+//            this.weixinShareOptions ={
+//
+//            };
+//
+//            //what
+//            this.setupShareTrigger();
+//
+//        },
+//
+//        showWeixinShareDialog: function(){
+//            bootbox.hideAll();
+//            bootbox.dialog({
+//                title: '分享 share with friends',
+//                onEscape: true,
+//                backdrop:true,
+//                closeButton: true,
+//                animate: true,
+//                className: 'share-friends-dialog',
+//                message: this.share_weixin_modal_content,
+//
+//            });
+//        },
+//
+//        setupShareTrigger: function(){
+//
+//            $('#share').setupWeixinShareSellerBtn.bind(this);
+//        },
+//
+//        setupWeixinShareSellerBtn: function(index, ele){
+//                $(ele).click(this.showWeixinShareDialog.bind(this));
+//        },
+//    });
+//    return ShareHanlder
+//});
