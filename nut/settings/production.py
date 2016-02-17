518f2d19c30f47e972d763194e2a5c1673f7cdbd
jiaxin <edison7500@gmail.com>
2016-02-17 17:07:05 +0800

    update

--- nut/settings/production.py
+++ nut/settings/production.py
@@ -1,7 +1,7 @@
 from settings import *
 
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 STATIC_URL = 'http://static.guoku.com/static/v4/075b34bf8a77778a7b8035b55b02e7896959d165/'
