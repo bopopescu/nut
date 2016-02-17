bdc8cea61eb4cdb508763a9b71868768951523d3
AzureRay <476468781@qq.com>
2016-02-17 15:29:56 +0800

    first build

---
+++ nut/static/js/web/app/subapp/entity/entity_share.js
@@ -0,0 +1,12 @@
+define(['jquery', 'libs/Class','underscore','bootbox'], function(
+    $, Class,_,bootbox
+){
+
+    var EntityShareApp= Class.extend({
+        init: function(){
+             console.log('hello rose rose rose!');
+        }
+    });
+
+    return EntityShareApp;
+});
