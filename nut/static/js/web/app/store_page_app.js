4c9beaabe7fc7a1a147d5e0604dd87a7f67eba91
AzureRay <476468781@qq.com>
2016-03-03 17:04:44 +0800

    build structure

---
+++ nut/static/js/web/app/store_page_app.js
@@ -0,0 +1,13 @@
+require([
+        'jquery',
+        'subapp/store/store_banner'
+    ],
+    function (
+              jQuery,
+              StoreBanner
+
+    ){
+        var store_banner = new StoreBanner();
+
+});
+
---
+++ nut/static/js/web/app/subapp/store/store_banner.js
@@ -0,0 +1,15 @@
+define(['jquery', 'libs/Class'], function(
+        $, Class
+){
+
+    var StoreBanner= Class.extend({
+        init: function(){
+             console.log('hello rose,hello good store!');
+
+        }
+    });
+    return StoreBanner;
+});
+
+
+
