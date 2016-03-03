d3d5227c763e5653b9373205176f58d072128abe
AzureRay <476468781@qq.com>
2016-03-03 18:05:14 +0800

    add index slider at tne bottom of slick js file

--- nut/static/js/web/app/subapp/store/store_banner.js
+++ nut/static/js/web/app/subapp/store/store_banner.js
@@ -1,10 +1,24 @@
-define(['jquery', 'libs/Class'], function(
-        $, Class
-){
+requirejs.config({
+    baseUrl:base_url,
+    paths: {
+        slick: '/libs/slick'
 
+    },
+
+    shim: {
+        'slick':{
+            deps:['jquery']
+        }
+    }
+});
+
+
+define(['jquery', 'libs/Class'], function(
+    $, Class
+){
     var StoreBanner= Class.extend({
         init: function(){
-             console.log('hello lq,hello good store!');
+            console.log('hello lq,hello good store!');
 
         }
     });
