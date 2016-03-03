21eb7022761dfeeefee9dfcaf2f53d47450e702d
AzureRay <476468781@qq.com>
2016-03-03 18:05:14 +0800

    add index slider at tne bottom of slick js file

--- nut/static/js/web/app/libs/slick.js
+++ nut/static/js/web/app/libs/slick.js
@@ -2640,4 +2640,39 @@
         return _;
     };
 
+//    add index_slider.js at bottom
+    $('#index-banners').slick({
+
+
+  centerMode: true,
+  arrows: true,
+  slidesToShow: 1,
+  centerPadding:'15%',
+  dots:true,
+
+  //centerPadding: '60px',
+  //slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+         centerMode:false,
+         slidesToShow:1,
+         slidesToScroll:1,
+         infinite: true
+      }
+    },
+  ]
+  //  {
+  //    breakpoint: 480,
+  //    settings: {
+  //      arrows: false,
+  //      centerMode: true,
+  //      centerPadding: '40px',
+  //      slidesToShow: 1
+  //    }
+  //  }
+  //]
+});
+
 }));
--- nut/static/js/web/app/store_page_app.js
+++ nut/static/js/web/app/store_page_app.js
@@ -1,3 +1,5 @@
+
+
 require([
         'jquery',
         'subapp/store/store_banner'
