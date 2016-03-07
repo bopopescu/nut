e344beb39f7fa7c76a8bd9b33b85df026a5eed2f
AzureRay <476468781@qq.com>
2016-03-07 15:30:12 +0800

    clean

--- nut/static/js/web/app/subapp/store/store_banner.js
+++ nut/static/js/web/app/subapp/store/store_banner.js
@@ -6,15 +6,12 @@ define(['jquery', 'libs/Class','libs/slick'], function(
         init: function () {
             console.log('subapp good store start !');
             this.sameHeightFrame('user-latest-article','latest-actions-sidebar');
-
         },
 
         sameHeightFrame: function (leftId,rightId) {
-
             var leftChildHeight = this.getElementHeight(leftId);
             var rightChildHeight = this.getElementHeight(rightId);
             var rightChild = this.getElement(rightId);
-
             if (rightChildHeight > leftChildHeight) {
                 rightChild.style.height = leftChildHeight + "px";
             }
@@ -24,9 +21,7 @@ define(['jquery', 'libs/Class','libs/slick'], function(
         },
         getElementHeight:function(id){
             return this.getElement(id).offsetHeight;
-
         }
-
     });
     return StoreBanner;
 });
