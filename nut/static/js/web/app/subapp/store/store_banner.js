3eeed37afaeb028005565819c28f59598d3f59b0
AzureRay <476468781@qq.com>
2016-03-07 13:52:59 +0800

    why my js not run well

--- nut/static/js/web/app/subapp/store/store_banner.js
+++ nut/static/js/web/app/subapp/store/store_banner.js
@@ -1,25 +1,26 @@
-//requirejs.config({
-//    baseUrl:base_url,
-//    paths: {
-//        slick: '/libs/slick'
-//
-//    },
-//
-//    shim: {
-//        'slick':{
-//            deps:['jquery']
-//        }
-//    }
-//});
-
 
 define(['jquery', 'libs/Class','libs/slick'], function(
     $, Class
 ){
     var StoreBanner= Class.extend({
         init: function(){
-            console.log('hello lqqqqqq,hello good store!');
+            console.log('subapp good store start !');
+            this.sameHeightFrame('user-latest-article','latest-actions-sidebar');
 
+        },
+        sameHeightFrame:function(leftChildId,rightChildId){
+            var leftChildHeight = this.getElementHeight(leftChildId);
+            var rightChildHeight = this.getElementHeight(rightChildId);
+            if(rightChildHeight > leftChildHeight){
+                console.log('right child height is more than left child');
+                this.getElement(rightChildId).style.height = leftChildHeight + "px";
+            }
+        },
+        getElementHeight:function(id){
+            return this.getElement(id).offsetHeight;
+        },
+        getElement:function(id){
+            return document.getElementById(id);
         }
     });
     return StoreBanner;
--- nut/static/js/web/jsbuild/store_page_app_build.js
+++ nut/static/js/web/jsbuild/store_page_app_build.js
@@ -3591,28 +3591,29 @@ define('subapp/topmenu',['bootstrap',
 
 }));
 
-//requirejs.config({
-//    baseUrl:base_url,
-//    paths: {
-//        slick: '/libs/slick'
-//
-//    },
-//
-//    shim: {
-//        'slick':{
-//            deps:['jquery']
-//        }
-//    }
-//});
-
 
 define('subapp/store/store_banner',['jquery', 'libs/Class','libs/slick'], function(
     $, Class
 ){
     var StoreBanner= Class.extend({
         init: function(){
-            console.log('hello lqqqqqq,hello good store!');
+            console.log('subapp good store start !');
+            this.sameHeightFrame('user-latest-article','latest-actions-sidebar');
 
+        },
+        sameHeightFrame:function(leftChildId,rightChildId){
+            var leftChildHeight = this.getElementHeight(leftChildId);
+            var rightChildHeight = this.getElementHeight(rightChildId);
+            if(rightChildHeight > leftChildHeight){
+                console.log('right child height is more than left child');
+                this.getElement(rightChildId).style.height = leftChildHeight + "px";
+            }
+        },
+        getElementHeight:function(id){
+            return this.getElement(id).offsetHeight;
+        },
+        getElement:function(id){
+            return document.getElementById(id);
         }
     });
     return StoreBanner;
