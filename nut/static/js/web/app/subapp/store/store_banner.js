1e62b258543fad5c79ec6395ab47377569838273
AzureRay <476468781@qq.com>
2016-03-07 14:11:41 +0800

    restore and test

--- nut/static/js/web/app/subapp/store/store_banner.js
+++ nut/static/js/web/app/subapp/store/store_banner.js
@@ -5,28 +5,11 @@ define(['jquery', 'libs/Class','libs/slick'], function(
     var StoreBanner= Class.extend({
         init: function(){
             console.log('subapp good store start !');
-            this.sameHeightFrame('user-latest-article','latest-actions-sidebar');
+            this.sameHeightFrame();
 
         },
-        sameHeightFrame:function(leftChildId,rightChildId){
-            if(this.getScreenWidth > 767){
-                console.log('screen width is more than 767px');
-                if(rightChildHeight > leftChildHeight){
-                    console.log('right child height is more than left child');
-                var leftChildHeight = this.getElementHeight(leftChildId);
-                var rightChildHeight = this.getElementHeight(rightChildId);
-                this.getElement(rightChildId).style.height = leftChildHeight + "px";
-                }
-            }
-        },
-        getElementHeight:function(id){
-            return this.getElement(id).offsetHeight;
-        },
-        getElement:function(id){
-            return document.getElementById(id);
-        },
-        getScreenWidth:function(){
-            return document.body.clientWidth;
+        sameHeightFrame:function(){
+            console.log('cry me');
         }
     });
     return StoreBanner;
