c19d3274d4ada917ca2e53ed3e0b452a18da5c27
An Chen <anchen@guoku.com>
2016-03-07 09:51:28 +0800

    sort recommend user by points

--- nut/apps/core/manager/account.py
+++ nut/apps/core/manager/account.py
@@ -51,8 +51,9 @@ class GKUserQuerySet(models.query.QuerySet):
 
     def recommended_user(self):
         return self.authorized_user()\
+                   .select_related('authorized_profile')\
                    .filter(authorized_profile__is_recommended_user=True)\
-                   .order_by('-points')
+                   .order_by('-authorized_profile__points')
 
 
 class GKUserManager(BaseUserManager):
