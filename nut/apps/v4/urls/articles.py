fae256997d474ab3c5dc39db543c7b5fcaed2aa5
jiaxin <edison7500@gmail.com>
2016-03-14 12:12:35 +0800

    add articles dig and undig in v4 api

--- nut/apps/v4/urls/articles.py
+++ nut/apps/v4/urls/articles.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url, patterns
-from apps.v4.views.articles import ArticlesListView, ArticleSearchView, ArticleTagView
+from apps.v4.views.articles import ArticlesListView, ArticleSearchView, \
+    ArticleTagView, ArticleDigView, ArticleUnDigView
 
 urlpatterns = patterns(
     'apps.v4.views.articles',
     url(r'^$', ArticlesListView.as_view(), name='v4_articles_list'),
     url(r'^search/?$', ArticleSearchView.as_view(), name='v4_articles_search'),
     url(r'^tags/(?P<tag_name>\w+)/?', ArticleTagView.as_view(), name='v4_articles_tags'),
+    url(r'^dig/$', ArticleDigView.as_view(), name='v4_article_dig'),
+    url(r'^undig/$', ArticleUnDigView.as_view(), name='v4_article_undig'),
 )
 
 __author__ = 'xiejiaxin'
