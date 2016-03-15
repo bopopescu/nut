9df7091a76232b53fee0c3e564cb69262a5b2d08
AzureRay <476468781@qq.com>
2016-03-15 15:51:58 +0800

    update seller page js file but seem does not work

--- nut/static/js/web/app/seller_page_app.js
+++ nut/static/js/web/app/seller_page_app.js
@@ -2,11 +2,21 @@
 require([
         'libs/polyfills',
         'jquery',
-        'subapp/entitylike'
+        'subapp/entitylike',
+        'subapp/topmenu',
+        'subapp/gotop',
+        'subapp/user_follow'
     ],
     function(polyfill,
              jQuery,
-             AppEntityLike
+             AppEntityLike,
+             Menu,
+             GoTop,
+             UserFollow
+
     ){
         var app_like = new  AppEntityLike();
+        var menu = new Menu();
+        var goto = new GoTop();
+        var user_follow = new UserFollow();
     });
