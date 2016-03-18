ddb084b8d9ae6f0ceeba4308bfb7a78dafa83c2b
jiaxin <edison7500@gmail.com>
2016-03-18 13:49:37 +0800

    fixed bug

--- nut/apps/v4/views/__init__.py
+++ nut/apps/v4/views/__init__.py
@@ -54,6 +54,7 @@ def decorate_taobao_url(url, ttid=None, sid=None, outer_code=None, sche=None):
 
 class APIJsonView(BaseJsonView):
 
+    @csrf_exempt
     @check_sign
     def dispatch(self, request, *args, **kwargs):
         return super(APIJsonView, self).dispatch(request, *args, **kwargs)
