9a9f8cc56a90bb7e785fb89c5375894341bf8947
AzureRay <476468781@qq.com>
2016-03-22 16:57:37 +0800

    update js

--- nut/static/js/web/app/subapp/store/annual_report.js
+++ nut/static/js/web/app/subapp/store/annual_report.js
@@ -5,6 +5,27 @@ define(['jquery', 'libs/Class','libs/slick'], function(
     var AnnualReport= Class.extend({
         init: function () {
             console.log('hello rose.write codes here');
+            console.log(this.getPageHeight());
+            console.log(this.getScrollHeight);
+            console.log(this.getFooterHeight('guoku_footer'));
+
+        },
+        hideReport:function(){
+            var pageHeight = this.getPageHeight();
+            var scrollHeight = this.getScrollHeight();
+            var bottomHeight = this.getFooterHeight('guoku_footer');
+            console.log(bottomHeight);
+            var bottomHeight = this.getFooterHeight('guoku_footer') + 50;
+            console.log(bottomHeight);
+        },
+        getScrollHeight:function(){
+            return document.body.scrollTop;
+        },
+        getPageHeight:function(){
+            return document.body.scrollHeight;
+        },
+        getFooterHeight:function(id){
+            return document.getElementById(id).offsetHeight;
         }
     });
     return AnnualReport;
