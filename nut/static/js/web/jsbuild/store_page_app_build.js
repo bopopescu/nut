24ab333b83b6ccc06301ef8d0601e8b2b029d2a6
AzureRay <476468781@qq.com>
2016-03-22 19:20:21 +0800

    a bug

--- nut/static/js/web/jsbuild/store_page_app_build.js
+++ nut/static/js/web/jsbuild/store_page_app_build.js
@@ -3617,21 +3617,20 @@ define('subapp/store/annual_report',['jquery', 'libs/Class','libs/slick'], funct
     var AnnualReport= Class.extend({
         init: function () {
             console.log('hello rose.write codes here');
-            console.log(this.getPageHeight());
-            console.log(this.getScrollHeight);
-            console.log(this.getFooterHeight('guoku_footer'));
+            if(this.getScrollHeight() > this.getPageHeight() - this.getFooterHeight('guoku_footer') - 50 ){
+                console.log('hide');
+                this.hideReport();
+            }else{
+                console.log('show');
+            }
 
         },
         hideReport:function(){
-            var pageHeight = this.getPageHeight();
-            var scrollHeight = this.getScrollHeight();
-            var bottomHeight = this.getFooterHeight('guoku_footer');
-            console.log(bottomHeight);
-            var bottomHeight = this.getFooterHeight('guoku_footer') + 50;
-            console.log(bottomHeight);
+            var report = document.getElementById(id);
+            $(report).hide();
         },
         getScrollHeight:function(){
-            return document.body.scrollTop;
+            return $(window).scrollTop;
         },
         getPageHeight:function(){
             return document.body.scrollHeight;
