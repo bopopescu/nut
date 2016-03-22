08c593c64c248288f9a71153a89d69c345e44d1c
NolanZhao <zhaoxu@syscloud.cn>
2016-03-22 15:38:45 +0800

    brand

--- nut/apps/web/views/brand.py
+++ nut/apps/web/views/brand.py
@@ -1,6 +1,8 @@
-from django.views.generic import  DetailView, ListView
-
-from apps.core.models import Brand
+from django.http import Http404
+from django.views.generic import ListView
+from haystack.query import SearchQuerySet
+from django.shortcuts import get_object_or_404
+from apps.core.models import Brand, Entity
 
 # TODO Brand front end view
 
@@ -9,14 +11,20 @@ class BrandListView(ListView):
     model = Brand
     template_name = 'web/brand/list.html'
     context_object_name = 'brands'
+
     def get_queryset(self):
         return Brand.objects.all()
 
 
 class BrandDetailView(ListView):
     template_name = 'web/brand/detail.html'
+    paginate_by = 40
+    context_object_name = 'entities'
 
     def get_queryset(self):
-        pass
+        brand_pk= self.kwargs.pop('pk')
+        brand = get_object_or_404(Brand, pk=brand_pk)
+        sqs = SearchQuerySet().models(Entity).filter(brand=brand.name)
 
+        return sqs
 
