e0b97300123bfc3ab7f84b05940fa49f4e945196
AzureRay <476468781@qq.com>
2016-03-23 11:04:34 +0800

    clear my codes

--- nut/static/js/web/app/subapp/store/annual_report.js
+++ nut/static/js/web/app/subapp/store/annual_report.js
@@ -5,27 +5,10 @@ define(['jquery', 'libs/Class','libs/slick'], function(
     var AnnualReport= Class.extend({
         init: function () {
             console.log('hello rose.write codes here');
-            if(this.getScrollHeight() > this.getPageHeight() - this.getFooterHeight('guoku_footer') - 50 ){
-                console.log('hide');
-                this.hideReport();
-            }else{
-                console.log('show');
-            }
+
 
         },
-        hideReport:function(){
-            var report = document.getElementById(id);
-            $(report).hide();
-        },
-        getScrollHeight:function(){
-            return $(window).scrollTop;
-        },
-        getPageHeight:function(){
-            return document.body.scrollHeight;
-        },
-        getFooterHeight:function(id){
-            return document.getElementById(id).offsetHeight;
-        }
+
     });
     return AnnualReport;
 });
