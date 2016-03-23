86f5e5fcddf275de656cc4a4d0ef442ee5439652
NolanZhao <zhaoxu@syscloud.cn>
2016-03-23 13:53:58 +0800

    brand entity like unlike

--- nut/apps/web/views/brand.py
+++ nut/apps/web/views/brand.py
@@ -1,9 +1,15 @@
 from django.http import Http404
 from django.views.generic import ListView
 from django.shortcuts import get_object_or_404
-from apps.core.models import Brand
+from apps.core.models import Brand, Entity_Like
+from braces.views import AjaxResponseMixin
+from braces.views import JSONResponseMixin
+from django.template import loader
+from django.template import RequestContext
+from apps.core.utils.http import JSONResponse
 
 # TODO Brand front end view
+
 
 
 class BrandListView(ListView):
@@ -15,7 +21,7 @@ class BrandListView(ListView):
         return Brand.objects.all()
 
 
-class BrandDetailView(ListView):
+class BrandDetailView(JSONResponseMixin, AjaxResponseMixin, ListView):
     template_name = 'web/brand/detail.html'
     paginate_by = 40
     context_object_name = 'entities'
@@ -27,9 +33,41 @@ class BrandDetailView(ListView):
 
         return sqs
 
+
     def get_context_data(self, **kwargs):
         brand_pk = self.kwargs.get('pk')
         context = super(BrandDetailView, self).get_context_data(**kwargs)
         context['brand'] = get_object_or_404(Brand, pk=brand_pk)
+
+        el = list()
+        sqs = self.get_queryset()
+
+        e_ids = [e.entity_id for e in sqs]
+
+
+        if self.request.user.is_authenticated():
+             el = Entity_Like.objects.user_like_list(user=self.request.user,
+                                                    entity_list=e_ids
+                                                    ).using('slave')
+
+        context['user_entity_likes'] = el
         return context
 
+
+    def get_ajax(self, request, *args, **kwargs):
+        self.object_list = getattr(self, 'object_list', self.get_queryset())
+        context = self.get_context_data()
+        template = 'web/main/partial/selection_ajax.html'
+        _t = loader.get_template(template)
+        _c = RequestContext(
+            request,
+            context
+        )
+        _data = _t.render(_c)
+        return JSONResponse(
+            data={
+                'data': _data,
+                'status': 1
+            },
+            content_type='text/html; charset=utf-8',
+        )
