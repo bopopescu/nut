84eb6c1de8b7381a27c7a009f221dec87de52563
An Chen <anchen@guoku.com>
2016-03-29 17:41:17 +0800

    update good store js

--- nut/static/js/web/app/subapp/store/store_banner.js
+++ nut/static/js/web/app/subapp/store/store_banner.js
@@ -1,70 +1,69 @@
 
-define(['jquery', 'libs/Class','libs/slick'], function(
-    $, Class
+define(['jquery', 'libs/Class','libs/slick','fastdom'], function(
+    $, Class, slick , fastdom
 ){
-    var StoreBanner= Class.extend({
-        init: function () {
-            this.init_slick();
-            console.log('subapp good store start !');
-            //this.sameHeightFrame('user-latest-article','latest-actions-sidebar');
-        },
-        init_slick:function(){
-            $('#index-banners').slick({
-                centerMode: true,
-                arrows: true,
-                slidesToShow: 1,
-                centerPadding:'18%',
-                dots:false,
+            var StoreBanner= Class.extend({
+                init: function () {
+                    this.init_slick();
+                    console.log('subapp good store start !');
+                    //this.sameHeightFrame('user-latest-article','latest-actions-sidebar');
+                },
+                init_slick:function(){
+                    $('#index-banners').slick({
+                        centerMode: true,
+                        arrows: true,
+                        slidesToShow: 1,
+                        centerPadding:'18%',
+                        dots:false,
 
-                //centerPadding: '60px',
-                //slidesToShow: 3,
-                responsive: [
-                    {
-                        breakpoint: 768,
-                        settings: {
-                            centerMode:false,
-                            slidesToShow:1,
-                            slidesToScroll:1,
-                            infinite: true
-                        }
-                    },
-                ]
-            }).on(
-                'beforeChange',function(event,slick,currentSlide,nextSlide){
-                    console.log('before change,currentSlide:');
-                    console.log(currentSlide);
-                    console.log('before change,nextSlide:');
-                    console.log(nextSlide);
+                        //centerPadding: '60px',
+                        //slidesToShow: 3,
+                        responsive: [
+                            {
+                                breakpoint: 768,
+                                settings: {
+                                    centerMode:false,
+                                    slidesToShow:1,
+                                    slidesToScroll:1,
+                                    infinite: true
+                                }
+                            },
+                        ]
+                    }).on('beforeChange', this.beforeSlide.bind(this));
+                },
 
+                beforeSlide: function(event,slick,currentSlide,nextSlide){
+                            console.log('before change,currentSlide:');
+                            console.log(currentSlide);
+                            console.log('before change,nextSlide:');
+                            console.log(nextSlide);
+                            this.nextSlide = nextSlide;
+                            fastdom.write(this.doRenderSlide.bind(this));
 
-                }).on(
-                'afterChange',function(event,slick,currentSlide){
-                    console.log('after change,currentSlide:');
-                    console.log(currentSlide);
-                    $('#index-banners .banner-image-cell').each(function(){
-                        if($(this).data('data-slick-index') == currentSlide){
-                            $(this).children('div.user-latest-article-intro-wrapper').addClass('show');
-                        } else{
-                              $(this).children('div.user-latest-article-intro-wrapper').addClass('hidden');
-                }       }
-            );      });
-        },
+                },
 
-        //sameHeightFrame: function (leftId,rightId) {
-        //    var leftChildHeight = this.getElementHeight(leftId);
-        //    var rightChildHeight = this.getElementHeight(rightId);
-        //    var rightChild = this.getElement(rightId);
-        //    if (rightChildHeight > leftChildHeight) {
-        //        rightChild.style.height = leftChildHeight + "px";
-        //    }
-        //},
-        //getElement:function(id){
-        //    return document.getElementById(id);
-        //},
-        //getElementHeight:function(id){
-        //    return this.getElement(id).offsetHeight;
-        //}
-    });
+                doRenderSlide:function(){
+                        $('.banner-image-cell').removeClass('gk-slide-current');
+                        var selector = '.gk-slide-'+  this.nextSlide ;
+                        $(selector).addClass('gk-slide-current');
+                        console.log('done');
+                },
+
+                //sameHeightFrame: function (leftId,rightId) {
+                //    var leftChildHeight = this.getElementHeight(leftId);
+                //    var rightChildHeight = this.getElementHeight(rightId);
+                //    var rightChild = this.getElement(rightId);
+                //    if (rightChildHeight > leftChildHeight) {
+                //        rightChild.style.height = leftChildHeight + "px";
+                //    }
+                //},
+                //getElement:function(id){
+                //    return document.getElementById(id);
+                //},
+                //getElementHeight:function(id){
+                //    return this.getElement(id).offsetHeight;
+                //}
+            });
     return StoreBanner;
 });
 
