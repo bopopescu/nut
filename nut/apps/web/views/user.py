8f9bf518a6a80eb0cb3ded17089b27af8d18aff1
NolanZhao <zhaoxu@syscloud.cn>
2016-03-31 14:20:45 +0800

    brand page and authorized seller page

--- nut/apps/web/views/user.py
+++ nut/apps/web/views/user.py
@@ -598,7 +598,12 @@ class UserIndex(UserPageMixin, DetailView):
         _entity_list = Entity.objects.get_user_added_entities(current_user)[:8]
 
         context_data['author_articles'] = self.get_author_articles(current_user)
-        context_data['author_articles_main_page'] = self.get_author_articles(current_user)[:12]
+        if self.get_author_articles(current_user):
+            if len(self.get_author_articles(current_user)) >= 12:
+                context_data['author_articles_main_page'] = self.get_author_articles(current_user)[:12]
+            else:
+                context_data['author_articles_main_page'] = self.get_author_articles(current_user)
+
         current_user_like_articles = Article_Dig.objects\
                                                .user_dig_list(user=current_user,\
                                                               article_list=Article.objects.published())[:3]
