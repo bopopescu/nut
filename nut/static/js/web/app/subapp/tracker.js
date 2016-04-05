3a57f8fa19a914db937628a6ac04dad2bfa9ba1a
An Chen <anchen@guoku.com>
2016-04-05 15:01:16 +0800

    initial commit

--- nut/static/js/web/app/subapp/tracker.js
+++ nut/static/js/web/app/subapp/tracker.js
@@ -3,13 +3,15 @@ define(['libs/Class'], function (Class) {
     //singleton for tracker
 
     var Tracker = Class.extend({
-        init: function () {
-            console.log('tracker init');
-            if (!instance) {
-                instance = this;
-            }
+        init: function (tracker_list) {
+            tracker_list.map(function(item){
+                  var selector = item.selector;
+                   var event = item.event;
+                  $(selector).on(event, function(){
+                     _hmt.push('_trackEvent', '')
+                  })
+            });
         }
     });
-    var instance =  new Tracker();
-    return instance;
+    return Tracker;
 });
