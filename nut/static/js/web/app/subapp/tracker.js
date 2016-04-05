fe7af74f7ce00859e71c0b1368852bb725a316be
An Chen <anchen@guoku.com>
2016-04-05 15:15:02 +0800

    update

--- nut/static/js/web/app/subapp/tracker.js
+++ nut/static/js/web/app/subapp/tracker.js
@@ -7,9 +7,13 @@ define(['libs/Class'], function (Class) {
             tracker_list.map(function(item){
                   var selector = item.selector;
                    var event = item.event;
-                  $(selector).on(event, function(){
+
+                  $(selector).on(event, function(event){
+                      var target = event.currentTarget;
+                      var lable = $(target).attr(item.label);
+                      // 闭包
                      _hmt.push('_trackEvent', '')
-                  })
+                  });
             });
         }
     });
