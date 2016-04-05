3263acd8097b96190f0d3c4f3e7d34b07922c915
NolanZhao <zhaoxu@syscloud.cn>
2016-04-05 12:00:40 +0800

    brand detail url change

--- nut/apps/web/views/brand.py
+++ nut/apps/web/views/brand.py
@@ -18,9 +18,8 @@ class BrandDetailView(ListView):
     paginate_by = 24
     context_object_name = 'entities'
 
-
     def get_order_by(self):
-        order_by = self.kwargs.get('order_by', 'pub_time')
+        order_by = self.request.GET.get("order_by", 'pub_time')
         return order_by
 
     def get_queryset(self):
@@ -34,9 +33,9 @@ class BrandDetailView(ListView):
 
         return sqs
 
-    def get_context_data(self,*args, **kwargs):
+    def get_context_data(self, *args, **kwargs):
         brand_pk = self.kwargs.get('pk')
-        context = super(BrandDetailView, self).get_context_data(*args,**kwargs)
+        context = super(BrandDetailView, self).get_context_data(*args, **kwargs)
         context['brand'] = get_object_or_404(Brand, pk=brand_pk)
         context['pk'] = brand_pk
         context['sort_method'] = self.get_order_by()
@@ -45,30 +44,9 @@ class BrandDetailView(ListView):
         sqs = self.get_queryset()
         e_ids = [e.entity_id for e in sqs]
         if self.request.user.is_authenticated():
-             el = Entity_Like.objects.user_like_list(user=self.request.user,
-                                                    entity_list=e_ids
-                                                    ).using('slave')
+             el = Entity_Like.objects.user_like_list(user=self.request.user, entity_list=e_ids).using('slave')
         context['user_entity_likes'] = el
         return context
 
-
-
-
-
-    # def get_queryset(self):
-    #     cid = self.get_category_id()
-    #     order_by_like = False
-    #     if self.get_order_by() == 'olike':
-    #         order_by_like = True
-    #     self.cid = cid
-    #     entity_list = Entity.objects.sort(category_id=cid,
-    #                                       like=order_by_like).exclude(
-    #         selection_entity__is_published=False)
-    #     return entity_list
-    #
-    #
-    #     order_by_like = False
-    #     if self.get_order_by() == 'olike':
-    #         order_by_like = True
 
 
