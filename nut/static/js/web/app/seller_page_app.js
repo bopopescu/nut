2ab9048d7b054ce538cd10bfa1dcd0855aad024c
AzureRay <476468781@qq.com>
2016-04-06 13:46:57 +0800

    clean

--- nut/static/js/web/app/seller_page_app.js
+++ nut/static/js/web/app/seller_page_app.js
@@ -3,20 +3,11 @@ require([
         'libs/polyfills',
         'jquery',
         'subapp/entitylike',
-        'subapp/topmenu',
-        'subapp/gotop',
-        'subapp/user_follow'
     ],
     function(polyfill,
              jQuery,
-             AppEntityLike,
-             Menu,
-             GoTop,
-             UserFollow
+             AppEntityLike
 
     ){
         var app_like = new  AppEntityLike();
-        var menu = new Menu();
-        var goto = new GoTop();
-        var user_follow = new UserFollow();
     });
