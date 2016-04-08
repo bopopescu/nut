0f172db843c46d61140b979ce0efaf4731afd5ae
zuoning <zuoning@ycombo.com>
2016-04-08 18:44:13 +0800

    tracker zn

--- nut/static/js/web/app/subapp/tracker.js
+++ nut/static/js/web/app/subapp/tracker.js
@@ -2,22 +2,38 @@ define(['libs/Class'], function (Class) {
     var Tracker = Class.extend({
         init: function (tracker_list) {
             tracker_list.map(function(ele){
-                  var wrapper = ele.wrapper;
                   var selector = ele.selector;
                   var trigger = ele.trigger;
-                  $(wrapper).on(trigger, selector, function(event){
-                      return (function() {
-                              var target = event.currentTarget;
-                              console.log(target);
-                              var category = ele.category;
-                              var action = ele.action;
-                              var opt_label = $(target).attr(ele.label) || $(target).parent().attr(ele.label);
-                              var opt_value = $(target).attr(ele.value) || $(target).parent().attr(ele.value);
-                               //闭包
-                             _hmt.push('_trackEvent', category, action, opt_label, opt_value);
-                      })();
+                  if (ele.wrapper) {
+                      var wrapper = ele.wrapper;
+                      $(wrapper).on(trigger, selector, function(event){
+                          return (function() {
+                                  var target = event.currentTarget;
+                                  var category = ele.category;
+                                  var action = ele.action;
+                                  var opt_label = $(target).attr(ele.label) || $(target).parent().attr(ele.label);
+                                  var opt_value = $(target).attr(ele.value) || $(target).parent().attr(ele.value);
+                                   //闭包
+                                 _hmt.push('_trackEvent', category, action, encodeURIComponent(opt_label), opt_value);
+                          })();
 
-                  });
+                      });
+                  } else {
+                      $(selector).on(trigger, function(event){
+                          return (function() {
+                                  var target = event.currentTarget;
+                                  var category = ele.category;
+                                  var action = ele.action;
+                                  var opt_label = $(target).attr(ele.label) || $(target).parent().attr(ele.label);
+                                  var opt_value = $(target).attr(ele.value) || $(target).parent().attr(ele.value);
+                                   //闭包
+                                 _hmt.push('_trackEvent', category, action, encodeURIComponent(opt_label), opt_value);
+                          })();
+
+                      });
+                  }
+
+
 
             });
         }
