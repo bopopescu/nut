426d4554a9391b3083ca599a5958ac1d2f700e2f
AzureRay <476468781@qq.com>
2016-04-15 10:27:55 +0800

    update follow btn

--- nut/static/js/web/app/gkuser_profile_app.js
+++ nut/static/js/web/app/gkuser_profile_app.js
@@ -2,15 +2,30 @@ require([
         'jquery',
         'subapp/topmenu',
         'subapp/gotop',
-        'subapp/user_follow'
+        'subapp/user_follow',
+        'subapp/tracker',
     ],
 
     function (
         jQuery,
         Menu,
         UserFollow,
-        GoTop) {
+        GoTop,
+        Tracker) {
         var menu = new Menu();
         var goto = new GoTop();
         var user_follow = new UserFollow();
+        var tracker_list = [
+            {
+                selector: '.follow.button-blue',
+                trigger: 'click',
+                category: 'user_detail',
+                action: 'follow',
+                label: 'data-current-user-nickname',
+                value: 'data-user-id',
+                wrapper: '.user-info-wrapper'
+            }
+        ];
+
+        var tracker = new Tracker(tracker_list);
     });
--- nut/static/js/web/jsbuild/gkuser_profile_app_build.js
+++ nut/static/js/web/jsbuild/gkuser_profile_app_build.js
@@ -1489,21 +1489,78 @@ define('subapp/user_follow',['libs/Class','jquery', 'subapp/account'], function(
     return UserFollow;
 });
 
+define('subapp/tracker',['libs/Class'], function (Class) {
+    var Tracker = Class.extend({
+        init: function (tracker_list) {
+            tracker_list.map(function(ele){
+                  var selector = ele.selector;
+                  var trigger = ele.trigger;
+                  if (ele.wrapper) {
+                      var wrapper = ele.wrapper;
+                      $(wrapper).delegate(selector, trigger, function(event){
+                          return (function() {
+                                  var target = event.currentTarget;
+                                  var category = ele.category;
+                                  var action = ele.action;
+                                  var opt_label = $(target).attr(ele.label) || $(target).parent().attr(ele.label);
+                                  var opt_value = $(target).attr(ele.value) || $(target).parent().attr(ele.value);
+                                   //闭包
+                                 _hmt.push('_trackEvent', category, action, encodeURIComponent(opt_label), opt_value);
+                          })();
+
+                      });
+                  } else {
+                      $(selector).on(trigger, function(event){
+                          return (function() {
+                                  var target = event.currentTarget;
+                                  var category = ele.category;
+                                  var action = ele.action;
+                                  var opt_label = $(target).attr(ele.label) || $(target).parent().attr(ele.label);
+                                  var opt_value = $(target).attr(ele.value) || $(target).parent().attr(ele.value);
+                                   //闭包
+                                 _hmt.push('_trackEvent', category, action, encodeURIComponent(opt_label), opt_value);
+                          })();
+
+                      });
+                  }
+
+
+
+            });
+        }
+    });
+    return Tracker;
+});
 require([
         'jquery',
         'subapp/topmenu',
         'subapp/gotop',
-        'subapp/user_follow'
+        'subapp/user_follow',
+        'subapp/tracker',
     ],
 
     function (
         jQuery,
         Menu,
         UserFollow,
-        GoTop) {
+        GoTop,
+        Tracker) {
         var menu = new Menu();
         var goto = new GoTop();
         var user_follow = new UserFollow();
+        var tracker_list = [
+            {
+                selector: '.follow.button-blue',
+                trigger: 'click',
+                category: 'user_detail',
+                action: 'follow',
+                label: 'data-current-user-nickname',
+                value: 'data-user-id',
+                wrapper: '.user-info-wrapper'
+            }
+        ];
+
+        var tracker = new Tracker(tracker_list);
     });
 
 define("gkuser_profile_app", function(){});
