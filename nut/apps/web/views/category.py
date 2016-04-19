0492f4b87d61ef29b4a5dd829bfa2095aee42b6e
NolanZhao <zhaoxu@syscloud.cn>
2016-04-19 23:28:21 +0800

    category group ajax page load

--- nut/apps/web/views/category.py
+++ nut/apps/web/views/category.py
@@ -39,6 +39,12 @@ class SubCategoryListView(ListView):
     template_name = "web/category/list.html"
     context_object_name = "sub_categories"
 
+    def get_refresh_time(self):
+        refresh_time = self.request.GET \
+            .get('t', datetime.now() \
+                 .strftime('%Y-%m-%d %H:%M:%S'))
+        return refresh_time
+
     def get_queryset(self):
         id = self.kwargs.get('id')
         sub_categories = Sub_Category.objects.filter(group=id).exclude(title='+')
@@ -51,27 +57,32 @@ class SubCategoryListView(ListView):
         return context
 
 
-class CategoryGroupListView(TemplateResponseMixin, ContextMixin, View):
-    http_method_names = ['get']
+class CategoryGroupListView(JSONResponseMixin, AjaxResponseMixin, ListView):
     template_name = 'web/category/detail.html'
+    model = Entity
+    paginate_by = 36
+    paginator_class = Jpaginator
+    ajax_template_name = 'web/category/cate_selection_ajax.html'
+
+    def get_refresh_time(self):
+        refresh_time = self.request.GET \
+            .get('t', datetime.now() \
+                 .strftime('%Y-%m-%d %H:%M:%S'))
+        return refresh_time
 
     def get_order_by(self):
         order_by = self.kwargs.get('order_by', 'pub_time')
         return order_by
 
-    def get(self, request, *args, **kwargs):
-        # log.info(kwargs)
+    def get_group_id(self):
+        gid = self.kwargs.get('gid', None)
+        return int(gid)
 
-        gid = kwargs.pop('gid', None)
-        _page = request.GET.get('page', 1)
-        category = Category.objects.get(pk=gid)
+    def get_queryset(self):
+        gid = self.get_group_id()
 
         sub_categories_ids = Sub_Category.objects.filter(group=gid).values_list(
             'id', flat=True)
-
-        sub_categories = Sub_Category.objects.filter(group=gid).exclude(title='+')
-        if len(sub_categories) > 10:
-            sub_categories = sub_categories[:10]
 
         order_by_like = False
         if self.get_order_by() == 'olike':
@@ -81,31 +92,58 @@ class CategoryGroupListView(TemplateResponseMixin, ContextMixin, View):
             category_ids=list(sub_categories_ids),
             like=order_by_like,).filter(buy_links__status=2)
 
-        paginator = ExtentPaginator(_entity_list, 24)
+        return _entity_list
+
+    def get_ajax(self, request, *args, **kwargs):
+        status = 1
+        self.object_list = getattr(self, 'object_list', self.get_queryset())
+        context = self.get_context_data()
+        _template = self.ajax_template_name
+        _t = loader.get_template(_template)
+        _c = RequestContext(
+            request,
+            context
+        )
         try:
-            _entities = paginator.page(_page)
-        except PageNotAnInteger:
-            _entities = paginator.page(1)
-        except EmptyPage:
-            raise Http404
-        # log.info(entity_list)
+            if not context['entities'].has_next():
+                status = 0
+        except:
+            pass
+        _data = _t.render(_c)
+        return JSONResponse(
+            data={
+                'data': _data,
+                'status': status
+            },
+            content_type='text/html; charset=utf-8',
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryGroupListView, self).get_context_data(**kwargs)
+        entities = context['page_obj']
+        gid = self.get_group_id()
+        category = Category.objects.get(pk=gid)
+
+        sub_categories = Sub_Category.objects.filter(group=gid).exclude(title='+')
+        if len(sub_categories) > 10:
+            sub_categories = sub_categories[:10]
 
         el = []
-        if request.user.is_authenticated():
-            e = _entities.object_list
+        if self.request.user.is_authenticated():
+            e = entities.object_list
             el = Entity_Like.objects.filter(entity_id__in=tuple(e),
-                                            user=request.user).values_list(
+                                            user=self.request.user).values_list(
                 'entity_id', flat=True)
 
-        context = {
-            'entities': _entities,
-            'user_entity_likes': el,
-            'category': category,
-            'sub_categories': sub_categories,
-            'gid': gid,
-            'sort_method': self.get_order_by()
-        }
-        return self.render_to_response(context)
+        context['gid'] = gid
+        context['entities'] = entities
+        context['sort_method'] = self.get_order_by()
+        context['user_entity_likes'] = el
+        context['category'] = category
+        context['sub_categories'] = sub_categories
+        context['refresh_datetime'] = self.get_refresh_time()
+        return context
+
 
 
 class OldCategory(RedirectView):
