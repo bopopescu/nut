bd2e3c0a44283d6c32df1edc6e99875dfa42dbf2
AzureRay <476468781@qq.com>
2016-04-20 13:59:15 +0800

    begin entity slick in store page

---
+++ nut/static/js/web/app/subapp/store/entity_slick.js
@@ -0,0 +1,34 @@
+define(['jquery', 'libs/Class','libs/slick','fastdom'], function(
+    $, Class, slick , fastdom
+){
+            var CategorySlick= Class.extend({
+                init: function () {
+                    this.init_slick();
+                    console.log('entity horizontal scrolling starts !');
+                },
+                init_slick:function(){
+                    //$('#category-item-container').slick({
+                    //    arrows: true,
+                    //    //on mobile,set slidesToshow and slidesToScroll like android
+                    //    slidesToShow: 6,
+                    //    slidesToScroll:6,
+                    //    autoplay:true,
+                    //    dots:false,
+                    //
+                    //    responsive: [
+                    //        {
+                    //            breakpoint: 768,
+                    //            settings: {
+                    //                slidesToShow:3,
+                    //                slidesToScroll:3,
+                    //                autoplay:true,
+                    //                dots:false
+                    //            }
+                    //        }
+                    //    ]
+                    //});
+
+                }
+            });
+    return CategorySlick;
+});
