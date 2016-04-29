3c88fb2e1e400227813381d575523cfa0eb3d94c
AzureRay <476468781@qq.com>
2016-04-29 15:41:30 +0800

    add follow btn js

--- nut/static/js/web/app/store_page_app.js
+++ nut/static/js/web/app/store_page_app.js
@@ -3,18 +3,21 @@ require([
         'subapp/topmenu',
         'subapp/store/store_banner',
         'subapp/store/annual_report',
-        'subapp/store/entity_slick'
+        'subapp/store/entity_slick',
+        'subapp/user_follow'
     ],
     function (
               jQuery,
               Menu,
               StoreBanner,
               AnnualReport,
-              EntitySlick
+              EntitySlick,
+              UserFollow
     ){
         var menu = new Menu();
         var store_banner = new StoreBanner();
         var annual_report = new AnnualReport();
         var entity_slick = new EntitySlick();
+         var user_follow = new UserFollow();
 });
 
