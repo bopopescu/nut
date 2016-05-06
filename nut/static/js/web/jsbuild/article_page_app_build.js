a4b2594126b1d7682ab5d2a855986ee0643a5fdd
AzureRay <476468781@qq.com>
2016-05-06 14:05:25 +0800

    sidebar js finish

--- nut/static/js/web/jsbuild/article_page_app_build.js
+++ nut/static/js/web/jsbuild/article_page_app_build.js
@@ -2801,8 +2801,49 @@ define('subapp/article/article_sidebar',['jquery','libs/underscore','libs/Class'
         var ArticleSidebar = Class.extend({
             init: function(){
                 console.log('article detail dig sidebar');
+                this.fixedSidebar = $('.article-sidebar-wrapper');
+                if (this.fixedSidebar.length > 0){
+                    this.setupWatcher();
+                }else{
+                    return ;
+                }
 
+            },
+             setupWatcher:function(){
+                $(window).scroll(this.onScroll.bind(this));
+            },
+            onScroll:function(){
+                if(this.read){
+                    fastdom.clear(this.read);
+                }
+                this.read = fastdom.read(this.doRead.bind(this));
+                if(this.write){
+                    fastdom.clear(this.write);
+                }
+                this.write = fastdom.write(this.doWrite.bind(this));
+            },
+            doRead: function(){
+                 this.screenHeight = window.screen.height;
+                this.shareWrapperTop = $('.share-wrapper')[0].getBoundingClientRect().top;
+            },
+            doWrite: function(){
+                var that = this ;
+                if (!this.shareWrapperTop){return ;}
+                if (this.shareWrapperTop + 70 <= this.screenHeight){
+                    fastdom.write(function(){
+                        that.fixedSidebar.addClass('hidden-article-sidebar');
+                        that.fixedSidebar.hide();
+                    });
+
+                }else{
+
+                    fastdom.write(function(){
+                        that.fixedSidebar.removeClass('hidden-article-sidebar');
+                        that.fixedSidebar.show();
+                    });
+                }
             }
+
         });
         return ArticleSidebar;
     });
