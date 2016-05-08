f60ddec1274c621dda20a618d7e23f7beb0626d5
AzureRay <476468781@qq.com>
2016-05-08 17:26:56 +0800

    update

--- nut/static/js/web/app/subapp/article/article_remark.js
+++ nut/static/js/web/app/subapp/article/article_remark.js
@@ -1,3 +1,23 @@
-/**
- * Created by zhangyao on 16/5/8.
- */
+define([
+    'libs/Class',
+    'subapp/account',
+    'subapp/article/article_user_comment',
+    'libs/fastdom',
+    'utils/io',
+    'libs/csrf'
+],function(
+    Class,
+    AccountApp,
+    ArticleCommentManager,
+    fastdom,
+    io
+){
+    var ArticleRemark = Class.extend({
+        init: function(){
+            console.log('article remark begin');
+            this.accountApp = new AccountApp();
+            this.articleCommentManager = new ArticleCommentManager();
+        }
+    });
+    return ArticleRemark;
+});
