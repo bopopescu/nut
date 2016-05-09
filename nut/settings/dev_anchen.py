ec5a0e8a38df66e6c41b143df2f471cd18f22e4d
An Chen <anchen@guoku.com>
2016-05-09 15:15:43 +0800

    update

--- nut/settings/dev_anchen.py
+++ nut/settings/dev_anchen.py
@@ -14,7 +14,7 @@ IMAGE_HOST = 'http://imgcdn.guoku.com/'
 
 # LOCAL_IMG_DEBUG=True
 # IMAGE_HOST = 'http://127.0.0.1:9766/'
-# INTRANET_IMAGE_SERVER = 'http://images.hello.new/'
+# INTRANET_IMAGE_SERVER = 'http://nutimages.hello.new/'
 # MEDIA_ROOT='/media/upload/'
 # AVATAR_HOST = IMAGE_HOST
 
