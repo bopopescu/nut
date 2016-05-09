e2d5d01d1c5e00f5191f3462ff2dbb5dd534fe20
AzureRay <476468781@qq.com>
2016-05-09 19:47:35 +0800

    update but the append child not well

--- nut/static/js/web/jsbuild/article_page_app_build.js
+++ nut/static/js/web/jsbuild/article_page_app_build.js
@@ -2857,9 +2857,10 @@ define('subapp/article/article_remark',[
         init: function(){
             console.log('article remark begin');
             this.accountApp = new AccountApp();
-             this.initVisitorNote();
+             this.initVisitorRemark();
+             this.initUserRemarkPost();
         },
-        initVisitorNote: function(){
+        initVisitorRemark: function(){
             var that = this;
             $('#visitor_note').click(function(){
                     $.when(
@@ -2874,7 +2875,56 @@ define('subapp/article/article_remark',[
                         function fail(){}
                     );
             });
+        },
+        initUserRemarkPost: function(){
+            var $remark = $(".post-note");
+            var $form = $remark.find("form");
+            $form.on('submit', this.submitRemark.bind(this));
+        },
+        submitRemark: function(event){
+            console.log(event.currentTarget);
+            var $form = $(event.currentTarget);
+            //var $form = $('#article_remark_form');
+            var url = $form.attr('action');
+            var $remarkContent = $form.find("textarea");
+             if ($.trim($remarkContent[0].value).length === 0) {
+                    $remarkContent[0].value = '';
+                    $remarkContent.focus();
+             }else{
+                 $.when(
+                     $.ajax({
+                         cache: true,
+                         type: "POST",
+                         url: url ,
+                         data: $form.serialize()
+                     })
+                 ).then(
+                     this.postRemarkSuccess.bind(this),
+                     this.postRemarkFail.bind(this)
+                 );
+                }
+            event.preventDefault();
+            return false;
+        },
+        addNewRemark: function($ele){
+            $ele.appendTo($(".remark-list"));
+        },
+        postRemarkSuccess: function(result){
+            var status = parseInt(result.status);
+            if (status === 1){
+                var $html = $(result.data);
+                this.addNewRemark($html);
+            }else if(status === 0){
+                this.postRemarkFail(result);
+            }else{
+                this.postRemarkFail(result);
+            }
+        },
+        postRemarkFail: function(data){
+            //should add bootbox to notice current remarking user
+            console.log('post remark fail!');
         }
+
     });
     return ArticleRemark;
 });
