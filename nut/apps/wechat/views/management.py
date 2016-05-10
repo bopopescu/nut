c8c0974e97d836443e69beedf7bdcc115076dead
An Chen <anchen@guoku.com>
2016-05-10 17:21:59 +0800

    wechat robot 2

---
+++ nut/apps/wechat/views/management.py
@@ -0,0 +1,12 @@
+from django.views.generic import CreateView,ListView,DeleteView
+
+
+
+class KeywordListView(ListView):
+    pass
+
+class KeywordCreateView(CreateView):
+    pass
+
+class KeywordDeleteView(DeleteView):
+    pass
