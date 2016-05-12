e4970f9b81b6cb66c459d09634c9e49906e7a293
jiaxin <edison7500@gmail.com>
2016-05-12 16:07:36 +0800

    merge

--- nut/apps/management/urls/wechat.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, patterns
-from apps.management.views.wechat import ReplyListView, CreateReplyView, EditReplyView
-
-urlpatterns = patterns(
-    'apps.management.views.wechat',
-    url(r'^reply/$', ReplyListView.as_view(), name='management_wechat_reply'),
-    url(r'^reply/create/', CreateReplyView.as_view(), name='management_wechat_reply_create'),
-    url(r'^reply/(?P<pk>[0-9]+)/$', EditReplyView.as_view(), name='management_wechat_reply_edit'),
-)
