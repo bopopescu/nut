153bb1910214fc7ddad6055e53dcd4569a2ad5ac
jiaxin <edison7500@gmail.com>
2016-05-16 15:55:08 +0800

    update

--- mangosteen/applications/tbrecommend.py
+++ mangosteen/applications/tbrecommend.py
@@ -4,6 +4,8 @@ from flask import Flask
 import top.api
 import top
 import json
+from werkzeug.contrib.cache import FileSystemCache
+# from werkzeug.contrib.cache import RedisCache
 # import time
 
 from model.taobao_token import TaobaoToken
@@ -11,6 +13,8 @@ from model.taobao_token import TaobaoToken
 app = Flask(__name__)
 app.config.from_pyfile('../config/default.py')
 
+# cache = SimpleCache()
+cache = FileSystemCache(cache_dir='/tmp/cache/')
 
 def handel(keyword, **kwargs):
     app_key = app.config.get('APP_KEY')
@@ -36,6 +40,10 @@ def handel(keyword, **kwargs):
             # 'userid': taobao.isv_uid
             })
 
+        item_key = "{0}:{1}".format(keyword.encode('utf-8'), user_id)
+    else:
+        item_key = "{0}".format(keyword.encode('utf-8'))
+
 
     params.update(
         {
@@ -49,15 +57,20 @@ def handel(keyword, **kwargs):
         params.update({
             'keyword':keyword,
         })
-    app.logger.info(params)
+    # app.logger.info(params)
     req.params = json.dumps( params )
 
-    try:
-        # access_time = time.time()
-        resp= req.getResponse(timeout=5)
-        res = resp['alibaba_orp_recommend_response']['recommend']
-        return res
-    except Exception, e:
-        app.logger.error(e)
-        return None
-
+    res = cache.get(item_key)
+    # print res
+    if res is None:
+        try:
+            # access_time = time.time()
+            resp= req.getResponse(timeout=5)
+            res = resp['alibaba_orp_recommend_response']['recommend']
+            cache.set(item_key, res, timeout=86400)
+            # return res
+        except Exception, e:
+            app.logger.error(e)
+            return None
+    # else:
+    return res
