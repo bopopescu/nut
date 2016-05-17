a1e2af973d8927cee9a9b758aa1deb131061fc55
jiaxin <edison7500@gmail.com>
2016-05-17 12:03:10 +0800

    update dict

--- mangosteen/applications/textrank.py
+++ mangosteen/applications/textrank.py
@@ -11,14 +11,15 @@ def get_textrank(article_id):
     article = Article.query.get(article_id)
     # print article
     try:
-        title = jieba.analyse.textrank(article.title, topK=3, withWeight=True)
+        title = jieba.analyse.textrank(article.title, topK=3, withWeight=True,
+                                       allowPOS=('nz', 'ns', 'vn', 'an', 'n'))
     except :
         return None, None
     # print title
 
     key = "article:{0}".format(article_id)
-    content = cache.get(key)
-
+    # content = cache.get(key)
+    content = None
     if content is None:
         content = jieba.analyse.textrank(article.strip_content, topK=10, withWeight=True,
                                      allowPOS=('nz', 'ns', 'vn', 'an', 'n'))
