b6b65b69c4611166e312385988bd81a2a4f1d129
An Chen <anchen@guoku.com>
2016-05-22 13:01:47 +0800

    update

---
+++ nut/static/html/pricetag/handle_csv.py
@@ -0,0 +1,38 @@
+import json
+import io
+
+def handle_file(filename=None):
+    if filename is None:
+        raise  Exception('can not find file None')
+
+    product_list = []
+    with open(filename, 'r') as fd:
+        lines = fd.readlines()
+        for line in lines[2:202]:
+            items = line.split(',')
+            product_list.append([
+                items[1],
+                items[2],
+                items[5],
+                items[7],
+                items[9]
+            ])
+
+
+
+
+    with open('data.js', 'w') as wfd:
+        wfd.write('var tag_list = [')
+        for product in product_list:
+            json.dump(product, wfd ,indent=4, separators=(',', ': '))
+            wfd.write(',')
+        wfd.write('];')
+
+
+    print 'done'
+
+
+
+
+if __name__ == '__main__':
+    handle_file('product_list.csv')
