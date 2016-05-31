5e30204ccebabd5c4fadf75de53a68009c0a3963
An Chen <anchen@guoku.com>
2016-05-31 15:55:48 +0800

    update push management update and delete

--- nut/apps/notifications/urls/management.py
+++ nut/apps/notifications/urls/management.py
@@ -1,12 +1,17 @@
 from django.conf.urls import url, patterns, include
-from apps.notifications.management import DailyPushListView, DailyPushCreateView
+from apps.notifications.management import DailyPushListView, DailyPushCreateView,\
+                                          DailyPushSendTestView, DailyPushSendProductionView,\
+                                          DailyPushUpdateView,DailyPushDeleteView
+
 
 urlpatterns = patterns(
-    'apps.notifications.views',
-    # url(r'^$', 'messages', name='web_messages'),
+    'apps.notifications.management',
     url(r'^$', DailyPushListView.as_view(), name='management_push_list'),
     url(r'^new/$', DailyPushCreateView.as_view(), name='management_push_create'),
-    url(r'^(?P<pk>\d+)/delete/$', DailyPushCreateView.as_view(), name='management_push_create'),
+    url(r'^(?P<pk>\d+)/delete/$', DailyPushDeleteView.as_view(), name='management_push_delete'),
+    url(r'^(?P<pk>\d+)/send_test/$', DailyPushSendTestView.as_view(), name='management_push_send_test'),
+    url(r'^(?P<pk>\d+)/send/$', DailyPushSendProductionView.as_view(), name='management_push_send_production'),
+    url(r'^(?P<pk>\d+)/update/$', DailyPushUpdateView.as_view(), name='management_push_update'),
 )
 
 
