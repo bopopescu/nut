ba41071862d6ce27dbba62c3994ab00306382289
An Chen <anchen@guoku.com>
2016-05-31 12:20:51 +0800

    update

--- nut/apps/notifications/management.py
+++ nut/apps/notifications/management.py
@@ -4,4 +4,18 @@ from braces.views import StaffuserRequiredMixin
 
 
 from apps.notifications.models import DailyPush
-from apps.notifications.forms
+from apps.notifications.forms import BaseDailyPushForm
+
+
+class DailyPushCreateView(StaffuserRequiredMixin, CreateView):
+    form_class = BaseDailyPushForm
+    model = DailyPush
+    template_name = 'management/notifications/daily_push_create.html'
+
+
+class DailyPushUpdateView(StaffuserRequiredMixin, UpdateView):
+    pass
+
+class DailyPushListView(StaffuserRequiredMixin, ListView):
+    template_name = 'management/notifications/daily_push_list.html'
+
--- nut/apps/notifications/urls/management.py
+++ nut/apps/notifications/urls/management.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, patterns, include
-from apps.notifications.management import MessageListView
+from apps.notifications.management import DailyPushListView, DailyPushCreateView
 
 urlpatterns = patterns(
     'apps.notifications.views',
     # url(r'^$', 'messages', name='web_messages'),
-    url(r'^$', MessageListView.as_view(), name='web_messages'),
+    url(r'^$', DailyPushListView.as_view(), name='management_push_list'),
+    url(r'^new/$', DailyPushCreateView.as_view(), name='management_push_create'),
 )
 
 
