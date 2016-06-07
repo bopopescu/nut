10bb92d324c9fcfa86de1141fe3066a4ae232e6f
AzureRay <476468781@qq.com>
2016-06-07 18:08:36 +0800

    fix toggle bug

--- nut/static/js/web/jsbuild/selection_entity_app_build.js
+++ nut/static/js/web/jsbuild/selection_entity_app_build.js
@@ -1432,64 +1432,63 @@ define('subapp/top_notification/top_notification',[
     var TopNotification = Class.extend({
         init: function(){
             console.log('top notification begin');
-            this.flag = false;
-            console.log('flag:'+this.flag);
             this.initClickBell();
         },
         initClickBell: function(){
-            if(!this.flag){
-                 $('.navbar-collapse .notification-icon').click(this.showNotificationDropList.bind(this));
-            }else{
-                 $('.navbar-collapse .notification-icon').click(this.hiddenNotificationDropList.bind(this));
-            }
+            var flag = 0;
+            var that = this;
+                 $('.navbar-collapse .notification-icon').click(function(){
+                    $('.navbar-collapse .notification-drop-list-wrapper').toggle(flag++ % 2 == 0);
+                     console.log('flag:'+flag);
+
+                 });
+
 
         },
-        showNotificationDropList:function(){
-             $('.navbar-collapse .notification-drop-list-wrapper').show();
-             this.postAjaxNotification();
-             this.flag = true;
-             console.log('flag:'+this.flag);
-
-        },
-        hiddenNotificationDropList:function(){
-             $('.navbar-collapse .notification-drop-list-wrapper').hide();
-             this.flag = false;
-             console.log('flag:'+this.flag);
-        },
-        postAjaxNotification:function(){
-            console.log('post ajax request');
-             $.when(
-                    $.ajax({
-                        cache:true,
-                        type:"post",
-                        url: '',
-                        data:''
-                    })
-                ).then(
-                  this.postSuccess.bind(this),
-                 this.postFail.bind(this)
-                );
-        },
-        postSuccess:function(result){
-            var status = parseInt(result.status);
-            if(status == 1){
-                this.showNotificationItems();
-            }else{
-                this.showFail();
-            }
-        },
-        showNotificationItems:function($ele){
-            var ajaxDatas = $ele;
-            var notificationItems = _.template($('#notification_item_template').html());
-            var datas = {
-                name:ajaxDatas['name'],
-                name:ajaxDatas['name']
-            };
-            $('.notification-drop-list').append(notificationItems(datas));
-        },
-        postFail:function(){
-            console.log('request failed.please try again');
-        }
+        //showNotificationDropList:function(){
+        //    var that = this;
+        //     $('.navbar-collapse .notification-drop-list-wrapper').toggle(that.flag++ % 2 == 0);
+        //
+        //}
+        //hiddenNotificationDropList:function(){
+        //     $('.navbar-collapse .notification-drop-list-wrapper').hide();
+        //     this.flag = false;
+        //     console.log('flag:'+this.flag);
+        //},
+        //postAjaxNotification:function(){
+        //    console.log('post ajax request');
+        //     $.when(
+        //            $.ajax({
+        //                cache:true,
+        //                type:"post",
+        //                url: '',
+        //                data:''
+        //            })
+        //        ).then(
+        //          this.postSuccess.bind(this),
+        //         this.postFail.bind(this)
+        //        );
+        //},
+        //postSuccess:function(result){
+        //    var status = parseInt(result.status);
+        //    if(status == 1){
+        //        this.showNotificationItems();
+        //    }else{
+        //        this.showFail();
+        //    }
+        //},
+        //showNotificationItems:function($ele){
+        //    var ajaxDatas = $ele;
+        //    var notificationItems = _.template($('#notification_item_template').html());
+        //    var datas = {
+        //        name:ajaxDatas['name'],
+        //        name:ajaxDatas['name']
+        //    };
+        //    $('.notification-drop-list').append(notificationItems(datas));
+        //},
+        //postFail:function(){
+        //    console.log('request failed.please try again');
+        //}
 
     });
 
