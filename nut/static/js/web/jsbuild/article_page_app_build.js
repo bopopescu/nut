36d5186ed88ccfe40f229b579244771d164a3714
AzureRay <476468781@qq.com>
2016-06-13 17:17:59 +0800

    clear gotop influence on other pages

--- nut/static/js/web/jsbuild/article_page_app_build.js
+++ nut/static/js/web/jsbuild/article_page_app_build.js
@@ -1489,8 +1489,9 @@ define('subapp/gotop',['jquery','libs/underscore','libs/Class','libs/fastdom'],
     var GoTop = Class.extend({
         init: function(){
             this.topLinkWrapper = $('.gotop-wrapper');
-            this.topLink = $('.new-btn-top');
-            if (this.topLink.length > 0){
+            this.topLink = $('.btn-top');
+            this.newTopLink = $('.new-btn-top');
+            if (this.topLink.length > 0 || this.newTopLink.length > 0){
                 this.setupWatcher();
                 this.topLink.on('click', function(){
                     $("html, body").animate(
@@ -1498,6 +1499,14 @@ define('subapp/gotop',['jquery','libs/underscore','libs/Class','libs/fastdom'],
                     );
                     return false;
                 });
+
+                 this.newTopLink.on('click', function(){
+                    $("html, body").animate(
+                    {scrollTop : 0}, 800
+                    );
+                    return false;
+                });
+
             }else{
                 return ;
             }
@@ -1517,8 +1526,8 @@ define('subapp/gotop',['jquery','libs/underscore','libs/Class','libs/fastdom'],
         },
         doRead: function(){
             this.scrollTop = $(window).scrollTop();
-            this.btnRect = this.topLink[0].getBoundingClientRect()
-            this.footerRect = $('#guoku_footer')[0].getBoundingClientRect()
+            this.btnRect = this.topLink[0].getBoundingClientRect();
+            this.footerRect = $('#guoku_footer')[0].getBoundingClientRect();
 
 
         },
@@ -1529,7 +1538,7 @@ define('subapp/gotop',['jquery','libs/underscore','libs/Class','libs/fastdom'],
 
                 fastdom.write(function(){
                         that.topLinkWrapper.show();
-                         that.topLink.show();
+                        that.topLink.show();
                         if (that.btnRect.bottom >= that.footerRect.top){
                             that.topLinkWrapper.css({bottom:'370px'});
                             that.topLink.css({bottom:'370px'});
