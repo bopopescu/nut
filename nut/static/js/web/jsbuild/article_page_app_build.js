f57bec1585378b265b81a926a34c00396bf69624
AzureRay <476468781@qq.com>
2016-06-14 14:44:53 +0800

    bottom share animation

--- nut/static/js/web/jsbuild/article_page_app_build.js
+++ nut/static/js/web/jsbuild/article_page_app_build.js
@@ -1042,6 +1042,8 @@ define('subapp/topmenu',['bootstrap',
             this.$menu.addClass('shown-header');
             $('.round-link').show();
             $('.bottom-ad').addClass('showing');
+            $('.bottom-article-share-wrapper').removeClass('hidden-animation');
+
             //console.log((new Date()).getMilliseconds());
 
         },
@@ -1051,6 +1053,8 @@ define('subapp/topmenu',['bootstrap',
             this.$menu.addClass('hidden-header');
             $('.round-link').hide();
             $('.bottom-ad').removeClass('showing');
+            $('.bottom-article-share-wrapper').addClass('hidden-animation');
+
             //console.log((new Date()).getMilliseconds());
         }
     });
