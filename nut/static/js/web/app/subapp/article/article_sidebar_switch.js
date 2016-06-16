8fdca46b47affe9893e62ea6cf9f6c784d65823e
AzureRay <476468781@qq.com>
2016-06-16 10:25:47 +0800

    update and clean

--- nut/static/js/web/app/subapp/article/article_sidebar_switch.js
+++ nut/static/js/web/app/subapp/article/article_sidebar_switch.js
@@ -9,7 +9,6 @@ define([
         init: function(){
             console.log('article sidebar switch begin');
             this.initClickSwitch();
-            console.log('sidebar width:'+this.getSidebarWidth());
         },
         initClickSwitch:function(){
             $('.sidebar-switch-wrapper').click(this.handleClickSwitch.bind(this));
@@ -22,19 +21,23 @@ define([
             }
         },
         hiddenSideBar:function(){
-            //$('#detail_content_right').hide();
-              $('#detail_content_right').removeClass('content-right-show');
+            $('#detail_content_right').removeClass('content-right-show');
             $('#detail_content_right').addClass('content-right-hidden');
-             $('.main-article-control').css('transform','translateX('+this.getArticleMoveDistance()+'px)');
-             $('.bottom-article-share').parent('.col-xs-12').css('transform','translateX('+this.getArticleMoveDistance()+'px)');
+            $('.main-article-control').css({
+                                             'transform':'translateX('+this.getArticleMoveDistance()+'px)',
+                                             '-webkit-transform':'translateX('+this.getArticleMoveDistance()+'px)'
+            });
+            $('.bottom-article-share').parent('.col-xs-12').css('transform','translateX('+this.getArticleMoveDistance()+'px)');
             $('.sidebar-switch.close-switch').hide();
             $('.sidebar-switch.open-switch').show();
         },
         showSidebar:function(){
-            //$('#detail_content_right').show();
-             $('#detail_content_right').removeClass('content-right-hidden');
-             $('#detail_content_right').addClass('content-right-show');
-            $('.main-article-control').css('transform','translateX(0)');
+            $('#detail_content_right').removeClass('content-right-hidden');
+            $('#detail_content_right').addClass('content-right-show');
+            $('.main-article-control').css({
+                                            'transform':'translateX(0)',
+                                            '-webkit-transform':'translateX(0)'
+            });
             $('.bottom-article-share').parent('.col-xs-12').css('transform','translateX(0)');
             $('.sidebar-switch.close-switch').show();
             $('.sidebar-switch.open-switch').hide();
