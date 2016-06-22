75184835e844dc9b85869541689459b43f21db0d
AzureRay <476468781@qq.com>
2016-06-22 22:23:07 +0800

    fix recommend user list page

---
+++ nut/static/js/web/app/recommend_user_list_app.js
@@ -0,0 +1,12 @@
+require(
+    [
+        'jquery',
+        'subapp/topmenu'
+    ],
+    function (
+        jQuery,
+        Menu
+    ){
+        var menu = new Menu();
+        console.log("recommend user list app init！");
+});
