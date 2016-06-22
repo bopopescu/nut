78078994f57b62426fe70e0854dc50a27ddeaa64
AzureRay <476468781@qq.com>
2016-06-22 14:47:03 +0800

    fix anchor scroll

--- nut/static/js/web/app/subapp/yearseller/linkscroll.js
+++ nut/static/js/web/app/subapp/yearseller/linkscroll.js
@@ -5,7 +5,6 @@ define(['jquery','libs/Class','jqueryeasing'], function($,Class){
             console.log('link scroll');
             $(selector).click(function(event){
                 $('html,body').animate({scrollTop: $(this.hash).offset().top - 50}, 1000, 'easeInOutExpo');
-
                 event.preventDefault();
                 event.stopPropagation();
                 return false;
