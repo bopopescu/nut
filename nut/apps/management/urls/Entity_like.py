b2aec63f92e37d37fcbe0b9aaedc8ce881a2cbd7
An Chen <y__n@liaomatoMacBook-Pro.local>
2016-06-29 15:57:29 +0800

    test

---
+++ nut/apps/management/urls/Entity_like.py
@@ -0,0 +1,10 @@
+from apps.management.views.entity_like import EntityLikeView
+import django.conf.urls
+
+
+urlpatterns = django.conf.urls.patterns(
+    'apps.management.views.entity_like',
+
+    django.conf.urls.url(r'^entitylike/$', EntityLikeView.index),
+
+)
--- nut/apps/management/views/entity_like.py
+++ nut/apps/management/views/entity_like.py
@@ -1,22 +1,24 @@
 #- * - coding: utf - 8 -*-
-
+from django.shortcuts import render_to_response
 from apps.core.models import Selection_Entity, Entity, Entity_Like
+from haystack.query import SearchQuerySet
 from apps.core.extend.paginator import ExtentPaginator
 from django.utils.log import getLogger
 from django.views.generic import ListView
 from datetime import datetime, timedelta
 
-from haystack.query import SearchQuerySet
-
 class EntityLikeView():
-    template_name = 'management/selection_report/list.html'
+    #template_name = 'management/selection_report/entity_like.html'
     model = Entity_Like
     # paginate_by = 40
     context_object_name = 'Entity_Like'
     # paginator_class = ExtentPaginator
 
     def get_query(self):
-        self.id = self.request.GET.get('id', None)
-        entity_id = self.request.GET.get('entity_id', None)
+        self.id = self.request.GET.get('id')
+        entity_id = self.request.GET.get("entity_id")
         use_id = self.request.GET.get("user_id")
         return use_id
+    def index(request):
+        user_id = get_query()
+        return render_to_response('entity_like.html',{'user_id':user_id})
