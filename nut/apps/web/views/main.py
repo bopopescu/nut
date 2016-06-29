bcde1f10c7599cd641b10363d59b113ab9f39357
goalong <goforalong@gmail.com>
2016-06-29 15:15:38 +0800

    add more context data

--- nut/apps/web/views/main.py
+++ nut/apps/web/views/main.py
@@ -1,4 +1,8 @@
+#encoding: utf-8
 from datetime import datetime
+
+from apps.shop.models import StorePageBanners
+from apps.site_banner.models import SiteBanner
 from braces.views import AjaxResponseMixin
 from braces.views import JSONResponseMixin
 from django.views.generic import ListView
@@ -11,7 +15,7 @@ from haystack.generic_views import SearchView
 from apps.core.tasks.recorder import record_search
 from apps.core.utils.commons import get_client_ip, get_user_agent
 from apps.tag.models import Tags
-from apps.core.models import Entity, Entity_Like
+from apps.core.models import Entity, Entity_Like, Category
 from apps.core.models import Selection_Entity
 from apps.core.models import GKUser
 from apps.core.models import Show_Banner
@@ -110,7 +114,15 @@ class SelectionEntityList(JSONResponseMixin, AjaxResponseMixin, ListView):
                                                             flat=True))
                                                     ).using('slave')
         context['user_entity_likes'] = el
-        context['selections'] = selections
+        context['selections'] = selections[:6]                           #精选商品
+        context['banners'] = SiteBanner.objects.get_mainpage_banner()    #顶部banner (link, image)
+        context['categories'] = Category.objects.filter(status=True)     #品类
+        popular_list = Entity_Like.objects.popular_random()
+        context['entities'] = Entity.objects.filter(id__in=popular_list) #热门商品
+        context['article_tags'] = Tags.objects.top_article_tags()        #图文标签
+        context['articles'] = Selection_Article.objects.all()[:3]        #最新精选图文
+        context['recommand_users'] = GKUser.objects.recommended_user()[:20]    #推荐用户
+        context['middle_banners'] = StorePageBanners.objects.filter(status=StorePageBanners.enabled)    #中间banner
         return context
 
     def get_like_list(self, entities):
