c5e1e4866ffc52d58f4e696159adf0e0609818e8
An Chen <y__n@liaomatoMacBook-Pro.local>
2016-07-01 11:49:16 +0800

    rest

--- nut/apps/management/views/selection_report.py
+++ nut/apps/management/views/selection_report.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-from apps.core.models import Selection_Entity, Entity
+from apps.core.models import Selection_Entity, Entity,Entity_Like
 from apps.core.extend.paginator import ExtentPaginator
 from django.utils.log import getLogger
 from django.views.generic import ListView
@@ -103,7 +103,21 @@ class SelectionReportListView(ListView):
         context = self.get_context_data()
         return self.render_to_response(context)
 
+class EntityLikeView():
+    template_name = 'management/selection_report/entity_like.html'
+    model = Entity_Like
+    # paginate_by = 40
+    context_object_name = 'Entity_Like'
+    # paginator_class = ExtentPaginator
 
+    def index(requst):
+        post = Entity_Like.objects.all()
+        #self.id = self.request.GET.get('id')
+        content = {'post':post}
+        #entity_id = self.request.GET.get("entity_id")
+        #use_id = self.request.GET.get("user_id")
+
+        return render_to_response('entity_like.html', content)
 
 
 
