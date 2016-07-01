ceb6597ded880ece4539bb461dad754de70d36f9
An Chen <y__n@liaomatoMacBook-Pro.local>
2016-07-01 12:20:25 +0800

    rest

--- nut/apps/management/views/selection_report.py
+++ nut/apps/management/views/selection_report.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-
-
 from apps.core.models import Selection_Entity, Entity,Entity_Like
 from apps.core.extend.paginator import ExtentPaginator
 from django.utils.log import getLogger
 from django.views.generic import ListView
 from datetime import datetime, timedelta
-
+from django.shortcuts import render_to_response
 from haystack.query import SearchQuerySet
 
 log = getLogger('django')
@@ -103,18 +101,19 @@ class SelectionReportListView(ListView):
         context = self.get_context_data()
         return self.render_to_response(context)
 
-def EntityLikeView(request):
+class EntityLikeView():
     template_name = 'management/selection_report/entity_like.html'
     model = Entity_Like
     # paginate_by = 40
     context_object_name = 'Entity_Like'
-    # paginator_class = ExtentPaginator
-    post = Entity_Like.objects.all()
-    #self.id = self.request.GET.get('id')
-    content = {'post':post}
-    #entity_id = self.request.GET.get("entity_id")
-    #use_id = self.request.GET.get("user_id")
-    return render_to_response('entity_like.html', content)
+    def get(requst):
+        # paginator_class = ExtentPaginator
+        post = Entity_Like.objects.all()
+        #self.id = self.request.GET.get('id')
+        content = {'post':post}
+        #entity_id = self.request.GET.get("entity_id")
+        #use_id = self.request.GET.get("user_id")
+        return render_to_response('entity_like.html', content)
 
 
 
