743a1b52f2552c80deadc68a8eb06610ac997afd
An Chen <y__n@liaomatoMacBook-Pro.local>
2016-07-01 12:03:01 +0800

    rest

--- nut/apps/management/urls/selection_report.py
+++ nut/apps/management/urls/selection_report.py
@@ -6,6 +6,6 @@ urlpatterns = patterns(
     'apps.management.views.selection_report',
 
     url(r'$', SelectionReportListView.as_view(), name='management_selection_report'),
-    url(r'entitylike', 'nut.apps.management.views.selection_report.EntityLikeView')
+    #url(r'entitylike', 'nut.apps.management.views.selection_report.EntityLikeView')
 )
 
