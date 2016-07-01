b5ae1a09d1df6ac05f2d56f799f7b963b410b0d5
An Chen <y__n@liaomatoMacBook-Pro.local>
2016-07-01 11:51:34 +0800

    rest

--- nut/apps/management/urls/Entity_like.py
+++ nut/apps/management/urls/Entity_like.py
@@ -1,10 +1,1 @@
-from apps.management.views.entity_like import EntityLikeView
-import django.conf.urls
 
-
-urlpatterns = django.conf.urls.patterns(
-    'apps.management.views.entity_like',
-
-    #django.conf.urls.url(r'^entitylike/$', EntityLikeView.get_query),
-
-)
