df9b39ef67a33c0311e2b62bc1bd9ec4cbc88a67
AzureRay <476468781@qq.com>
2016-07-06 14:43:26 +0800

    update

--- nut/static/js/web/app/subapp/index/category_tab_view.js
+++ nut/static/js/web/app/subapp/index/category_tab_view.js
@@ -4,6 +4,7 @@ define(['jquery', 'libs/Class'], function(
 ){
     var CategoryTabView= Class.extend({
         init: function () {
+            this.$article_container = $('#selection_article_list');
             this.initHoverCategory();
             console.log('category tab view begin');
         },
@@ -13,7 +14,35 @@ define(['jquery', 'libs/Class'], function(
         },
         handleHoverCategory:function(event){
             var dataValue = $(event.target).attr('data-value');
-            console.log(dataValue);
+            console.log('data value:'+dataValue+' send ajax request');
+              $.when(
+                    $.ajax({
+                        cache:true,
+                        type:"get",
+                        url: '',
+                        data: data,
+                        dataType:'json'
+                    })
+                ).then(
+                  this.postSuccess.bind(this),
+                 this.postFail.bind(this)
+                );
+        },
+        postSuccess:function(result){
+            var status = parseInt(result.status);
+            if(status == 1){
+                 this.showContent($(result.data));
+            }else{
+                this.showFail(result);
+            }
+        },
+        showFail:function(result){
+            console.log('post failed');
+        },
+        showContent: function(elemList){
+            var that = this;
+            that.$article_container.empty();
+            that.$article_container.append(elemList);
         }
     });
     return CategoryTabView;
--- nut/static/js/web/jsbuild/index_page_app_build.js
+++ nut/static/js/web/jsbuild/index_page_app_build.js
@@ -4536,6 +4536,7 @@ define('subapp/index/category_tab_view',['jquery', 'libs/Class'], function(
 ){
     var CategoryTabView= Class.extend({
         init: function () {
+            this.$article_container = $('#selection_article_list');
             this.initHoverCategory();
             console.log('category tab view begin');
         },
@@ -4545,7 +4546,35 @@ define('subapp/index/category_tab_view',['jquery', 'libs/Class'], function(
         },
         handleHoverCategory:function(event){
             var dataValue = $(event.target).attr('data-value');
-            console.log(dataValue);
+            console.log('data value:'+dataValue+' send ajax request');
+              $.when(
+                    $.ajax({
+                        cache:true,
+                        type:"get",
+                        url: '',
+                        data: data,
+                        dataType:'json'
+                    })
+                ).then(
+                  this.postSuccess.bind(this),
+                 this.postFail.bind(this)
+                );
+        },
+        postSuccess:function(result){
+            var status = parseInt(result.status);
+            if(status == 1){
+                 this.showContent($(result.data));
+            }else{
+                this.showFail(result);
+            }
+        },
+        showFail:function(result){
+            console.log('post failed');
+        },
+        showContent: function(elemList){
+            var that = this;
+            that.$article_container.empty();
+            that.$article_container.append(elemList);
         }
     });
     return CategoryTabView;
