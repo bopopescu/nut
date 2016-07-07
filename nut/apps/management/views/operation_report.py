9125292b7e9ccb69d67c374ccf61474bb2a2633c
goalong <goforalong@gmail.com>
2016-07-07 16:13:10 +0800

    update queryset

--- nut/apps/management/views/operation_report.py
+++ nut/apps/management/views/operation_report.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-from apps.core.models import  GKUser, Entity, Note, Article, Article_Remark
+from apps.core.models import  GKUser, Entity, Note, Article, Article_Remark, Entity_Like
 from django.utils.log import getLogger
 from django.views.generic import View
 from django.db.models import  Count
@@ -11,7 +11,7 @@ from django.views.generic.base import TemplateResponseMixin
 
 log = getLogger('django')
 
-TIME_FORMAT = '%Y-%m-%d 8:00:00'
+TIME_FORMAT = '%Y-%m-%d 00:00:00'
 
 
 class OperationReportListView(UserPassesTestMixin, TemplateResponseMixin, View):
@@ -36,21 +36,27 @@ class OperationReportListView(UserPassesTestMixin, TemplateResponseMixin, View):
             self.start_time = 'lastweek'
             self.start_date, self.end_date = ((datetime.now() - timedelta(days=7)).strftime(TIME_FORMAT)), (
                 datetime.now() + timedelta(days=1)).strftime(TIME_FORMAT)
-        context['auth_seller_count'] = GKUser.objects.authorized_seller().using('slave').filter(
+        context['auth_seller_count'] = GKUser.objects.authorized_seller().using('slave').count()
+        context['auth_author_count'] = GKUser.objects.authorized_author().using('slave').count()
+        context['active_user_count'] = GKUser.objects.active_user().using('slave').count()
+        context['new_auth_seller_count'] = GKUser.objects.authorized_seller().using('slave').filter(
             date_joined__range=(self.start_date, self.end_date)).count()
-        context['auth_author_count'] = GKUser.objects.authorized_author().using('slave').filter(
+        context['new_auth_author_count'] = GKUser.objects.authorized_author().using('slave').filter(
+            date_joined__range=(self.start_date, self.end_date)).count()
+        context['new_active_user_count'] = GKUser.objects.active_user().using('slave').filter(
             date_joined__range=(self.start_date, self.end_date)).count()
         context['entity_count'] = Entity.objects.using('slave').filter(
-            created_time__range=(self.start_date, self.end_date)).count()
+            created_time__range=(self.start_date, self.end_date), status__gt=-1).count()
         context['note_count'] = Note.objects.using('slave').filter(
             post_time__range=(self.start_date, self.end_date)).count()
-        entitys = Entity.objects.using('slave').filter(created_time__range=(self.start_date, self.end_date))
-        context['entity_count_100'] = entitys.annotate(likes_count=Count('likes')).filter(likes_count__gt=100).count()
-        context['entity_count_50'] = entitys.annotate(likes_count=Count('likes')).filter(likes_count__gt=50).count()
-        articles = Article.objects.using('slave').filter(created_datetime__range=(self.start_date, self.end_date))
+        entitys = Entity.objects.using('slave').filter(likes__created_time__range=(self.start_date, self.end_date))
+        context['entity_count_100'] = entitys.annotate(likes_count=Count('likes')).filter(likes_count__gte=100).count()
+        context['entity_count_50'] = entitys.annotate(likes_count=Count('likes')).filter(likes_count__gte=50).count()
+        articles = Article.objects.using('slave').filter(updated_datetime__range=(self.start_date, self.end_date), publish=2)
         context['article_count'] = articles.count()
         context['article_read'] = articles.filter(read_count__gt=1000).count()
-        context['article_dig'] = articles.annotate(digs_count=Count('digs')).filter(digs_count__gt=10).count()
+        context['article_dig'] = Article.objects.using('slave').filter(digs__created_time__range=(self.start_date, self.end_date)).\
+            annotate(digs_count=Count('digs')).filter(digs_count__gte=10).count()
         context['article_remark_count'] = Article_Remark.objects.using('slave').filter(
             create_time__range=(self.start_date, self.end_date)).count()
         context['status'] =  self.status
