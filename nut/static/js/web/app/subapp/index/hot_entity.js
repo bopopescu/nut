cdf79d5049036acb1bc7cf670fca23c2a18c4388
AzureRay <476468781@qq.com>
2016-07-26 15:32:06 +0800

    calculate well

--- nut/static/js/web/app/subapp/index/hot_entity.js
+++ nut/static/js/web/app/subapp/index/hot_entity.js
@@ -2,18 +2,53 @@
 define(['jquery', 'libs/Class','fastdom'], function(
     $, Class,fastdom
 ){
-            var HotEntity= Class.extend({
-                init: function () {
-                    console.log('begin hot entity ajax js');
-                    this.setupScrollEntity();
-                },
-                setupScrollEntity:function(){
-                     $(window).scroll(this.scrollEntityHandler.bind(this));
-                },
-                scrollEntityHandler:function(){
+    var HotEntity= Class.extend({
+        init: function () {
+            this.$loading_icon = $('.loading-icon');
+            this.$hot_entity_wrapper = $('#hot-entity-list');
+            if(this.$hot_entity_wrapper.length > 0){
+                this.setupWatcher();
+            }else{
+                return;
+            }
+        },
+        setupWatcher:function(){
+            $(window).scroll(this.onScroll.bind(this));
+        },
+        onScroll:function(){
+            if(this.read){
+                fastdom.clear(this.read);
+            }
+            this.read = fastdom.read(this.doRead.bind(this));
+            if(this.write){
+                fastdom.clear(this.write);
+            }
+            this.write = fastdom.write(this.doWrite.bind(this));
+        },
+        doRead: function(){
+            this.scrollTop = $(window).scrollTop();
+            this.screenHeight = window.screen.height;
+            this.pageHeight = document.body.scrollHeight;
+            this.middleBannerBottom = document.getElementById('hot-entity-list').getBoundingClientRect().bottom;
+            this.leftCondition = this.screenHeight + this.scrollTop;
+            this.rightCondition = this.pageHeight - this.screenHeight + this.middleBannerBottom;
 
-                }
-            });
+        },
+        doWrite: function(){
+            var that = this ;
+            if (!this.scrollTop){return ;}
+            if (this.leftCondition > this.rightCondition){
+                fastdom.write(function(){
+                    that.$loading_icon.hide();
+                });
+
+            }else{
+                fastdom.write(function(){
+                    that.$loading_icon.show();
+                });
+            }
+        }
+    });
     return HotEntity;
 });
 
