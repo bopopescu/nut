0df2e97e2a36fb9752f41ae9bb71d2344a203107
AzureRay <476468781@qq.com>
2016-08-08 10:32:22 +0800

    begin refactor js

---
+++ nut/static/js/management/seller_management/sku_add.js
@@ -0,0 +1,13 @@
+var SKUAddManager = Class.extend({
+    init: function(){
+        console.log('sku add begin');
+    }
+
+});
+
+
+(function($, window, document){
+    $(function(){
+         var sku_manager = new SKUAddManager();
+    });
+})(jQuery, window, document);
---
+++ nut/static/js/management/seller_management/sku_switch.js
@@ -0,0 +1,15 @@
+var SkuSwitch = Class.extend({
+    init: function(){
+       console.log('sku switch begin');
+    }
+
+});
+
+
+
+(function($, window, document){
+    $(function(){
+         var SiteBanners = new SkuSwitch();
+    });
+})(jQuery, window, document);
+
