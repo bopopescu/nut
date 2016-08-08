a8d085bc419e72027b21c35e1d7624574ff46eb7
mabing <mabing@guoku.com>
2016-08-08 17:06:43 +0800

    update

--- nut/apps/web/views/seller_management.py
+++ nut/apps/web/views/seller_management.py
@@ -331,21 +331,6 @@ class SKUCreateBoxView(EntityUserPassesTestMixin, AjaxResponseMixin, CreateView)
     template_name = 'management/sku/sku_add_template.html'
     def get_success_url(self):
         return reverse('sku_list_management', args=[self.entity_id])
-    def post_ajax(self, request, *args, **kwargs):
-        _forms = SKUForm(request.POST)
-        if _forms.is_valid():
-            _forms.save()
-            return JSONResponse(
-                data={
-                    'status': 1
-                }
-            )
-        else:
-            return JSONResponse(
-                data={
-                    'status': 0
-                }
-            )
     def get_context_data(self, **kwargs):
         context = super(SKUCreateBoxView,self).get_context_data(**kwargs)
         context['entity_id']=self.entity_id
