cba2dbb2eb683ada8d52e2a2238b000acb853740
AzureRay <476468781@qq.com>
2016-08-12 11:14:15 +0800

    update sku edit js

--- nut/static/js/management/seller_management/sku_edit.js
+++ nut/static/js/management/seller_management/sku_edit.js
@@ -3,10 +3,10 @@ var SKUEditManager = Class.extend({
         console.log('sku edit begin');
         this.initSkuEdit();
     },
-    initSkuEdit:function(){
-        var that = this;
-        $('.edit-sku-trigger').click(function(){
-            var url = $(this).attr('data-url');
+    show_sku_edit_dialog:function(e){
+            var that = this;
+            var target = e.currentTarget;
+            var url = $(target).attr('data-url');
             $.when($.ajax({
                 url: url,
                 method: 'GET'
@@ -16,16 +16,27 @@ var SKUEditManager = Class.extend({
                         message: html
                     });
                     //成功渲染弹框之后再初始化保存按钮
-                    that.initSaveSku();
+                     that.initSaveSku();
+                    that.sku_attr_app = new SKU_MNG_APP();
+                    //that.sku_attr_app.render_attributes();
+                    if($('#sku_form').find('input')){
+                        return ;
+                    }else{
+                      that.sku_attr_app.render_attributes();
+                    }
+
                 },
                 function(){
                     console.log('get form fail ');
                 });
-        });
+        },
+    initSkuEdit:function(){
+        $('.edit-sku-trigger').click(this.show_sku_edit_dialog.bind(this));
     },
     post_edit_sku: function(){
         var that = this;
         var _form = $('#sku_form');
+        _form.find('#id_attrs').val(this.sku_attr_app.collect_attribute());
         _form.onsubmit=function(){return false;};
         //alert(_form.serialize());
 
@@ -64,7 +75,7 @@ var SKUEditManager = Class.extend({
         else{
             return bootbox.alert({
                 size: 'small',
-                message: '编辑失败'
+                message: '编辑失败!'
             }) ;
         }
     },
