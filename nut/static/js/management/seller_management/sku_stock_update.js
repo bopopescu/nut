6088a3f4ea9f47616ca631dc48897ca2c30153f2
AzureRay <476468781@qq.com>
2016-08-17 14:06:36 +0800

    show edit content

--- nut/static/js/management/seller_management/sku_stock_update.js
+++ nut/static/js/management/seller_management/sku_stock_update.js
@@ -1,6 +1,14 @@
 var SKUStockUpdateManager = Class.extend({
     init: function(){
         console.log('init sku stock update begin');
+        this.initEditPencil();
+    },
+    initEditPencil:function(){
+        $('.stock-edit-pencil').click(this.showInputContent.bind(this));
+    },
+    showInputContent:function(event){
+        $(event.currentTarget).parent().addClass('hidden');
+        $(event.currentTarget).parent().next().addClass('show');
     }
 
 });
