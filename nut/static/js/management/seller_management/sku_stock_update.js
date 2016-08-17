d9fddc5f0214181df21fac1078c6e6502fc63a69
AzureRay <476468781@qq.com>
2016-08-17 14:57:09 +0800

    sku update price well

--- nut/static/js/management/seller_management/sku_stock_update.js
+++ nut/static/js/management/seller_management/sku_stock_update.js
@@ -2,6 +2,7 @@ var SKUStockUpdateManager = Class.extend({
     init: function(){
         console.log('init sku stock update begin');
         this.initEditPencil();
+        this.initSaveBtn();
     },
     initEditPencil:function(){
         $('.stock-edit-pencil').click(this.showInputContent.bind(this));
@@ -9,8 +10,43 @@ var SKUStockUpdateManager = Class.extend({
     showInputContent:function(event){
         $(event.currentTarget).parent().addClass('hidden');
         $(event.currentTarget).parent().next().addClass('show');
-    }
+    },
+    initSaveBtn:function(){
+        $('.sku-save-btn').click(this.postUpdateSku.bind(this));
+    },
+    postUpdateSku:function(event){
+        var that = this;
+        var targetElement = $(event.currentTarget);
+        var targetInput = targetElement.prev();
+        var url =  targetElement.attr('data-url');
+        //var entity_id = targetInput.attr('data-entity-id');
+        var sku_id = targetInput.attr('data-sku-id');
+        var stock =  targetInput.val();
 
+        var data = {
+            'sku_id':sku_id,
+            'stock':stock
+        };
+
+        $.when($.ajax({
+
+            url:url,
+            method:'POST',
+            data:data
+
+            })).then(
+            that.postSaveSuccess.bind(this),
+            that.postSaveFail.bind(this)
+        );
+    },
+    postSaveSuccess:function(data){
+        if(data.status > 0){
+             bootbox.alert('更新成功');
+        }
+    },
+    postSaveFail:function(data){
+         bootbox.alert('更新失败');
+    }
 });
 
 
