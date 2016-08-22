d7262be0420f9d25223790a22435668cfa030e65
AzureRay <476468781@qq.com>
2016-08-22 14:36:06 +0800

    clean

--- nut/static/js/web/app/subapp/checkout/check_order_status.js
+++ nut/static/js/web/app/subapp/checkout/check_order_status.js
@@ -1,40 +1,6 @@
 var CheckoutManager = Class.extend({
     init: function(){
-        console.log('checkout begin');
         this.initCheckOrderStatusBtn();
-
-        //$('.btn.check-order-status').on('click', check_order_status);
-        //function check_order_status(e) {
-        //        e.preventDefault();
-        //        $(this).off('click');
-        //        var target_url = $(this).attr("data-url");
-        //        var order_id = $(this).attr("data-order-id");
-        //        bootbox.confirm('确认已经成功收到买家付款？', function(result){
-        //            if(result){
-        //                $.ajax({
-        //                url: target_url,
-        //                method: 'POST',
-        //                data:{
-        //                    order_id:order_id
-        //
-        //                }
-        //                }).done(function() {
-        //                   bootbox.alert({
-        //                       size: 'small',
-        //                       message: '操作成功'
-        //                   }) ;
-        //
-        //                    window.setTimeout( function(){ bootbox.hideAll();}, 1000);
-        //                    window.location.reload();
-        //                });
-        //
-        //            }else{
-        //               return ;
-        //            }
-        //    });
-        //
-        //}
-
     },
     initCheckOrderStatusBtn:function(){
         $('.btn.check-order-status').click(this.showDialog.bind(this));
@@ -96,7 +62,6 @@ var CheckoutManager = Class.extend({
     }
 
 });
-
 
 (function($, window, document){
     $(function(){
