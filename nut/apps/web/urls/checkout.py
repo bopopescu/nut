53bf6b9a9fb170c6306b1f6d6ddda1c53eaa09be
mabing <mabing@guoku.com>
2016-08-23 18:05:55 +0800

    update

--- nut/apps/web/urls/checkout.py
+++ nut/apps/web/urls/checkout.py
@@ -1,12 +1,11 @@
 
 from django.conf.urls import url, patterns
-from apps.web.views.checkout import SellerOrderListView,SellerOrderDetailView,SellerOrderDeleteView,IndexView,CheckoutView,\
+from apps.web.views.checkout import SellerOrderListView,SellerOrderDeleteView,IndexView,CheckoutView,\
                                     AllOrderListView
 
 urlpatterns = patterns(
     'apps.web.views.checkout',
     url(r'^$',AllOrderListView.as_view(),name='checkout_index'),
-    url(r'^order/(?P<order_number>\d+)/detail/$', SellerOrderDetailView.as_view(), name='checkout_order_detail_management'),
     url(r'^order/(?P<order_number>\d+)/delete/$',SellerOrderDeleteView.as_view(),name='checkout_order_delete_management'),
     url(r'^order_list/$', SellerOrderListView.as_view(), name='checkout_order_list'),
     url(r'^order_list/checkout/$',CheckoutView.as_view(),name="checkout_done")
