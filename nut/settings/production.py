aff39d037d994722f8079d7ee46d32d5738909df
jiaxin <edison7500@gmail.com>
2016-08-31 12:50:47 +0800

    update

--- nut/settings/production.py
+++ nut/settings/production.py
@@ -5,7 +5,7 @@ from settings import *
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-STATIC_URL = 'http://static.guoku.com/static/v4/be9846365038c569ebe530596e0d37217fb23d19/'
+STATIC_URL = 'http://static.guoku.com/static/v4/33930e4b6976b3b59d8892f4df36f0224433460d/'
 
 LANGUAGE_CODE = 'zh-cn'
 
