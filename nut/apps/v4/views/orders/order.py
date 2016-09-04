6f09303df5ad84f25f1ba6ade799ee1be97dc9a7
jiaxin <edison7500@gmail.com>
2016-09-05 00:42:54 +0800

    update

--- nut/apps/v4/views/orders/order.py
+++ nut/apps/v4/views/orders/order.py
@@ -1,3 +1,6 @@
+from django.core.paginator import Paginator
+from django.utils.log import getLogger
+
 from apps.order.models import Order as OrderModel
 from apps.v4.views import APIJsonSessionView
 from apps.v4.schema.order import OrderSchema
@@ -5,6 +8,7 @@ from apps.v4.schema.order import OrderSchema
 from pprint import pprint
 
 order_schema = OrderSchema()
+log = getLogger('django')
 
 
 class CheckOutView(APIJsonSessionView):
@@ -27,12 +31,23 @@ class OrderListView(APIJsonSessionView):
     http_method_names = ['get']
 
     def get_data(self, context):
-        order_list = OrderModel.objects.filter(customer=self.session.user)
-        # print order_list
-        # print order_schema.dump(order_list).data
-        # pprint(order_schema.dump(order_list.first(), many=False).data, indent=2)
-        return order_schema.dump(order_list, many=True).data
+        if self.status is None:
+            orders = OrderModel.objects.filter(customer=self.session.user)
+        else:
+            orders = OrderModel.objects.filter(customer=self.session.user, status=self.status)
+
+        paginator = Paginator(orders, self.size)
+        try:
+            order_list = paginator.page(self.page)
+        except Exception as e:
+            log.info("<Error: %s>" % e.message)
+            return []
+
+        return order_schema.dump(order_list.object_list, many=True).data
 
     def get(self, request, *args, **kwargs):
+        self.page = request.GET.get('page', 1)
+        self.size = request.GET.get('size', 10)
+        self.status = request.GET.get('status', None)
 
         return super(OrderListView, self).get(request, *args, **kwargs)
