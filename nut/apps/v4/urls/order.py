f91cacfde13b8a281664d9457033d3d101667da8
jiaxin <edison7500@gmail.com>
2016-09-07 21:10:40 +0800

    update

--- nut/apps/v4/urls/order.py
+++ nut/apps/v4/urls/order.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, patterns
-from apps.v4.views.orders.order import OrderListView, WeChatPaymentView
+from apps.v4.views.orders.order import OrderListView, WeChatPaymentView, AliPayPaymentView
 
 urlpatterns = patterns(
     'apps.v4.views.orders.order',
     url(r'^$', OrderListView.as_view(), name='v4_user_order_list'),
 
-    url(r'payment/weixin/(?P<order_id>\d+)/$', WeChatPaymentView.as_view(), name='v4_wechat_payment_url'),
+    url(r'^payment/weixin/(?P<order_id>\d+)/$', WeChatPaymentView.as_view(), name='v4_wechat_payment_url'),
+    url(r'^payment/alipay/(?P<order_id>\d+)/$', AliPayPaymentView.as_view(), name='v4_alipay_payment_url'),
 )
--- nut/apps/v4/views/orders/order.py
+++ nut/apps/v4/views/orders/order.py
@@ -1,3 +1,4 @@
+#encoding=utf-8
 from django.core.paginator import Paginator
 from django.utils.log import getLogger
 
@@ -38,8 +39,11 @@ class OrderListView(APIJsonSessionView):
         return super(OrderListView, self).get(request, *args, **kwargs)
 
 
-class WeChatPaymentView(APIJsonSessionView):
 
+class WeChatPaymentView(APIJsonSessionView):
+    '''
+        获取 微信支付链接
+    '''
     http_method_names = ['get']
 
     def get_data(self, context):
@@ -54,3 +58,21 @@ class WeChatPaymentView(APIJsonSessionView):
         self.order_id   = kwargs.pop('order_id', None)
         assert self.order_id is not None
         return super(WeChatPaymentView, self).get(request, *args, **kwargs)
+
+
+class AliPayPaymentView(APIJsonSessionView):
+
+    http_method_names = ['get']
+
+    def get_data(self, context):
+        try:
+            order = OrderModel.objects.get(pk = self.order_id)
+        except OrderModel.DoesNotExist as e:
+            log.info(e.message)
+            raise ErrorJsonResponse(status=404)
+        return {'alipay_payment_url': order.generate_alipay_payment_url()}
+
+    def get(self, request, *args, **kwargs):
+        self.order_id   = kwargs.pop('order_id', None)
+        assert self.order_id is not None
+        return super(AliPayPaymentView, self).get(request, *args, **kwargs)
