fe066720efb07fb4bae00d75f02a112858e29be7
AzureRay <476468781@qq.com>
2016-09-13 11:14:02 +0800

    update js

--- nut/static/js/management/seller_management/print_checked_entity.js
+++ nut/static/js/management/seller_management/print_checked_entity.js
@@ -5,9 +5,40 @@ var PrintCheckedEntityManager = Class.extend({
     },
     initPrintQRCodeBtn:function(){
         console.log('init print btn');
+        $('#checked_print_qrcode').click(this.handlePrintQrcode.bind(this));
+    },
+    handlePrintQrcode:function(e) {
+        var checked_entity_ids = this.collectCheckedEntity();
+        var url = $(e.currentTarget).attr('data-url');
+        if(checked_entity_ids){
+            console.log('checked entities');
+        }
 
+        this.postPrintQrcode(checked_entity_ids,url);
     }
-
+    ,
+    collectCheckedEntity:function(){
+         var checked_entity_collection = $('.usite-chk:checked').map(function(idx, item){
+            return $(item).attr('value');
+        });
+        return [].slice.call(checked_entity_collection);
+    },
+    postPrintQrcode:function(entities,url){
+         $.when($.ajax({
+            url: url,
+            method: 'POST',
+            data:{'checked_entity_ids':JSON.stringify(entities)}
+        })).then(
+            this.checkedPrintEntitySuccess.bind(this),
+            this.checkedPrintEntityFail.bind(this)
+        );
+    },
+    checkedPrintEntitySuccess:function(){
+        console.log('checked print entity success');
+    },
+    checkedPrintEntityFail:function(){
+        console.log('checked print entity fail');
+    }
 });
 
 (function($, window, document){
