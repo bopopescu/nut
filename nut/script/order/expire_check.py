d47331a5b01cf12675f1a8427a9f1d4e8ed80d0a
An Chen <anchen@guoku.com>
2016-09-21 09:30:31 +0800

    update

--- nut/script/order/expire_check.py
+++ nut/script/order/expire_check.py
@@ -1,11 +1,11 @@
 import os, sys
 
-# sys.path.append('/data/www/nut')
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.production'
+sys.path.append('/data/www/nut')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.production'
 
-
-sys.path.append('/new_sand/guoku/nut/nut')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.dev_anchen'
+#
+# sys.path.append('/new_sand/guoku/nut/nut')
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.dev_anchen'
 
 
 from apps.order.models import Order
