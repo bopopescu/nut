35a3f69c6ef96dcdded3839f7c62d3e4c7b46d9f
AzureRay <476468781@qq.com>
2016-09-21 14:36:53 +0800

    update

--- nut/static/js/web/app/subapp/top_ad/top_ad.js
+++ nut/static/js/web/app/subapp/top_ad/top_ad.js
@@ -1,18 +1,19 @@
 
 define(['libs/Class', 'jquery','cookie'], function(Class, $){
 
-    var  test_url_reg = /20160624/;
+    var  test_url_reg = /20160508/;
     var visited_cookie_key = 'pop_up_store_key';
     // here we use a global var isFromMobile, which is bootstraped in base.html (template)
 
     var TopAd = Class.extend({
         init: function(){
+            console.log('top ad begin');
             this.handleTrackerCookie();
             this.handleTopAdDisplay();
             this.initCloseButton();
         },
         initCloseButton: function(){
-            if($('.entity-selection-body').length>0){
+            if($('.new-index-body').length>0){
                 $('.top-ad .close-button').hide();
             }else{
                 $('.top-ad .close-button').click(this.hideTopAd.bind(this));
