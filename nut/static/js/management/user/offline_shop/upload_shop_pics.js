4ecc204bb3b0ac12d26a7415349872fb7cb1b027
AzureRay <476468781@qq.com>
2016-10-16 22:05:05 +0800

    update

--- nut/static/js/management/user/offline_shop/upload_shop_pics.js
+++ nut/static/js/management/user/offline_shop/upload_shop_pics.js
@@ -2,13 +2,24 @@ var OfflineShopManager = Class.extend({
     init: function(){
         console.log('offline shop begin');
         this.initAddPicBtn();
+        this.upload_pic_modal_content = $('#add_pic_modal_content').html();
     },
 
     initAddPicBtn:function(){
         $('#add_pic_button').click(this.show_add_pic_dialog.bind(this));
     },
      show_add_pic_dialog:function(e){
-           bootbox.alert('show add pic dialog');
+         bootbox.hideAll();
+         bootbox.dialog({
+             title: '上传商店图片',
+             onEscape: true,
+             backdrop:true,
+             closeButton: true,
+             animate: true,
+             className: 'upload-pic-dialog',
+             message:this.upload_pic_modal_content
+            });
+
         }
 
 });
