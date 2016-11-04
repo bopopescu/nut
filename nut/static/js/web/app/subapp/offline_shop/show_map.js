a44c1ef5dfff4c846197803ce47a17afae8443ab
AzureRay <476468781@qq.com>
2016-11-04 17:38:31 +0800

    disable drag map on mobile

--- nut/static/js/web/app/subapp/offline_shop/show_map.js
+++ nut/static/js/web/app/subapp/offline_shop/show_map.js
@@ -6,14 +6,17 @@ define(['libs/Class','jquery'], function(Class,$) {
         },
         initShowMap:function(){
             // 百度地图API功能
-        var map = new BMap.Map("map");
-        //启用滚轮放大缩小，默认禁用
-        map.enableScrollWheelZoom();
-        //启用地图惯性拖拽，默认禁用
-        map.enableContinuousZoom();
-
-        var getInputLng = $('#map').attr('data-lng');
-        var getInputLat =  $('#map').attr('data-lat');
+            var map = new BMap.Map("map");
+            if(isFromMobile){
+                map.disableDragging();
+            }else{
+                //启用滚轮放大缩小，默认禁用
+                map.enableScrollWheelZoom();
+                //启用地图惯性拖拽，默认禁用
+                map.enableContinuousZoom();
+            }
+            var getInputLng = $('#map').attr('data-lng');
+            var getInputLat =  $('#map').attr('data-lat');
             if(getInputLng && getInputLat){
                 var point = new BMap.Point(getInputLng,getInputLat);
                 var marker = new BMap.Marker(new BMap.Point(getInputLng,getInputLat));
