dcce6a5c8b66c040fe07e264aa4a9f7fba16cfab
AzureRay <476468781@qq.com>
2016-11-20 22:41:17 +0800

    update

--- nut/static/js/web/app/subapp/market/device_orientation.js
+++ nut/static/js/web/app/subapp/market/device_orientation.js
@@ -3,28 +3,28 @@ define(['libs/Class','jquery'], function(
 ){
     var DeviceOrientationApp = Class.extend({
         init: function(){
-            //var self = this;
-            //if(window.DeviceOrientationEvent){
-            //    console.log('device orientation is supported');
-            //    window.addEventListener('deviceorientation',function(eventData){
-            //        var LR = eventData.gamma;
-            //        var FB = eventData.beta;
-            //        var DIR = eventData.alpha;
-            //        self.deviceOrientationHandler(LR,FB,DIR);
-            //    },false);
-            //}else{
-            //    console.log('device orientation is not supported');
-            //}
+            var self = this;
+            if(window.DeviceOrientationEvent){
+                console.log('device orientation is supported');
+                window.addEventListener('deviceorientation',function(eventData){
+                    var LR = eventData.gamma;
+                    var FB = eventData.beta;
+                    var DIR = eventData.alpha;
+                    self.deviceOrientationHandler(LR,FB,DIR);
+                },false);
+            }else{
+                console.log('device orientation is not supported');
+            }
         },
         deviceOrientationHandler:function(LR,FB,DIR){
             var titles_array = $('.title-rect');
             titles_array.each(function(index,ele){
                 //if(index % 2 ==0){
-                //    ele.style.webkitTransform = "rotate("+LR+"deg) rotate3d(1,0,0,"+(FB*-1+30)+"deg)";
-                //    ele.style.transform = "rotate("+LR+"deg) rotate3d(1,0,0,"+(FB*-1+30)+"deg)";
+                //    ele.style.webkitTransform = "rotate("+LR+"deg)";
+                //    ele.style.transform = "rotate("+LR+"deg)";
                 //}else{
-                    ele.style.webkitTransform = "rotate("+(LR*-1)+"deg) rotate3d(1,0,0,"+(FB-30)+"deg)";
-                    ele.style.transform = "rotate("+(LR*-1)+"deg) rotate3d(1,0,0,"+(FB-30)+"deg)";
+                    ele.style.webkitTransform = "rotate("+(LR*-1)+"deg)";
+                    ele.style.transform = "rotate("+(LR*-1)+"deg)";
                 //}
             });
         }
