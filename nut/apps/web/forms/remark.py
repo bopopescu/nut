7672458ab57327e66af97e92493627c8e80af10c
An Chen <anchen@guoku.com>
2017-02-13 15:12:37 +0800

    fix article remark js injection

--- nut/apps/web/forms/remark.py
+++ nut/apps/web/forms/remark.py
@@ -1,8 +1,16 @@
+import bleach
 from django.forms import ModelForm, Textarea, HiddenInput, NumberInput
 from apps.core.models import Article_Remark
 
 
+ALLOWED_REMARK_TAGS=[]
+
 class ArticleRemarkForm(ModelForm):
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        return bleach.clean(content, tags=ALLOWED_REMARK_TAGS)
+
+
     class Meta:
         model = Article_Remark
         fields = ['content', 'reply_to']
