da39ec07f44318060020604853545603a5c0d9f9
0bit <0bit@ohmail.org>
2017-04-15 17:47:08 +0800

    update deploy/upload_test.py file

--- deploy/upload_test.py
+++ deploy/upload_test.py
@@ -16,11 +16,11 @@ root_dir = os.path.join(os.getcwd(), '..')
 env.hosts = ['114.113.154.48']
 
 env.user = Config.get('global', 'user')
-#env.key = os.path.join(root_dir, Config.get('global', 'key'))
 env.password = 'jessie1@#'
 
 env.local_root = os.path.join(root_dir, Config.get('local', 'project_dir'))
 env.project_root = Config.get('server', 'project_dir')
+
 
 def update_code():
     local('git pull origin dev')
@@ -29,20 +29,11 @@ def update_code():
 @parallel
 def upload_code():
     rsync_project(
-            remote_dir = env.project_root,
-            local_dir = env.local_root,
-            delete = True,
-            extra_opts = "--password-file='guoku.pass' "
-        )
+        remote_dir=env.project_root,
+        local_dir=env.local_root,
+        delete=True,
+    )
 
-# script_dir = Config.get('server', 'script_dir')
-#
-# def reload_gunicorn():
-# 	with cd(script_dir):
-# 		sudo('/bin/bash ./gunicorn reload')
 
 def upload():
     execute(upload_code)
-
-# def reload_test():
-#     execute(reload_gunicorn)
