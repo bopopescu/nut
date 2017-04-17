146bfa5a0e1771f79f8391acfa598c08f519df99
0bit <0bit@ohmail.org>
2017-04-17 11:43:23 +0800

    更新微信菜单修改接口

--- nut/apps/wechat/views/menu.py
+++ nut/apps/wechat/views/menu.py
@@ -1,38 +1,30 @@
 # -*- coding: utf-8 -*-
+import json
+
 from django.http import HttpResponse
 from django.views.generic.base import View, TemplateResponseMixin, ContextMixin
 from django.utils.log import getLogger
 from django.core.cache import cache
-# from json import loads
 import time
-# import urllib2
-# import urllib
 import requests
-from django.utils import simplejson
 from django.conf import settings
 
 AppID = getattr(settings, 'WECHAT_APP_ID', None)
 AppSecret = getattr(settings, 'WECHAT_APP_SECRET', None)
 
-# AppID = settings.WeChatAppID
-# AppSecret = settings.WeChatAppSecret
-
-
 log = getLogger('django')
 
 
 class MenuCreateView(TemplateResponseMixin, ContextMixin, View):
-
     template_name = 'wechat/menu.html'
     parameters = {
-            'appid': AppID,
-            'secret': AppSecret,
-            'grant_type': 'client_credential',
-        }
+        'appid': AppID,
+        'secret': AppSecret,
+        'grant_type': 'client_credential',
+    }
     log.info(parameters)
     _access_token = None
     _expires_in = None
-
 
     @property
     def access_token(self):
@@ -65,41 +57,40 @@ class MenuCreateView(TemplateResponseMixin, ContextMixin, View):
         return self.access_token
 
     def post_menu(self, access_token, body):
-        json_string = simplejson.dumps(body, ensure_ascii=False)
+        json_string = json.dumps(body, ensure_ascii=False)
         log.info(json_string)
         headers = {'content-type': 'application/json'}
-        r = requests.post('https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s' % access_token, data=json_string, headers=headers)
+        r = requests.post(
+            'https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s' % access_token,
+            data=json_string, headers=headers)
 
         return r.text
 
     def get(self, request, *args, **kwargs):
-
         return self.render_to_response(self.get_context_data())
 
     def post(self, request, *args, **kwargs):
-        # access_token = cache.get('wechat_access_token')
-        # if access_token is None:
         access_token = self.get_token()
 
         data = {
-            "button":[
+            "button": [
                 {
-                    "name":"热门精选",
+                    "name": "热门精选",
                     "sub_button": [
                         {
-                            "name":"每日精选",
-                            "type":"view",
-                            "url":"http://guoku.com/selected/",
+                            "name": "每日精选",
+                            "type": "view",
+                            "url": "http://guoku.com/selected/",
                         },
                         {
-                            "name":"热门商品",
-                            "type":"view",
-                            "url":"http://guoku.com/popular/",
+                            "name": "热门商品",
+                            "type": "view",
+                            "url": "http://guoku.com/popular/",
                         },
                         {
-                            "name":"更多专题",
-                            "type":"view",
-                            "url":"http://guoku.com/event/",
+                            "name": "更多专题",
+                            "type": "view",
+                            "url": "http://guoku.com/event/",
                         },
                         {
                             "type": "click",
@@ -109,43 +100,22 @@ class MenuCreateView(TemplateResponseMixin, ContextMixin, View):
                     ]
                 },
                 {
-                    "name":"线下商店",
-                    "sub_button": [
-                        # {
-                        #     "name": "十一活动",
-                        #     "type": "view",
-                        #     "url": "http://m.guoku.com/articles/135144/"
-                        # },
-                        {
-                            "name"  : "服务计划",
-                            "type"  : "view",
-                            "url"   : "http://m.guoku.com/shopservice/"
-                        },
-                        {
-                            "name"  : "商店列表",
-                            "type"  : "view",
-                            "url"   : "http://m.guoku.com/articles/135791/"
-                        },
-
-                    ]
-                },
-                {
-                    "name":"应用下载",
+                    "name": "应用下载",
                     "sub_button": [
                         {
-                            "name":"iPhone 版",
-                            "type":"view",
-                            "url":"https://itunes.apple.com/cn/app/guo-ku/id477652209?mt=8",
+                            "name": "iPhone 版",
+                            "type": "view",
+                            "url": "https://itunes.apple.com/cn/app/guo-ku/id477652209?mt=8",
                         },
                         {
-                            "name":"iPad 版",
-                            "type":"view",
-                            "url":"https://itunes.apple.com/cn/app/guo-kuhd/id450507565?mt=8",
+                            "name": "iPad 版",
+                            "type": "view",
+                            "url": "https://itunes.apple.com/cn/app/guo-kuhd/id450507565?mt=8",
                         },
                         {
-                            "name":"Andorid 版",
-                            "type":"view",
-                            "url":"http://www.wandoujia.com/apps/com.guoku",
+                            "name": "Android 版",
+                            "type": "view",
+                            "url": "http://www.wandoujia.com/apps/com.guoku",
                         }
                     ]
                 }
@@ -155,8 +125,6 @@ class MenuCreateView(TemplateResponseMixin, ContextMixin, View):
         res = self.post_menu(access_token=access_token, body=data)
         log.info(res)
         return HttpResponse(res)
-
-
 
 
 __author__ = 'edison7500'
