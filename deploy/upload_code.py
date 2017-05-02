d757334bedf97f02589b33ceeee2bf652606936d
0bit <0bit@ohmail.org>
2017-05-02 22:13:50 +0800

    更新部署脚本

--- deploy/upload_code.py
+++ deploy/upload_code.py
@@ -16,11 +16,11 @@ root_dir = os.path.join(os.getcwd(), '..')
 env.hosts = ['10.0.2.46', '10.0.2.47']
 
 env.user = Config.get('global', 'user')
-#env.key = os.path.join(root_dir, Config.get('global', 'key'))
-env.password = 'jessie1@#'
+env.key = os.path.expanduser(Config.get('global', 'key'))
 
 env.local_root = os.path.join(root_dir, Config.get('local', 'project_dir'))
 env.project_root = Config.get('server', 'project_dir')
+
 
 def update_code():
     local('git pull origin master')
@@ -28,12 +28,14 @@ def update_code():
 
 @parallel
 def upload_code():
+    ssh_opts = '-i {}'.format(env.key)
     rsync_project(
-            remote_dir = env.project_root,
-            local_dir = env.local_root,
-            delete = True,
-            extra_opts = "--password-file='guoku.pass' "
-        )
+        remote_dir=env.project_root,
+        local_dir=env.local_root,
+        delete=True,
+        ssh_opts=ssh_opts
+    )
+
 
 def upload():
     execute(upload_code)
