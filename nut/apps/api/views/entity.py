5ae8ff1b19f0e5b3f24250580ce0683a2328f5a3
0bit <0bit@ohmail.org>
2017-06-01 17:00:06 +0800

    更新上传图文到百度百家的接口

--- nut/apps/api/views/entity.py
+++ nut/apps/api/views/entity.py
@@ -74,18 +74,32 @@ class UploadArticleView(APIView):
     def post(self, request):
         article_id = request.data.get('article_id')
         article_slug = request.data.get('article_slug', '')
+        article_url = request.data.get('article_url', '')
         abstract = request.data.get('abstract', u'')
+        title = request.data.get('title', u'')
         domain = request.data.get('domain', u'1')
-        if article_id:
-            article = Article.objects.get(pk=article_id)
-        elif article_slug:
-            article = Article.objects.get(article_slug=article_slug)
+
+        try:
+            if article_id:
+                article = Article.objects.get(pk=article_id)
+            elif article_slug:
+                article = Article.objects.get(article_slug=article_slug)
+            elif article_url:
+                article_slug = article_url.split('/')[-2]
+                article = Article.objects.get(article_slug=article_slug)
+            else:
+                return Response(u'article_id、article_slug、article_url至少包含一个')
+        except Article.DoesNotExist:
+            return Response(u'文章不存在。 ({})'.format(article_id or article_slug or article_url))
+
         soup = BeautifulSoup(article.content)
         cards = soup.find_all(class_='guoku-card')
         entity_hash_list = [card['data_entity_hash'] for card in cards]
         for index, card in enumerate(cards):
-            node = u'<iframe class="spd-faked-goods" data-pos={}></iframe>'.format(index)
-            card.replace_with(node)
+            new_tag = soup.new_tag(u'iframe')
+            new_tag['class'] = 'spd-faked-goods'
+            new_tag['data-pos'] = index
+            card.replace_with(new_tag)
         goods_info = [{'sku_id': entity_hash, 'tpl_id': 'BdrainrwSpdGoodsHasPic'} for entity_hash in
                       entity_hash_list]
         url = u'http://hoteltest.baidu.com/business/article_publish'
@@ -97,7 +111,8 @@ class UploadArticleView(APIView):
             'v': u'1.0',
             'domain': domain,
             'origin_url': urljoin(u'http://www.guoku.com', article.url).encode('utf-8'),
-            'abstract': abstract or article.title,
+            'title': title or article.title,
+            'abstract': abstract or '',
             'content': unicode(soup),
             'cover_layout': u'one',
             'cover_images': json.dumps([article.cover_url]).encode('utf-8'),
@@ -108,8 +123,12 @@ class UploadArticleView(APIView):
         values += token
         sign = hashlib.md5(values.encode('utf-8')).hexdigest()
         payload['sign'] = sign
-        response = requests.post(url, data=payload)
-        return Response(response.json())
+
+        try:
+            response = requests.post(url, data=payload)
+            return Response(response.json())
+        except Exception as e:
+            return Response(unicode(e))
 
 
 upload_article_view = UploadArticleView.as_view()
