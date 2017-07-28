bf269afd844225bc5189bce1557ad55c5d8f099f
0bit <0bit@ohmail.org>
2017-07-28 11:25:48 +0800

    清除恶意用户的更多信息

--- nut/apps/core/management/commands/clean_removed_user_data.py
+++ nut/apps/core/management/commands/clean_removed_user_data.py
@@ -2,7 +2,7 @@
 
 from django.core.management.base import BaseCommand
 
-from apps.core.models import GKUser, Note, Entity
+from apps.core.models import GKUser, Note, Entity, Article_Remark
 
 
 class Command(BaseCommand):
@@ -11,10 +11,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         notes = Note.objects.filter(user__is_active=GKUser.remove).exclude(status=Note.remove)
         delete_note_amount = notes.update(status=Note.remove)
-
         self.stdout.write('Delete {} notes.'.format(delete_note_amount))
 
         entities = Entity.objects.filter(user__is_active=GKUser.remove).exclude(status=Entity.remove)
-        entities.update(status=Entity.remove)
+        delete_entity_amount = entities.update(status=Entity.remove)
+        self.stdout.write('Delete {} entities.'.format(delete_entity_amount))
 
-        self.stdout.write('Delete {} entities.'.format(delete_note_amount))
+        remarks = Article_Remark.objects.filter(user__is_active=GKUser.remove).exclude(status=Article_Remark.remove)
+        delete_remark_amount = remarks.update(status=Article_Remark.remove)
+        self.stdout.write('Delete {} entities.'.format(delete_remark_amount))
