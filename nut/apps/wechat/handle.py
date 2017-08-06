7d3b8fc2511e09b67495e2022382f0146608712d
0bit <0bit@ohmail.org>
2017-08-06 13:22:57 +0800

    整理代码

--- nut/apps/wechat/handle.py
+++ nut/apps/wechat/handle.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-# from apps.wechat.models import Robots
 from apps.core.models import Entity, Selection_Entity, Entity_Like
 from apps.wechat.models import Token
 from datetime import datetime
@@ -13,14 +12,15 @@ log = getLogger('django')
 
 robot_handler = RobotHandler()
 
+
 def regex(content, pattern):
-    pobj = re.compile(pattern)
-    return pobj.search(content.decode('utf-8'))
+    p = re.compile(pattern)
+    return p.search(content.decode('utf-8'))
+
 
 def handle_reply(content):
     log.error(content.decode('utf-8'))
     res = list()
-
 
     if content.decode('utf-8') == u'精选':
         _refresh_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -37,14 +37,11 @@ def handle_reply(content):
     elif content.decode('utf-8') == u'转载':
         return u'文章转载授权请联系 alka@guoku.com'
     elif regex(content.lower(), u'平壤'):
-        return  u'免费送你去最神秘的国度撒野｜平壤马拉松体验师招募 立即领取抽奖码 ►' \
-                u'<a href="http://www.yun-wifi.net/index.php/home/code/index/from/8">立即领取抽奖码 ►</a>'
-    #     return u'「清迈旅行体验师招募｜你的工作就是玩够5天！」 ' \
-    #            u'<a href="http://www.yun-wifi.net/index.php/home/code/index/from/8">►立即领抽奖码：</a>'
+        return u'免费送你去最神秘的国度撒野｜平壤马拉松体验师招募 立即领取抽奖码 ►' \
+               u'<a href="http://www.yun-wifi.net/index.php/home/code/index/from/8">立即领取抽奖码 ►</a>'
     elif robot_handler.can_handle(content.lower()):
         return robot_handler.handle(content.lower())
     else:
-        # _entities = Entity.search.query(content.decode('utf-8')).order_by('@weight', '-created_time')
         sqs = SearchQuerySet()
         results = sqs.auto_query(content.decode('utf-8')).models(Entity).order_by('-like_count')
 
@@ -57,7 +54,6 @@ def handle_reply(content):
 
 
 def handle_event(content):
-    # log.info(content)
     items = []
     if content['EventKey'] == "V1001_SELECTION":
         _refresh_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -65,7 +61,6 @@ def handle_event(content):
 
         for row in entities[:5]:
             items.append(row.entity)
-        # return items
     elif content['EventKey'] == "V1002_POPULAR":
         popular_list = Entity_Like.objects.popular_random()
         entities = Entity.objects.filter(id__in=popular_list)
@@ -78,13 +73,8 @@ def handle_event(content):
             log.info(e)
             return None
 
-        el = Entity_Like.objects.filter(user = token.user)
+        el = Entity_Like.objects.filter(user=token.user)
         for row in el[:5]:
             items.append(row.entity)
-        # return items
 
     return items
-
-
-
-__author__ = 'edison'
