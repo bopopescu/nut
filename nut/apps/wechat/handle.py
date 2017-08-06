11648247ef93ad89956ed60fa69bbc05ec2527b2
0bit <0bit@ohmail.org>
2017-08-06 18:31:00 +0800

    整理微信处理代码，新增自动回复内容

--- nut/apps/wechat/handle.py
+++ nut/apps/wechat/handle.py
@@ -8,6 +8,14 @@ from apps.wechat.robot import RobotHandler
 
 import re
 
+auto_replies = {
+    u'合作': u'合作事宜请发邮件到 bd@guoku.com',
+    u'转载': u'文章转载授权请联系 alka@guoku.com',
+    u'糯言': u'''哈喽，果库妹给你送福利啦：
+<a href="https://taoquan.taobao.com/coupon/unify_apply.htm?sellerId=2186239207&activityId=fa282ab6e48b4c439dd223864059081a">糯言5.00元商品优惠劵</a>
+<a href="https://taoquan.taobao.com/coupon/unify_apply.htm?sellerId=2186239207&activityId=fc5cec52fd2646f5a4135c365fd49d54">糯言10.00元商品优惠劵</a>'''
+}
+
 log = getLogger('django')
 
 robot_handler = RobotHandler()
@@ -18,39 +26,34 @@ def regex(content, pattern):
     return p.search(content.decode('utf-8'))
 
 
-def handle_reply(content):
-    log.error(content.decode('utf-8'))
-    res = list()
+def handle_reply(raw_content):
+    raw_content = raw_content.lower()
+    content = raw_content.decode('utf-8')
 
-    if content.decode('utf-8') == u'精选':
+    log.info(content)
+
+    if content == u'精选':
         _refresh_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         entities = Selection_Entity.objects.published().filter(pub_time__lte=_refresh_datetime)
-
-        for row in entities[:5]:
-            res.append(row.entity)
-    elif content.decode('utf-8') == u'热门':
+        return [row.entity for row in entities[:5]]
+    elif content == u'热门':
         popular_list = Entity_Like.objects.popular_random()
         entities = Entity.objects.filter(id__in=popular_list)
-        res = entities[:5]
-    elif content.decode('utf-8') == u'合作':
-        return u'合作事宜请发邮件到 bd@guoku.com'
-    elif content.decode('utf-8') == u'转载':
-        return u'文章转载授权请联系 alka@guoku.com'
-    elif regex(content.lower(), u'平壤'):
-        return u'免费送你去最神秘的国度撒野｜平壤马拉松体验师招募 立即领取抽奖码 ►' \
-               u'<a href="http://www.yun-wifi.net/index.php/home/code/index/from/8">立即领取抽奖码 ►</a>'
-    elif robot_handler.can_handle(content.lower()):
-        return robot_handler.handle(content.lower())
+        return entities[:5]
+    elif content in auto_replies:
+        return auto_replies[content]
+    elif robot_handler.can_handle(raw_content):
+        return robot_handler.handle(raw_content)
     else:
         sqs = SearchQuerySet()
-        results = sqs.auto_query(content.decode('utf-8')).models(Entity).order_by('-like_count')
-
+        results = sqs.auto_query(content).models(Entity).order_by('-like_count')
+        res = []
         for row in results[:10]:
             try:
                 res.append(row.object)
             except Exception as e:
                 log.error("<Error: {0}>".format(e.message))
-    return res
+        return res
 
 
 def handle_event(content):
