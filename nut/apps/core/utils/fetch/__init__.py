95490869341bd53375eb848ac2da808380894bc2
0bit <0bit@ohmail.org>
2017-08-06 19:23:19 +0800

    优化parse_taobao_id_from_url函数，解决id中可能包含#的问题；

--- nut/apps/core/utils/fetch/__init__.py
+++ nut/apps/core/utils/fetch/__init__.py
@@ -1,19 +1,24 @@
 import re
+from six.moves import urllib_parse
+
 
 def parse_taobao_id_from_url(url):
-    params = url.split("?")[1]
-    for param in params.split("&"):
-        tokens = param.split("=")
-        if len(tokens) >= 2 and (tokens[0] == "id" or tokens[0] == "item_id"):
-            return tokens[1]
+    url_obj = urllib_parse.urlparse(url)
+    query_dict = urllib_parse.parse_qs(url_obj.query)
+    accept_keys = ('id', 'item_id')
+    for key in accept_keys:
+        if key in query_dict:
+            return query_dict[key][0]
     return None
 
+
 def parse_jd_id_from_url(url):
-    ids = re.findall(r'\d+',url)
+    ids = re.findall(r'\d+', url)
     if len(ids) > 0:
         return ids[0]
     else:
         return None
+
 
 def parse_kaola_id_from_url(url):
     ids = re.findall(r'\d+', url)
@@ -22,6 +27,7 @@ def parse_kaola_id_from_url(url):
     else:
         return None
 
+
 def parse_booking_id_from_url(url):
     params = url.split("?")[1]
     for param in params.split(";"):
@@ -29,5 +35,3 @@ def parse_booking_id_from_url(url):
         if len(tokens) >= 2 and tokens[0] == "sid":
             return tokens[1]
     return None
-
-__author__ = 'edison'
